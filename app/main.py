949194dde2513551923251becda98b72eab29360
Ihor Olifirenko <igoroliphirenko@gmail.com>
2022-01-20 19:42:53 +0200

    replaced list with accum variable

--- app/main.py
+++ app/main.py
@@ -24,14 +24,14 @@ class CarWashStation:
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, list_of_cars: list):
-        amount = []
+        amount = 0
 
         for car in list_of_cars:
             if car.clean_mark < self.clean_power:
-                amount.append(self.calculate_washing_price(car))
+                amount += (self.calculate_washing_price(car))
                 self.wash_single_car(car)
 
-        return sum(amount)
+        return amount
 
     def calculate_washing_price(self, car):
         difference = self.clean_power - car.clean_mark
