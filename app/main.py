7c5fe7cd30ab6792578fcd25a4c6429138f378b4
Denys Lipin <lipin.transstream@gmail.com>
2022-01-20 18:36:28 +0200

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -28,9 +28,10 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car):
         if car.clean_mark < self.clean_power:
-            pow_to_mark = self.clean_power - car.clean_mark
-            rat_to_dist = self.average_rating / self.distance_from_city_center
-            return round(car.comfort_class * pow_to_mark * rat_to_dist, 2)
+            price = car.comfort_class \
+                     * (self.clean_power - car.clean_mark) \
+                     * self.average_rating / self.distance_from_city_center
+            return round(price, 2)
         else:
             return 0
 
