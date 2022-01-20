90c21d495315abde473fa998fb03188da957f827
Denys Lipin <lipin.transstream@gmail.com>
2022-01-20 12:12:20 +0200

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -19,23 +19,26 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, list_of_cars: list) -> int:
+    def serve_cars(self, list_of_cars: list):
         income = 0
         for one_car in list_of_cars:
-            if one_car.clean_mark < self.clean_power:
-                income += self.calculate_washing_price(one_car)
-                self.wash_single_car(one_car)
+            income += self.calculate_washing_price(one_car)
+            self.wash_single_car(one_car)
         return income
 
-    def calculate_washing_price(self, single_car):
-        rat_to_dist = self.average_rating / self.distance_from_city_center
-        pow_to_mark = self.clean_power - single_car.clean_mark
-        return round(single_car.comfort_class * pow_to_mark * rat_to_dist, 2)
+    def calculate_washing_price(self, car: Car):
+        if car.clean_mark < self.clean_power:
+            pow_to_mark = self.clean_power - car.clean_mark
+            rat_to_dist = self.average_rating / self.distance_from_city_center
+            return round(car.comfort_class * pow_to_mark * rat_to_dist, 2)
+        else:
+            return 0
 
-    def wash_single_car(self, single_car):
-        single_car.clean_mark = self.clean_power
+    def wash_single_car(self, single_car: Car) -> None:
+        if single_car.clean_mark < self.clean_power:
+            single_car.clean_mark = self.clean_power
 
-    def rate_service(self, rate):
+    def rate_service(self, rate: int) -> None:
         new_rating = (self.average_rating * self.count_of_ratings) + rate
         new_average_rating = new_rating / (self.count_of_ratings + 1)
         self.average_rating = round(new_average_rating, 1)
