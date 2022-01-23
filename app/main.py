a2e859dbe897123838b67eaff825d859975a8c4a
Oleksandr Zhukov <alexanderzhukov88@gmail.com>
2022-01-24 00:24:34 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -19,12 +19,14 @@ class CarWashStation:
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, cars):
-        income = []
+        income = sum(
+            [self.calculate_washing_price(car)
+             for car in cars
+             if car.clean_mark < self.clean_power]
+        )
         for car in cars:
-            if car.clean_mark < self.clean_power:
-                income.append(self.calculate_washing_price(car))
-                self.wash_single_car(car)
-        return sum(income)
+            self.wash_single_car(car)
+        return income
 
     def calculate_washing_price(self, car):
         level_of_dirt = self.clean_power - car.clean_mark
