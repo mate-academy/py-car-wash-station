eee57099d11cafff0d5347e4c851820d318f0b79
roland813 <olegkorostensky@gmail.com>
2022-01-24 15:53:45 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -19,23 +19,24 @@ class CarWashStation:
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, cars: list):
-        suma = sum([self.calculate_washing_price(car) for car in cars
-                    if car.clean_mark < self.clean_power])
+        suma = round(sum([self.calculate_washing_price(car)
+                          for car in cars
+                          if car.clean_mark < self.clean_power]), 1)
         for car in cars:
             if car.clean_mark <= self.clean_power:
                 self.wash_single_car(car)
         return suma
 
     def calculate_washing_price(self, car):
-        return round(
-            (car.comfort_class * (self.clean_power - car.clean_mark)) *
-            (self.average_rating / self.distance_from_city_center), 1)
+        class_clean = car.comfort_class * (self.clean_power - car.clean_mark)
+        rate_daistance = self.average_rating / self.distance_from_city_center
+        return round(class_clean * rate_daistance, 1)
 
     def wash_single_car(self, car):
-        car.clean_mark = self.clean_power
+        if car.clean_mark <= self.clean_power:
+            car.clean_mark = self.clean_power
 
     def rate_service(self, rate):
-        self.average_rating = round(
-            (self.average_rating * self.count_of_ratings + rate) /
-            (self.count_of_ratings + 1), 1)
+        rate_change = self.average_rating * self.count_of_ratings + rate
         self.count_of_ratings += 1
+        self.average_rating = round(rate_change / self.count_of_ratings, 1)
