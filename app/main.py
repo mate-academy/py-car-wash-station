1b3430259ba5f80d6700ce7fc7623dbe2eea351c
Marko Khodan <khodanmarko@gmail.com>
2022-05-12 00:00:39 +0300

    flake8 fixed

--- app/main.py
+++ app/main.py
@@ -29,18 +29,18 @@ class CarWashStation:
         return income
 
     def calculate_washing_price(self, car):
-        return round(
-            car.comfort_class
-            * (self.clean_power - car.clean_mark)
-            * self.average_rating / self.distance_from_city_center, 1)
+        clean_rate = self.clean_power - car.clean_mark
+        branch_rate = self.average_rating / self.distance_from_city_center
+        washing_price = car.comfort_class * clean_rate * branch_rate
+
+        return round(washing_price, 1)
 
     def wash_single_car(self, car):
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
     def rate_service(self, rate_mark):
-        self.average_rating = round(
-            (self.average_rating * self.count_of_ratings + rate_mark)
-            / (self.count_of_ratings + 1), 1)
-
+        total_of_marks = self.average_rating * self.count_of_ratings
         self.count_of_ratings += 1
+        new_rate = (total_of_marks + rate_mark) / self.count_of_ratings
+        self.average_rating = round(new_rate, 1)
