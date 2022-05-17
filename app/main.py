c5478e66e183302e1ac2274eb5fca367e583dc65
Max Sukhaniuk <maksym.sukhaniuk@gmail.com>
2022-05-17 19:16:27 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -26,20 +26,21 @@ class CarWashStation:
         return round(income, 1)
 
     def wash_single_car(self, car):
-        car.clean_mark = self.clean_power
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
 
     def calculate_washing_price(self, car: Car):
-        return round(
-            (car.comfort_class * (self.clean_power - car.clean_mark
-                                  ) * self.average_rating
-             ) / self.distance_from_city_center, 1
+        income = car.comfort_class * self.average_rating * (
+            self.clean_power - car.clean_mark
         )
+        spending = self.distance_from_city_center
+        return round(income / spending, 1)
 
     def rate_service(self, rate):
-        self.average_rating = round(
-            ((self.average_rating * self.count_of_ratings) + rate
-             ) / (self.count_of_ratings + 1), 1)
+        new_sum_of_rate = self.average_rating * self.count_of_ratings + rate
         self.count_of_ratings += 1
+
+        self.average_rating = round(new_sum_of_rate / self.count_of_ratings, 1)
 
     def __repr__(self):
         return f"{self.distance_from_city_center, self.clean_power}" \
