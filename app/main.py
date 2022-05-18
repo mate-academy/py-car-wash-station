f307cde1d0849c364d7666b1a4c1ac1144fbc27e
Flashmobber <ufinaid@gmail.com>
2022-05-18 17:15:48 +0300

    Recomedation is implemmented. Сonclusions will be drawn...

--- app/main.py
+++ app/main.py
@@ -1,40 +1,33 @@
 class Car:
-    comfort_class: int
-    clean_mark: int
-    brand: str
 
-    def __init__(self, c_class: int, c_mark: int, car_brand: str):
-        self.comfort_class = c_class
-        self.clean_mark = c_mark
+    def __init__(self, comfort_class: int, clean_mark: int, car_brand: str):
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
         self.brand = car_brand
 
 
 class CarWashStation:
-    distance_from_city_center: float
-    clean_power: int
-    average_rating: float
-    count_of_ratings: int
 
-    def __init__(self, distance: float, c_power: int,
-                 rating: float, count_of_votes: int):
-        self.distance_from_city_center = distance
-        self.clean_power = c_power
-        self.average_rating = rating
-        self.count_of_ratings = count_of_votes
+    def __init__(self, distance_from_city_center: float, clean_power: int,
+                 average_rating: float, count_of_ratings: int):
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def calculation(self, car):
+        result = self.calculate_washing_price(car)
+        self.wash_single_car(car)
+        return result
 
     def serve_cars(self, washes: list) -> float:
-        def calculation(car):
-            result = self.calculate_washing_price(car)
-            self.wash_single_car(car)
-            return result
-
-        return sum(calculation(car) for car in washes
+        return sum(self.calculation(car) for car in washes
                    if self.clean_power > car.clean_mark)
 
     def calculate_washing_price(self, car: Car):
-        return round(car.comfort_class * (
-                     self.clean_power - car.clean_mark) * (
-                     self.average_rating / self.distance_from_city_center), 1)
+        count_var = car.comfort_class * (self.clean_power - car.clean_mark)
+        second_var = self.average_rating / self.distance_from_city_center
+        return round(count_var * second_var, 1)
 
     def wash_single_car(self, car: Car):
         if self.clean_power > car.clean_mark:
@@ -42,7 +35,7 @@ class CarWashStation:
         return car
 
     def rate_service(self, vote: int):
-        vt = round((self.average_rating * self.count_of_ratings + vote
-                    ) / (self.count_of_ratings + 1), 1)
-        self.average_rating = round(vt, 1)
+        count_var = self.average_rating * self.count_of_ratings + vote
+        vt = round(count_var / (self.count_of_ratings + 1), 1)
+        self.average_rating = vt
         self.count_of_ratings += 1
