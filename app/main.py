2bb26813d286779e927c0ef1ccc75dd35110ab62
Roma Tsybulia <Romalonstejn@gmail.com>
2022-05-18 16:19:09 +0300

    Refactoring

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 class Car:
-    def __init__(self, comfort_class, clean_mark, brand):
+    def __init__(
+            self,
+            comfort_class,
+            clean_mark,
+            brand
+    ):
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -10,10 +15,10 @@ class CarWashStation:
     def __init__(
             self,
             distance_from_city_center,
-            clean_power, average_rating,
+            clean_power,
+            average_rating,
             count_of_ratings
     ):
-
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -26,15 +31,16 @@ class CarWashStation:
                 print(car.clean_mark)
                 income += float(self.calculate_washing_price(car))
             self.wash_single_car(car)
+
         return income
 
     def calculate_washing_price(self, car):
-        car_class = car.comfort_class
         clean_difference = self.clean_power - car.clean_mark
-        rating = self.average_rating
-        distance = self.distance_from_city_center
+        multipliers_1 = self.average_rating * car.comfort_class
+        multipliers_2 = clean_difference * multipliers_1
+        result = multipliers_2 / self.distance_from_city_center
 
-        return f'{(car_class * clean_difference * rating / distance):.1f}'
+        return round(result, 1)
 
     def wash_single_car(self, car):
         if car.clean_mark < self.clean_power:
