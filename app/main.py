eb286372a9f037974c3fcd7eadce4a9982405e95
Dima Svirsa <august4smile@gmail.com>
2022-05-18 20:51:55 +0300

    each parameter from new line, sory

--- app/main.py
+++ app/main.py
@@ -14,7 +14,8 @@ class CarWashStation:
     def __init__(
             self,
             distance_from_city_center,
-            clean_power, average_rating,
+            clean_power,
+            average_rating,
             count_of_ratings
     ):
         self.distance_from_city_center = distance_from_city_center
