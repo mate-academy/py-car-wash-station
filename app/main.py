aec64d17ea52778e4c68c184dc822818b1fb0945
Vasyl Smutok <23.smutok@gmail.com>
2022-05-18 12:21:03 +0300

    fixed errors flake8

--- app/main.py
+++ app/main.py
@@ -6,7 +6,12 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: int, clean_power: int, average_rating: float, count_of_ratings: int):
+    def __init__(
+            self,
+            distance_from_city_center: int,
+            clean_power: int, average_rating: float,
+            count_of_ratings: int
+    ):
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = round(average_rating, 1)
@@ -21,10 +26,9 @@ class CarWashStation:
         return result
 
     def calculate_washing_price(self, car: Car):
-        return round(
-            car.comfort_class * (
-                        self.clean_power - car.clean_mark) * self.average_rating / self.distance_from_city_center, 1
-        )
+        res = (car.comfort_class * (self.clean_power - car.clean_mark)) * \
+              (self.average_rating / self.distance_from_city_center)
+        return round(res, 1)
 
     def wash_single_car(self, car):
         if car.clean_mark < self.clean_power:
@@ -34,4 +38,3 @@ class CarWashStation:
         all_ratings = self.average_rating * self.count_of_ratings + points
         self.count_of_ratings += 1
         self.average_rating = round((all_ratings / self.count_of_ratings), 1)
-
