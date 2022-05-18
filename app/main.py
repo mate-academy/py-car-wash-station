db841af95064c300c41e727174aab4020ff86519
bohdan <bodyan1108@gmail.com>
2022-05-18 09:36:09 +0300

    added check that clean_mark < clean_power

--- app/main.py
+++ app/main.py
@@ -33,7 +33,8 @@ class CarWashStation:
             ) * self.average_rating / self.distance_from_city_center, 1)
 
     def wash_single_car(self, car: Car):
-        car.clean_mark = self.clean_power
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
 
     def rate_service(self, rate):
         self.average_rating = round(
