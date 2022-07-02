5c5e13ea70354fface5340df11f20943837576ed
Anastasiia Shevchenko <anastasiia.sshevchenko@gmail.com>
2022-07-02 18:12:35 +0300

    MYSolution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self, distance_from_city_center: int, clean_power: int, average_rating: float, count_of_ratings: int):
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self):
+
+    def wash_single_car(self):
+
+    def rate_service(self):
