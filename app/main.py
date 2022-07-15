15f3d01bb6031ff0c3205693f41a36db640c572b
yaroslav-demchenko <dastex1411@gmail.com>
2022-07-15 17:29:53 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ class CarWashStation:
         for car in cars:
             if car.clean_mark < self.clean_power:
                 res += self.calculate_washing_price(car)
-                car.clean_mark = self.clean_power
+                self.wash_single_car(car)
         return res
 
     def calculate_washing_price(self, car):
