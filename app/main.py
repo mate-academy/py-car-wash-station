726d0f6fda29948a24217bc26155822ccaa9cf1e
AnDy <ansmbox@gmail.com>
2022-07-25 18:16:44 +0300

    fix flake8 warnings

--- app/main.py
+++ app/main.py
@@ -23,8 +23,9 @@ class CarWashStation:
         return income
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round(car.comfort_class * (self.clean_power - car.clean_mark) *
-                     self.average_rating / self.distance_from_city_center, 1)
+        clean_diff = self.clean_power - car.clean_mark
+        convenience = self.average_rating / self.distance_from_city_center
+        return round(car.comfort_class * clean_diff * convenience, 1)
 
     def wash_single_car(self, car: Car):
         if car.clean_mark < self.clean_power:
