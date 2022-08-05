910e9dabda6690b8c14df94e3c05626b1640ebee
KevinXD <kevinfdevid@gmail.com>
2022-08-05 11:47:30 +0300

    cleaned from  comments

--- app/main.py
+++ app/main.py
@@ -27,10 +27,7 @@ class CarWashStation:
         return round(income, 1)
 
     def calculate_washing_price(self, car):
-        # difference of car's power and car's clean mark
         difference = self.clean_power - car.clean_mark
-        # multiplication of car's comfort class, difference of car's power,
-        # car's clean mark and average rating
         numerator = car.comfort_class * difference * self.average_rating
         income = round(numerator / self.distance_from_city_center, 1)
         return income
@@ -41,7 +38,5 @@ class CarWashStation:
     def rate_service(self, rate: float):
         old_count_of_ratings = self.count_of_ratings
         self.count_of_ratings += 1
-        # multiplication of old average rating, old count of ratings
-        # plus current rate
         numerator = (self.average_rating * old_count_of_ratings) + rate
         self.average_rating = round(numerator / self.count_of_ratings, 1)
