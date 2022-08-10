05bef0850d31dc7c84bf66d05ee96a14b1bae090
Diliaver Yusupov <diliaver795@gmail.com>
2022-08-10 13:47:30 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -22,22 +22,20 @@ class CarWashStation:
         return income
 
     def calculate_washing_price(self, car):
-        price = round(((car.comfort_class * (self.clean_power_clean_mark - car.
-                                             clean_mark) * self.
-                        average_rating) / self.
-                       distance_from_city_center), 1)
+        local_variable1 = self.clean_power_clean_mark - car.clean_mark
+        local_variable2 = car.comfort_class * self.average_rating
+        local_variable3 = local_variable2 * local_variable1
+        local_variable4 = local_variable3 / self.distance_from_city_center
+        price = round(local_variable4, 1)
         return price
 
     def wash_single_car(self, cars):
         for car in cars:
             if car.clean_mark < self.clean_power_clean_mark:
-                clean_mark = self.clean_power_clean_mark
-        return clean_mark
+                car.clean_mark = self.clean_power_clean_mark
 
     def rate_service(self, number):
-        self.average_rating = \
-            round(((self.average_rating * self.
-                    count_of_ratings) + number) / (self.
-                                                   count_of_ratings + 1), 1)
+        local_variable1 = self.average_rating * self.count_of_ratings + number
+        local_variable2 = local_variable1 / (self.count_of_ratings + 1)
+        self.average_rating = round(local_variable2, 1)
         self.count_of_ratings = self.count_of_ratings + 1
-        return self.average_rating
