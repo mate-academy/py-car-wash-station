a840ca81e5352a1830501446e14cd526b2d7690d
Diliaver Yusupov <diliaver795@gmail.com>
2022-08-11 10:19:26 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -6,31 +6,31 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center, clean_power_clean_mark,
+    def __init__(self, distance_from_city_center, clean_power,
                  average_rating, count_of_ratings):
         self.distance_from_city_center = distance_from_city_center
-        self.clean_power_clean_mark = clean_power_clean_mark
+        self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, cars):
         wash_single_car = 0
         for car in cars:
-            if car.clean_mark < self.clean_power_clean_mark:
+            if car.clean_mark < self.clean_power:
                 wash_single_car += self.calculate_washing_price(car)
-                car.clean_mark = self.clean_power_clean_mark
+                car.clean_mark = self.clean_power
         return wash_single_car
 
     def calculate_washing_price(self, car):
-        dif_ce = self.clean_power_clean_mark - car.clean_mark
+        dif_ce = self.clean_power - car.clean_mark
         class_rate = car.comfort_class * self.average_rating
         price = round(class_rate * dif_ce / self.distance_from_city_center, 1)
         return price
 
     def wash_single_car(self, cars):
         for car in cars:
-            if car.clean_mark < self.clean_power_clean_mark:
-                car.clean_mark = self.clean_power_clean_mark
+            if car.clean_mark < self.clean_power:
+                car.clean_mark = self.clean_power
 
     def rate_service(self, number):
         all_marks = self.average_rating * self.count_of_ratings + number
