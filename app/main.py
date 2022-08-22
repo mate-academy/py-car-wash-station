26d4f8162c6ec419801bb4a7450d50bf43ef5c5c
Oleksandr Tkach <110629074+wanmitsuha@users.noreply.github.com>
2022-08-22 22:12:43 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -18,26 +18,25 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, car_list):
-        cars = [
-            auto
-            for auto in car_list
-            if auto.clean_mark <= self.clean_power
-        ]
-        income = sum([self.calculate_washing_price(car) for car in cars])
-        for c in cars:
-            c.clean_mark = self.clean_power
-        return income
+    def serve_cars(self, car_list: list):
+        return sum([
+            self.wash_single_car(car)
+            for car in car_list
+        ])
 
-    def calculate_washing_price(self, auto: Car):
-        if auto.clean_mark <= self.clean_power:
-            rate_diff = self.clean_power - auto.clean_mark
-            coff_rates = auto.comfort_class * rate_diff * self.average_rating
-            price_for_one = (coff_rates / self.distance_from_city_center)
-            return round(price_for_one, 1)
+    def calculate_washing_price(self, car: Car):
+        rate_diff = self.clean_power - car.clean_mark
+        coff_rates = car.comfort_class * rate_diff * self.average_rating
+        price_for_one = (coff_rates / self.distance_from_city_center)
+        return round(price_for_one, 1)
 
-    def wash_single_car(self, auto: Car):
-        return True if auto.clean_mark <= self.clean_power else False
+    def wash_single_car(self, car: Car):
+        if self.clean_power > car.clean_mark:
+            price = self.calculate_washing_price(car)
+            car.clean_mark = self.clean_power
+            return price
+        else:
+            return 0
 
     def rate_service(self, rate: int):
         all_rates = (self.average_rating * self.count_of_ratings + rate)
@@ -45,4 +44,3 @@ class CarWashStation:
             (all_rates / (self.count_of_ratings + 1)), 1
         )
         self.count_of_ratings = (self.count_of_ratings + 1)
-        return
