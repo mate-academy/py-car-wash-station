fa57f66dfcff60ae19f37e2ac99572a067d7a655
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2022-08-23 15:00:27 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -22,15 +22,15 @@ class CarWashStation:
         return sum_price
 
     def calculate_washing_price(self, car: Car):
-        return round(car.comfort_class * (self.clean_power - car.clean_mark)
-                     * self.average_rating / self.distance_from_city_center, 1)
+        dif_clean = self.clean_power - car.clean_mark
+        div_rating_dist = self.average_rating / self.distance_from_city_center
+        return round(car.comfort_class * dif_clean * div_rating_dist, 1)
 
     def wash_single_car(self, car: Car):
         if car.clean_mark <= self.clean_power:
             car.clean_mark = self.clean_power
 
     def rate_service(self, rate: int):
-        self.average_rating = \
-            round((self.average_rating * self.count_of_ratings + rate)
-                  / (self.count_of_ratings + 1), 1)
+        rating = self.average_rating * self.count_of_ratings + rate
+        self.average_rating = round(rating / (self.count_of_ratings + 1), 1)
         self.count_of_ratings += 1
