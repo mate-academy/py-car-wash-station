372b3709b4f6198525a30a8e5e8593ca156145cf
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-09-05 19:00:55 +0300

    start

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 class Car:
-    # write your code here
-    pass
+    
+    def __init__(self, 
+                 comfort_class, 
+                 clean_mark,
+                 brand):
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
+
+
 
 
 class CarWashStation:
