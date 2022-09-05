588cbfb6d648d19d1170ee12940db1639a1b0a2c
Nazar Dmytrenko <nazarkadm@gmail.com>
2022-09-05 13:34:40 +0300

    Fixed Solution

--- app/main.py
+++ app/main.py
@@ -32,7 +32,7 @@ class CarWashStation:
         for car in cars:
             if car.clean_mark < self.clean_power:
                 res += self.calculate_washing_price(car)
-                car.clean_mark = self.clean_power
+                self.wash_single_car(car)
         return res
 
     def rate_service(self, rate):
