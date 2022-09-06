43c9a0497738eedc380e579826db466044d6d9f9
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-09-06 20:48:09 +0300

    sol6

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-class Car:    
+class Car:
+
     def __init__(self, 
                  comfort_class, 
                  clean_mark,
@@ -8,13 +9,13 @@ class Car:
         self.brand = brand
 
 
-class CarWashStation:    
+class CarWashStation:
+
     def __init__(self,
                  distance_from_city_center,
                  clean_power,
                  average_rating,
                  count_of_ratings):
-
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -25,16 +26,19 @@ class CarWashStation:
         for car in car_list:
             if car.clean_mark < self.clean_power:
                 self.wash_single_car(car)
+
         return self.income                
 
     def calculate_washing_price(self, car):
         wash_price = car.comfort_class*(
             self.clean_power - car.clean_mark)
+
         return round(wash_price, 1)
 
     def wash_single_car(self, car):
         self.income = self.calculate_washing_price + self.income
         car.clean_mark = self.clean_power
+
         return self.income
 
     def rate_service(self, mark):
@@ -44,4 +48,5 @@ class CarWashStation:
             round(new_sum_of_rating / (self.count_of_ratings + 1),
                 1)
         self.count_of_ratings += 1
+        
         return self.average_rating
