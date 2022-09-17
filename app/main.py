aef6d171fab078c8a035a7b1fb01459c9481d9f3
Yaroslav Stetsko <yar.stetsko@gmail.com>
2022-09-17 15:55:51 +0300

    Added type annotation

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 class Car:
-    def __init__(self, comfort_class, clean_mark, brand):
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -7,16 +7,16 @@ class Car:
 
 class CarWashStation:
     def __init__(self,
-                 distance_from_city_center,
-                 clean_power,
-                 average_rating,
-                 count_of_ratings):
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int):
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, car_list):
+    def serve_cars(self, car_list: list):
         income = 0
         for car in car_list:
             if car.clean_mark < self.clean_power:
@@ -24,15 +24,15 @@ class CarWashStation:
                 self.wash_single_car(car)
         return income
 
-    def calculate_washing_price(self, car):
+    def calculate_washing_price(self, car: Car):
         price = car.comfort_class * (self.clean_power - car.clean_mark) * \
             (self.average_rating / self.distance_from_city_center)
         return round(price, 1)
 
-    def wash_single_car(self, car):
+    def wash_single_car(self, car: Car):
         car.clean_mark = self.clean_power
 
-    def rate_service(self, rating):
+    def rate_service(self, rating: float):
         total_rating = self.average_rating * self.count_of_ratings + rating
         self.count_of_ratings += 1
         self.average_rating = round(total_rating / self.count_of_ratings, 1)
