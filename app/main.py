6ae2be5a4c14e563efa25f42ca6e7813a6b478af
Ivan Berezyanskiy <65457374+Vanya2389@users.noreply.github.com>
2022-09-18 10:48:38 +0300

    Flake8

--- app/main.py
+++ app/main.py
@@ -27,9 +27,9 @@ class CarWashStation:
 
     def calculate_washing_price(self, car):
         if car.clean_mark < self.clean_power:
-            price_comfort_and_clean = car.comfort_class * (self.clean_power - car.clean_mark)
-            price_distance_and_rating = self.average_rating / self.distance_from_city_center
-            income1 = price_comfort_and_clean * price_distance_and_rating
+            price_comfort_clean = car.comfort_class * (self.clean_power - car.clean_mark)
+            price_distance_rating = self.average_rating / self.distance_from_city_center
+            income1 = price_comfort_clean * price_distance_rating
             return round(income1, 1)
         return 0
 
