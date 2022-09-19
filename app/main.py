7048738b685e26494666801931e9f507eaf0fe61
Dmytro Skrypka <2346677tyu@gmail.com>
2022-09-19 16:46:38 +0300

    Solution add new annotations #3

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ class CarWashStation:
                 self.wash_single_car(car)
         return round(count, 1)
 
-    def calculate_washing_price(self, cars: Car) -> None:
+    def calculate_washing_price(self, cars: Car) -> float:
         calculate = cars.comfort_class * (self.clean_power - cars.clean_mark)
         return round(
             calculate * self.average_rating / self.distance_from_city_center, 1
