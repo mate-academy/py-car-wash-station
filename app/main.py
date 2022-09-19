188e12f5310c8f8affebd5a4477e53ce97dc3b81
Dmytro Skrypka <2346677tyu@gmail.com>
2022-09-19 15:52:07 +0300

    Solution add new annotations

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars):
+    def serve_cars(self, cars: int):
         count = 0
 
         for car in cars:
@@ -24,13 +24,13 @@ class CarWashStation:
                 self.wash_single_car(car)
         return round(count, 1)
 
-    def calculate_washing_price(self, cars):
+    def calculate_washing_price(self, cars: int):
         calculate = cars.comfort_class * (self.clean_power - cars.clean_mark)
         return round(
             calculate * self.average_rating / self.distance_from_city_center, 1
         )
 
-    def wash_single_car(self, cars):
+    def wash_single_car(self, cars: int):
         if cars.clean_mark < self.clean_power:
             cars.clean_mark = self.clean_power
 
