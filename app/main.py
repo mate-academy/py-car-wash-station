4eed30fd6177ab0b12c7a0fbf96018b34db1b2ca
graandda <graanddadays@gmail.com>
2022-09-19 19:46:35 +0300

    Solution - with edits

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 class Car:
-    def __init__(self, comfort_class, clean_mark, brand):
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -7,34 +7,34 @@ class Car:
 
 class CarWashStation:
     def __init__(self,
-                 distance_from_city_center,
-                 clean_power,
-                 average_rating,
-                 count_of_ratings):
+                 distance_from_city_center: int,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int):
 
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars_list):
+    def serve_cars(self, cars: list):
         count = 0
-        for car in cars_list:
+        for car in cars:
             if car.clean_mark < self.clean_power:
                 count += self.calculate_washing_price(car)
                 self.wash_single_car(car)
         return round(count, 1)
 
-    def calculate_washing_price(self, car):
+    def calculate_washing_price(self, car: Car):
         washing_effort = self.clean_power - car.clean_mark
         washing_rating = self.average_rating / self.distance_from_city_center
         return round(car.comfort_class * washing_effort * washing_rating, 1)
 
-    def wash_single_car(self, car):
+    def wash_single_car(self, car: Car):
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
-    def rate_service(self, stars):
+    def rate_service(self, stars: int):
         marks = self.average_rating * self.count_of_ratings
         self.count_of_ratings += 1
         self.average_rating = round((stars + marks) / self.count_of_ratings, 1)
