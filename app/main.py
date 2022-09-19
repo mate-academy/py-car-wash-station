4472330fbb0db30cc6f8b177dc138a5cafe47728
HStakhiv <gsstahiv@gmail.com>
2022-09-19 16:20:58 +0300

    Fixed

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list):
+    def serve_cars(self, cars: list) -> float:
         income = 0
 
         for car in cars:
@@ -28,7 +28,7 @@ class CarWashStation:
 
         return round(income, 1)
 
-    def calculate_washing_price(self, car: Car):
+    def calculate_washing_price(self, car: Car) -> float:
         wash_cost = car.comfort_class * \
             (self.clean_power - car.clean_mark) * \
             self.average_rating / self.distance_from_city_center
