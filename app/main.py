f578d37b1f89016490b69f28d3595663c40e68e5
Dmytro Petliovanyi <petliovany@gmail.com>
2022-09-19 20:17:32 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,43 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(
+            self,
+            distance_from_city_center,
+            clean_power,
+            average_rating,
+            count_of_ratings
+    ):
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def calculate_washing_price(self, car):
+        work = (self.clean_power - car.clean_mark)
+        rating_dist = self.average_rating / self.distance_from_city_center
+        price = car.comfort_class * work * rating_dist
+        return round(price, 1)
+
+    def wash_single_car(self, car):
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
+
+    def rate_service(self, rate):
+        rates = self.average_rating * self.count_of_ratings + rate
+        count_rates = self.count_of_ratings + 1
+        self.average_rating = round(rates / count_rates, 1)
+        self.count_of_ratings += 1
+
+    def serve_cars(self, cars: list):
+        income = 0
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
+        return income
