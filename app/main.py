6ae3daf8d7ef446be1b1dffe4a029c2a5067e2a2
Ivan Berezyanskiy <65457374+Vanya2389@users.noreply.github.com>
2022-09-19 14:20:23 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -38,6 +38,6 @@ class CarWashStation:
     def rate_service(self, rate):
         total_rate = self.count_of_ratings / (self.count_of_ratings + 1
                                               ) * self.average_rating
-        how_many_gives_one_rate = 1 / (self.count_of_ratings + 1) * rate
-        self.average_rating = round((total_rate + how_many_gives_one_rate), 1)
+        how_many_give_rate = 1 / (self.count_of_ratings + 1) * rate
+        self.average_rating = round((total_rate + how_many_give_rate), 1)
         self.count_of_ratings += 1
