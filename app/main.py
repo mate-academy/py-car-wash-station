c585e2745b076a7947ec9e12e8e7ffd35aa80dd6
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-09-19 12:24:38 +0300

    Used the method for washing a car, deleted unnecessary calculations and added, added annotations everywhere

--- app/main.py
+++ app/main.py
@@ -6,33 +6,31 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power,
-                 average_rating, count_of_ratings):
+    def __init__(self, distance_from_city_center: float, clean_power: int,
+                 average_rating: float, count_of_ratings: int):
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list):
+    def serve_cars(self, cars: list) -> float:
         income = 0
         for car in cars:
-            if self.clean_power >= car.clean_mark:
+            if self.clean_power > car.clean_mark:
                 income += self.calculate_washing_price(car)
-                car.clean_mark = self.clean_power
-        return income
+                self.wash_single_car(car)
+        return round(income, 1)
 
-    def calculate_washing_price(self, car):
+    def calculate_washing_price(self, car: Car) -> float:
         price = car.comfort_class * (self.clean_power - car.clean_mark) \
             * self.average_rating / self.distance_from_city_center
         return round(price, 1)
 
-    def wash_single_car(self, car):
+    def wash_single_car(self, car: Car) -> None:
         if self.clean_power >= car.clean_mark:
-            price = self.calculate_washing_price(car)
             car.clean_mark = self.clean_power
-            return price
 
-    def rate_service(self, rating):
+    def rate_service(self, rating: int) -> None:
         self.average_rating = round(
             (self.average_rating * self.count_of_ratings + rating)
             / (self.count_of_ratings + 1), 1
