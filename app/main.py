7d4a18670a63c89296e1d732b2916d7d6dcf52be
VitaliiPiaskivskyi <Vitaliipiaskivskyi@gmail.com>
2022-09-19 22:30:17 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ class CarWashStation:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
-    def rate_service(self, new_rate: int) -> None:
-        self.average_rating = round(((self.average_rating * self.count_of_ratings) + new_rate)
+    def rate_service(self, new_rate_car: int) -> None:
+        self.average_rating = round(((self.average_rating * self.count_of_ratings) + new_rate_car)
                                     / (self.count_of_ratings + 1), 1)
         self.count_of_ratings += 1
