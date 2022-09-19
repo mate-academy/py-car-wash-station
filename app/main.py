07efc20d4c4e9b124c7ebc569b5f79df2ef9f1f6
Nikyxa <kolisova.veronika81@gmail.com>
2022-09-19 21:16:50 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -6,14 +6,19 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center, clean_power, average_rating, count_of_ratings):
+    def __init__(self,
+                 distance_from_city_center,
+                 clean_power,
+                 average_rating,
+                 count_of_ratings):
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
     def calculate_washing_price(self, car):
-        price = car.comfort_class * (self.clean_power - car.clean_mark)
+        price = self.clean_power - car.clean_mark
+        price *= car.comfort_class
         price *= self.average_rating / self.distance_from_city_center
         return round(price, 1)
 
