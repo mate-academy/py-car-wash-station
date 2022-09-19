6da4c7617126a4b33e321639cb639b88df1f0174
Sergii Franchuk <Franchuksergei@gmail.com>
2022-09-19 13:27:26 +0300

    second try

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list[Car]):
+    def serve_cars(self, cars: list[Car]) -> float:
         price = 0
         for car in cars:
             if self.clean_power > car.clean_mark:
@@ -25,17 +25,18 @@ class CarWashStation:
                 self.wash_single_car(car)
         return price
 
-    def calculate_washing_price(self, car: Car):
+    def calculate_washing_price(self, car: Car) -> float:
         return round(
             car.comfort_class
             * (self.clean_power - car.clean_mark)
             * self.average_rating / self.distance_from_city_center,
             1)
 
-    def wash_single_car(self, car: Car):
-        car.clean_mark = self.clean_power
+    def wash_single_car(self, car: Car) -> None:
+        if self.clean_power > car.clean_mark:
+            car.clean_mark = self.clean_power
 
-    def rate_service(self, rating: int):
+    def rate_service(self, rating: int) -> None:
         self.average_rating = round(
             (self.average_rating * self.count_of_ratings + rating)
             / (self.count_of_ratings + 1),
