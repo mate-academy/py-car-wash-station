e6fe2bfd737efd3fa4a093fa06438115bf80c413
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-09-20 16:46:00 +0300

    ÐDONE with annotations

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list):
+    def serve_cars(self, cars: list) -> float:
         income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
@@ -27,18 +27,18 @@ class CarWashStation:
                 self.wash_single_car(car)
         return income
 
-    def calculate_washing_price(self, car: Car):
+    def calculate_washing_price(self, car: Car) -> float:
         difference = abs(self.clean_power - car.clean_mark)
         rate_div_dist = self.average_rating / self.distance_from_city_center
 
         return round((car.comfort_class * difference * rate_div_dist), 1)
 
-    def wash_single_car(self, car: Car):
+    def wash_single_car(self, car: Car) -> int:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
         return car.clean_mark
 
-    def rate_service(self, rating: int):
+    def rate_service(self, rating: int) -> None:
         rate_var = (self.average_rating * self.count_of_ratings) + rating
         self.count_of_ratings += 1
         self.average_rating = round((rate_var / self.count_of_ratings), 1)
