ecdff673ce2b8dddbe28391a2e78798cbf647c98
ilya <druzilya@ukr.net>
2022-09-20 14:58:58 +0300

    'Solution3'

--- app/main.py
+++ app/main.py
@@ -7,7 +7,8 @@ class Car:
 
 class CarWashStation:
 
-    def __init__(self, distance_from_city_center: float,
+    def __init__(self,
+                 distance_from_city_center: float,
                  clean_power: int,
                  average_rating: float,
                  count_of_ratings: int):
