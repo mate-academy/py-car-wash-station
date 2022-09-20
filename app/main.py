61117005aeab52797869c9d934543001fe4e693c
Mazur Mariya <mazurinka29@gmail.com>
2022-09-20 10:11:11 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -15,27 +15,23 @@ class CarWashStation:
 
     def serve_cars(self, cars: list):
         income_new = 0
-        for brand in cars:
-            if brand.clean_mark <= self.clean_power:
-                income_new += self.calculate_washing_price(brand)
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                income_new += self.calculate_washing_price(car)
+                self.wash_single_car(car)
         return round(income_new, 1)
 
-    def calculate_washing_price(self, brand):
-        income = (brand.comfort_class
-                  * (self.clean_power - brand.clean_mark)
+    def calculate_washing_price(self, car):
+        income = (car.comfort_class
+                  * (self.clean_power - car.clean_mark)
                   * self.average_rating
                   / self.distance_from_city_center)
         return round(income, 1)
 
-    def wash_single_car(self, cars: list):
-        new_clean_cars = []
-        for brand in cars:
-            if brand.clean_mark < self.clean_power:
-                new_clean_cars.append(self.clean_power)
-            else:
-                brand.clean_mark = brand.clean_mark
-                new_clean_cars.append(brand.clean_mark)
-        return new_clean_cars
+    def wash_single_car(self, car):
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
+            return car.clean_mark
 
     def rate_service(self, rate: int):
         new_average_rating = ((self.average_rating
@@ -44,4 +40,3 @@ class CarWashStation:
                               / (self.count_of_ratings + 1))
         self.average_rating = round(new_average_rating, 1)
         self.count_of_ratings = self.count_of_ratings + 1
-        return self.average_rating, self.count_of_ratings
