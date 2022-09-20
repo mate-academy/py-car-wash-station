4aaa60b9ccb0643516822ae0342bbcf6a6cd1160
Dima Ovcharenko <112913633+OvDima1@users.noreply.github.com>
2022-09-20 17:13:32 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 class Car:
-    def __init__(self, comfort_class, clean_mark, brand):
+    def __init__(self,
+                comfort_class: int,
+                clean_mark: int,
+                brand: str
+                ):
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -8,17 +12,17 @@ class Car:
 class CarWashStation:
     def __init__(
             self,
-            distance_from_city_center,
-            clean_power,
-            average_rating,
-            count_of_ratings
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
     ):
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars):
+    def serve_cars(self, cars: list) -> float:
         income = 0
         for car in cars:
             if self.clean_power > car.clean_mark:
@@ -27,18 +31,18 @@ class CarWashStation:
 
         return income
 
-    def calculate_washing_price(self, car):
+    def calculate_washing_price(self, car: Car) -> float:
         washing_volume = self.clean_power - car.clean_mark
         rating_with_dist = self.average_rating / self.distance_from_city_center
         washing_price = car.comfort_class * washing_volume * rating_with_dist
 
         return round(washing_price, 1)
 
-    def wash_single_car(self, car):
+    def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
-    def rate_service(self, mark):
+    def rate_service(self, mark: float) -> None:
         all_marks = self.count_of_ratings * self.average_rating
 
         all_marks += mark
