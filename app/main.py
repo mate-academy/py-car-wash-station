67757d0a11e1e596bfe4210e81e11e82b10f7a8c
Dima Ovcharenko <112913633+OvDima1@users.noreply.github.com>
2022-09-20 17:35:31 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 class Car:
     def __init__(self,
-                comfort_class: int,
-                clean_mark: int,
-                brand: str
-                ):
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str
+                 ):
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
