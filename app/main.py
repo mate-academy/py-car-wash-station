fb21c66660717f92f18122945dab736924feb0c0
Vitalii Bychevyi <bychevoyvitaliy@gmail.com>
2022-09-22 12:18:27 +0300

    bracket was canseled

--- app/main.py
+++ app/main.py
@@ -13,7 +13,8 @@ class CarWashStation:
             distance_from_city_center: float,
             clean_power: int,
             average_rating: float,
-            count_of_ratings: int):
+            count_of_ratings: int
+    ):
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
