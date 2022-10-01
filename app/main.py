eff40ec4eb41dd4b47bbdeab837a8153ec7c637a
Roman Pisarskiy <darkvader.ukr.net@gmail.com>
2022-10-01 13:46:57 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 class Car:
-    # write your code here
-    pass
+    def __int__(self, comfort_class, clean_mark, brand):
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __int__(self, distance_from_city_center, clean_power, average_rating, count_of_ratings):
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_mark
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self):
+
