89862d44beed42d4fa6003e09ab96422a8c8c0fa
Roman Pisarskiy <darkvader.ukr.net@gmail.com>
2022-10-02 15:05:01 +0300

    Solution1

--- app/main.py
+++ app/main.py
@@ -8,9 +8,19 @@ class Car:
 class CarWashStation:
     def __int__(self, distance_from_city_center, clean_power, average_rating, count_of_ratings):
         self.distance_from_city_center = distance_from_city_center
-        self.clean_power = clean_mark
+        self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self):
+        pass
+    
+    def calculate_washing_price(self):
+        pass
+
+    def wash_single_car(self):
+        pass
+
+    def rate_service(self):
+        pass
 
