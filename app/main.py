b7e2c86cfe369497f82e1a4edaaf211b528b172a
Dmytro Bleichyk <dima.badiuk@gmail.com>
2022-10-03 14:15:41 +0300

    added type annotation

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 class Car:
-    def __init__(self, comfort_class, clean_mark, brand):
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -8,17 +13,17 @@ class Car:
 class CarWashStation:
     def __init__(
             self,
-            distance_from_city_center,
-            clean_power,
-            average_rating,
-            count_of_ratings,
-    ):
+            distance_from_city_center: int,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int,
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, list_of_cars):
+    def serve_cars(self, list_of_cars: list) -> float:
         income = 0
         for car in list_of_cars:
             if self.calculate_washing_price(car) > 0:
@@ -26,18 +31,18 @@ class CarWashStation:
                 self.wash_single_car(car)
         return income
 
-    def calculate_washing_price(self, car):
+    def calculate_washing_price(self, car: Car) -> float:
         to_wash = self.clean_power - car.clean_mark
         cost = car.comfort_class * to_wash * self.average_rating
         return round(
             cost / self.distance_from_city_center, 1
         )
 
-    def wash_single_car(self, car):
+    def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
-    def rate_service(self, mark):
+    def rate_service(self, mark: int) -> None:
         sum_of_marks = self.average_rating * self.count_of_ratings + mark
         self.count_of_ratings += 1
         self.average_rating = round(sum_of_marks / self.count_of_ratings, 1)
