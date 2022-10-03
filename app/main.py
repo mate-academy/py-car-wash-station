23b48306eee343f2f506fc18b97e3c91b843a8ac
Roman Pisarskiy <darkvader.ukr.net@gmail.com>
2022-10-03 18:22:43 +0300

    Solution_v2

--- app/main.py
+++ app/main.py
@@ -12,15 +12,36 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self):
-        pass
+    def serve_cars(self, list_of_car: list):
+        result = 0
+        for car in list_of_car:
+            if car.clean_mark < self.clean_power:
+                result += self.calculate_washing_price(car)
+        return result
     
-    def calculate_washing_price(self):
-        pass
+    def calculate_washing_price(self, car: Car):
+        result = (
+                car.comfort_class
+                * (self.clean_power - car.clean_mark)
+                * self.average_rating
+                / self.distance_from_city_center
+        )
+        return round(result, 1)
+
 
     def wash_single_car(self):
-        pass
+        """
+        - метод, который моет одну машину,
+         поэтому у него clean_mark должен быть равен clean_power мойки,
+         если wash_station.clean_power больше car.clean_mark;
+        :return:
+        """
 
     def rate_service(self):
-        pass
+        """
+        метод, добавляющий одну ставку к станции мойки,
+         и на основе этой одной ставки следует изменить medium_rating и count_of_ratings:
+        :return:
+        """
+        return
 
