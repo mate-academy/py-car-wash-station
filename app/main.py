dd88fc26148b5cae8c851a1803d2aa8b5137f8e2
6aPa6aH <6apa6ah666@gmail.com>
2022-10-26 10:05:38 +0300

    fixed_type_annotation

--- app/main.py
+++ app/main.py
@@ -11,20 +11,20 @@ class Car:
 class CarWashStation:
     def __init__(self,
                  distance_from_city_center: int,
-                 clean_power: int, average_rating: int,
+                 clean_power: int, average_rating: float,
                  count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list) -> int:
+    def serve_cars(self, cars: list) -> float:
         cash = 0
         for car in cars:
             cash += self.wash_single_car(car)
         return round(cash, 1)
 
-    def calculate_washing_price(self, car: object) -> int:
+    def calculate_washing_price(self, car: Car) -> float:
         cc = car.comfort_class
         cp = self.clean_power
         cm = car.clean_mark
@@ -33,14 +33,14 @@ class CarWashStation:
         price = cc * (cp - cm) * ar / dfcc
         return price
 
-    def wash_single_car(self, car: object) -> int:
+    def wash_single_car(self, car: Car) -> float:
         cash = 0
         if car.clean_mark < self.clean_power:
             cash += self.calculate_washing_price(car)
             car.clean_mark = self.clean_power
         return round(cash, 1)
 
-    def rate_service(self, rating: int) -> None:
+    def rate_service(self, rating: float) -> None:
         self.average_rating = round(
             (self.average_rating * self.count_of_ratings + rating)
             / (self.count_of_ratings + 1), 1)
