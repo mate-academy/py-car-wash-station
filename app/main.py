4d4c416ee7cd3d0638399383c802bb72d0989c38
Vladimir Titarenko <tytarenkova@gmail.com>
2022-10-28 17:14:05 +0300

    the changes was applied

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, list_of_cars: list) -> float:
+    def serve_cars(self, list_of_cars: list[Car]) -> float:
         income = 0
         for car in list_of_cars:
             if car.clean_mark < self.clean_power:
@@ -23,12 +23,11 @@ class CarWashStation:
         return income
 
     def calculate_washing_price(self, car: Car) -> float:
-        if car.clean_mark < self.clean_power:
-            return round(
-                car.comfort_class
-                * (self.clean_power - car.clean_mark)
-                * (self.average_rating / self.distance_from_city_center), 1
-            )
+        return round(
+            car.comfort_class
+            * (self.clean_power - car.clean_mark)
+            * (self.average_rating / self.distance_from_city_center), 1
+        )
 
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
