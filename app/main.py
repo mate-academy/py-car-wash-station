e1fa599c0579af07839a6fb137ed9ea0eb608ecd
Haishuk Pavlo <xomas@ukr.net>
2022-11-08 10:36:19 +0200

    Revert "done"
    This reverts commit 4433cfe72c8c03e2312dfc6be148418f920e2de0.

--- app/main.py
+++ app/main.py
@@ -1,54 +1,8 @@
 class Car:
-    def __init__(
-            self,
-            comfort_class: int,
-            clean_mark: int,
-            brand: str
-    ) -> None:
-        self.comfort_class = comfort_class
-        self.clean_mark = clean_mark
-        self.brand = brand
+    # write your code here
+    pass
 
 
 class CarWashStation:
-    def __init__(
-            self,
-            distance_from_city_center: float,
-            clean_power: int,
-            average_rating: float,
-            count_of_ratings: int
-    ) -> None:
-        self.distance_from_city_center = distance_from_city_center
-        self.clean_power = clean_power
-        self.average_rating = average_rating
-        self.count_of_ratings = count_of_ratings
-
-    def serve_cars(self, cars: list[Car]) -> int:
-        final_price = 0
-        for car in cars:
-            if car.clean_mark < self.clean_power:
-                final_price += self.calculate_washing_price(car)
-                self.wash_single_car(car)
-        return final_price
-
-    def calculate_washing_price(self, car: Car) -> float:
-        return round(
-            car.comfort_class * (
-                self.clean_power - car.clean_mark
-            ) * (
-                self.average_rating / self.distance_from_city_center
-            ), 1
-        )
-
-    def wash_single_car(self, car: Car) -> None:
-        if self.clean_power > car.clean_mark:
-            car.clean_mark = self.clean_power
-
-    def rate_service(self, new_rate: int) -> None:
-        self.average_rating = (
-            (
-                (self.average_rating * self.count_of_ratings) + new_rate
-            ) / (self.count_of_ratings + 1)
-        )
-        self.average_rating = round(self.average_rating, 1)
-        self.count_of_ratings += 1
+    # write your code here
+    pass
