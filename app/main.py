57dae4c15a4aa83c14f462b84c8dba7c45909511
Andrey Polyakov <polyakovv.andrey.01@gmail.com>
2022-11-14 12:34:07 +0200

    My_Car_WashStation

--- app/main.py
+++ app/main.py
@@ -1,8 +1,52 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+
+    def __init__(
+            self,
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
+    ) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def calculate_washing_price(self, car: Car) -> float:
+        if car.clean_mark < self.clean_power:
+            stp1 = car.comfort_class * (self.clean_power - car.clean_mark)
+            stp2 = stp1 * self.average_rating / self.distance_from_city_center
+            wash_cost = round(stp2, 1)
+            return wash_cost
+
+    def wash_single_car(self, car: Car) -> None:
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
+
+    def serve_cars(self, cars: list) -> float:
+        income = 0
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
+        return income
+
+    def rate_service(self, rate: float) -> None:
+        sum_of_rating = self.average_rating * self.count_of_ratings + rate
+        self.count_of_ratings += 1
+        self.average_rating = sum_of_rating / self.count_of_ratings
+        self.average_rating = round(self.average_rating, 1)
+        print(self.count_of_ratings)
+        print(self.average_rating)
