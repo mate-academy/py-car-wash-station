767d33824459a275057bf715815cadddcfe74b4a
Tetiana Kozakova <kleotan901@gmail.com>
2022-11-15 13:11:48 +0100

    solutionpytest

--- app/main.py
+++ app/main.py
@@ -20,12 +20,11 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def wash_single_car(self, car: str) -> list:
+    def wash_single_car(self, car: str) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
-        return car.clean_mark
 
-    def serve_cars(self, cars_list: list) -> float:
+    def serve_cars(self, cars_list: list[Car]) -> float:
         result_price_lst = []
         for car in cars_list:
             if car.clean_mark < self.clean_power:
@@ -34,15 +33,14 @@ class CarWashStation:
                 result_price_lst.append(price)
         return round(sum(result_price_lst), 1)
 
-    def calculate_washing_price(self, car: str) -> float:
+    def calculate_washing_price(self, car: Car) -> float:
         price_wash_car = car.comfort_class *\
             (self.clean_power - car.clean_mark) *\
             self.average_rating / self.distance_from_city_center
         return price_wash_car
 
-    def rate_service(self, rate: int) -> tuple:
+    def rate_service(self, rate: int) -> None:
         result_rate = ((self.average_rating * self.count_of_ratings)
                        + rate) / (self.count_of_ratings + 1)
         self.average_rating = round(result_rate, 1)
         self.count_of_ratings += 1
-        return self.average_rating, self.count_of_ratings
