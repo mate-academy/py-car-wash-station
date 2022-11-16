0b6dc96fadca0790fd03c193bc130b726e772721
Viachesal Shcherbak <misternictof@gmail.com>
2022-11-16 22:53:17 +0200

    Fixed type annotations

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> any:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -8,7 +13,7 @@ class Car:
 class CarWashStation:
     def __init__(
             self,
-            distance_from_city_center: int,
+            distance_from_city_center: float,
             clean_power: int,
             average_rating: float,
             count_of_ratings: int
@@ -18,7 +23,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         cost = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
@@ -26,12 +31,12 @@ class CarWashStation:
                 self.wash_single_car(car)
         return cost
 
-    def calculate_washing_price(self, car: any) -> float:
+    def calculate_washing_price(self, car: Car) -> float:
         dif = self.clean_power - car.clean_mark
         rating = self.average_rating / self.distance_from_city_center
         return round(car.comfort_class * dif * rating, 1)
 
-    def wash_single_car(self, car: object) -> None:
+    def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
