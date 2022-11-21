6c1ac76704b0dbdaadcc6501758166a1df8145de
Viacheslav Yasynetskyi <slawa1594@gmail.com>
2022-11-21 18:06:07 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -1,34 +1,41 @@
 class Car:
-    def __init__(self, comfort_class: int,
-                 clean_mark: int,
-                 brand: str
-                 ) -> None:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float,
-                 clean_power: int,
-                 average_rating: float,
-                 count_of_ratings: int) -> None:
+    def __init__(
+            self,
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, cars: list) -> float:
-        totall = 0
+        total = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
-                totall += self.calculate_washing_price(car)
+                total += self.calculate_washing_price(car)
                 self.wash_single_car(car)
-        return round(totall, 1)
+        return round(total, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round(car.comfort_class * (self.clean_power - car.clean_mark)
-                     * self.average_rating / self.distance_from_city_center, 1)
+        return round(car.comfort_class
+                     * (self.clean_power - car.clean_mark)
+                     * self.average_rating
+                     / self.distance_from_city_center, 1)
 
     def wash_single_car(self, cars: Car) -> None:
         if self.clean_power > cars.clean_mark:
