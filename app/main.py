e0764195e51af0a75bddd32b56033bc6ee295772
MaksymUK <clear>
2022-11-21 18:27:10 +0000

    Code style improved

--- app/main.py
+++ app/main.py
@@ -1,23 +1,27 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) \
-            -> None:
+    def __init__(self, comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int,
-                 average_rating: int, count_of_ratings: int) -> None:
+    def __init__(self, distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: int,
+                 count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
     def calculate_washing_price(self, car: str) -> float:
-        single_car_wash_price = (car.comfort_class) * \
-            (self.clean_power - car.clean_mark) * \
-            (self.average_rating / self.distance_from_city_center)
+        single_car_wash_price = (car.comfort_class
+                                 * (self.clean_power - car.clean_mark)
+                                 * (self.average_rating
+                                    / self.distance_from_city_center))
         return round(single_car_wash_price, 1)
 
     def wash_single_car(self, car: str) -> int:
@@ -37,5 +41,5 @@ class CarWashStation:
         sum_of_new_rating = self.average_rating * self.count_of_ratings + rate
         new_count_of_ratings = self.count_of_ratings + 1
         self.count_of_ratings = new_count_of_ratings
-        self.average_rating = round(sum_of_new_rating / new_count_of_ratings,
-                                    1)
+        self.average_rating = round(sum_of_new_rating
+                                    / new_count_of_ratings, 1)
