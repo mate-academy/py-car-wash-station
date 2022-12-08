39e8104e993a5a48d925d309387e320b52ab605c
Alina Boichenko <ilub1108@gmail.com>
2022-12-08 15:06:46 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ class Car:
             clean_mark: int,
             brand: str
     ) -> None:
+
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -15,6 +16,7 @@ class CarWashStation:
             clean_power: int, average_rating: float,
             count_of_ratings: int
     ) -> None:
+
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
