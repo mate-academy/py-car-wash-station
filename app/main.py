b18de18c844586a6ba058ae6778d9fd1d67ecb93
Sashkoooo <o.denysenko@hotmail.com>
2022-12-16 20:56:01 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -21,43 +26,27 @@ class CarWashStation:
     def serve_cars(self, list_of_cars: list) -> float:
         income = 0
         for car in list_of_cars:
-            print(car.brand)
             if car.clean_mark < self.clean_power:
                 income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
         return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        washing_price = \
-            car.comfort_class * (self.clean_power - car.clean_mark) * \
-            self.average_rating / self.distance_from_city_center
+        washing_price = (car.comfort_class
+                         * (self.clean_power
+                            - car.clean_mark)
+                         * self.average_rating
+                         / self.distance_from_city_center)
         return round(washing_price, 1)
 
-    def wash_single_car(self, car: Car):
-        if wash_station.clean_power > car.clean_mark:
+    def wash_single_car(self, car: Car) -> None:
+        if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
-
-    def rate_service(self):
-        pass
-
-
-bmw = Car(comfort_class=3, clean_mark=3, brand='BMW')
-audi = Car(comfort_class=4, clean_mark=9, brand='Audi')
-
-print(f"bmv clean mark: {bmw.clean_mark}")  # 3
-
-wash_station = CarWashStation(
-    distance_from_city_center=5,
-    clean_power=6,
-    average_rating=3.5,
-    count_of_ratings=6
-)
-print(wash_station.distance_from_city_center)
-# income = wash_station.serve_cars([bmw, audi])
-#
-# print(income)  # 6.3
-print(audi.clean_mark)  # 6
-
-
-
-
+    def rate_service(self, single_rate: float) -> None:
+        new_average_rating = ((self.count_of_ratings
+                              * self.average_rating
+                              + single_rate)
+                              / (self.count_of_ratings + 1))
+        self.average_rating = round(new_average_rating, 1)
+        self.count_of_ratings += 1
