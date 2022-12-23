e2173b70c1e96dd6ed9fb8655c704ec145bc5a91
Marharyta Sovpenko <margaretsovpenko@gmail.com>
2022-12-23 17:19:35 +0200

    Fix the variable

--- app/main.py
+++ app/main.py
@@ -27,11 +27,10 @@ class CarWashStation:
         return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        math = round(
+        return round(
             (car.comfort_class * (self.clean_power - car.clean_mark)
              * self.average_rating / self.distance_from_city_center),
             1)
-        return math
 
     def wash_single_car(self, car: Car) -> int:
         if car.clean_mark < self.clean_power:
