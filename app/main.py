55cd05ab0cde65b228985c89a15bfbe3944c34a5
Oleksandr Hryshchenko <oleksandr.grk@gmail.com>
2023-01-05 13:44:21 +0200

    separate arguments to differant lines

--- app/main.py
+++ app/main.py
@@ -1,6 +1,3 @@
-from typing import Any
-
-
 class Car:
     def __init__(self,
                  comfort_class: int,
@@ -12,14 +9,18 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int,
-                 average_rating: float, count_of_ratings: int) -> None:
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int
+                 ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: Any) -> int:
+    def serve_cars(self, cars: list) -> int:
         income = 0
         if isinstance(cars, list):
             for car in cars:
