ca196fb68e2e677f3d0e81e867a2b9b674d64b0d
Oleksandr Lysanets <oleksandrlysanets@gmail.com>
2023-01-05 12:04:38 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -34,4 +34,3 @@ class CarWashStation:
                                       * self.count_of_ratings) + single_rate)
                                     / (self.count_of_ratings + 1), 1)
         self.count_of_ratings += 1
-    pass
