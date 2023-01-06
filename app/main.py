e0d10dce5d8165669e0cfbb49c668ece5dc89a00
Dmytro Martyshevskiy <codingeo@gmail.com>
2023-01-06 10:25:08 +0200

    make wash_single_car method consistent

--- app/main.py
+++ app/main.py
@@ -22,19 +22,21 @@ class CarWashStation:
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, cars_list: list) -> float:
-        cars = [car for car in cars_list if car.clean_mark < self.clean_power]
         income = 0
-        for car in cars:
-            income += self.calculate_washing_price(car)
-            self.wash_single_car(car)
+        for car in cars_list:
+            income += self.wash_single_car(car)
         return income
 
     def calculate_washing_price(self, car: Car) -> float:
         return round(car.comfort_class * (self.clean_power - car.clean_mark)
                      * self.average_rating / self.distance_from_city_center, 1)
 
-    def wash_single_car(self, car: Car) -> None:
-        car.clean_mark = self.clean_power
+    def wash_single_car(self, car: Car) -> float:
+        if self.clean_power > car.clean_mark:
+            income = self.calculate_washing_price(car)
+            car.clean_mark = self.clean_power
+            return income
+        return 0
 
     def rate_service(self, rating: int) -> None:
         self.count_of_ratings += 1
