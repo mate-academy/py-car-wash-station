ed03b9233c34fab819f029e9270061714b485468
19 <117291303+numerodiciannove@users.noreply.github.com>
2023-01-18 12:42:05 +0200

    line 2 separated

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 class Car:
-    def __init__(self, comfort_class: int,
+    def __init__(self,
+                 comfort_class: int,
                  clean_mark: int,
                  brand: str) -> None:
         self.comfort_class = comfort_class
