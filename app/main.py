a51c24127540ace3c1ec9260efe22551a778124c
Ruslan Yanyshyn <ruslan.yanyshyn@yahoo.com>
2023-01-31 08:58:01 -0600

    fix indentation and type annotation

--- app/main.py
+++ app/main.py
@@ -9,11 +9,13 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self,
-                 distance_from_city_center: float,
-                 clean_power: int,
-                 average_rating: float,
-                 count_of_ratings: int) -> None:
+    def __init__(
+        self,
+        distance_from_city_center: float,
+        clean_power: int,
+        average_rating: float,
+        count_of_ratings: int,
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -21,8 +23,10 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         return (
-            car.comfort_class * (self.clean_power - car.clean_mark)
-            * self.average_rating) / self.distance_from_city_center
+            car.comfort_class
+            * (self.clean_power - car.clean_mark)
+            * self.average_rating
+        ) / self.distance_from_city_center
 
     def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
@@ -32,7 +36,9 @@ class CarWashStation:
         self.count_of_ratings += 1
         self.average_rating = round(
             (self.average_rating * (self.count_of_ratings - 1) + like)
-            / self.count_of_ratings, 1)
+            / self.count_of_ratings,
+            1,
+        )
 
     def serve_cars(self, cars: list) -> float:
         income_of = 0
