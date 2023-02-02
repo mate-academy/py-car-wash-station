713e7bf2baf6dd8650344d21f20599c596a888d0
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-02 16:42:09 +0200

    add identations and change name of variable

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ class Car:
                  comfort_class: int,
                  clean_mark: int,
                  brand: str) -> None:
+
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -14,6 +15,7 @@ class CarWashStation:
                  clean_power: int,
                  average_rating: float,
                  count_of_ratings: int) -> None:
+
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -29,11 +31,11 @@ class CarWashStation:
         return income
 
     def calculate_washing_price(self, car: Car) -> float:
-        calc = round((car.comfort_class
-                      * (self.clean_power - car.clean_mark)
-                      * self.average_rating
-                      / self.distance_from_city_center), 1)
-        return calc
+        washing_price = round((car.comfort_class
+                               * (self.clean_power - car.clean_mark)
+                               * self.average_rating
+                               / self.distance_from_city_center), 1)
+        return washing_price
 
     def rate_service(self, mark: int) -> None:
         self.average_rating = round((self.average_rating
