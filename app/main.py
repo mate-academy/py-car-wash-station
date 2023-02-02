bd5814d46e0521a6ea8638a94c9b10abbae2397f
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-02 18:30:18 +0200

    3 Commit

--- app/main.py
+++ app/main.py
@@ -1,28 +1,35 @@
-# v.1.0
 class Car:
+
     def __init__(
+
             self,
             comfort_class: int,
             clean_mark: int,
-            brand: str) -> None:
+            brand: str) \
+            -> None:
+
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
+
     def __init__(
+
             self,
             distance_from_city_center: float,
             clean_power: int,
             average_rating: float,
             count_of_ratings: int) -> None:
+
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, cars_car_instances: list) -> float:
+
         costs = 0
         for car in cars_car_instances:
             if car.clean_mark < self.clean_power:
@@ -40,11 +47,12 @@ class CarWashStation:
         )
 
     def wash_single_car(self, car: Car) -> None:
+
         car.clean_mark = self.clean_power
 
     def rate_service(self, rate: int) -> None:
-        s_a_r = self.average_rating
-        s_c_r = self.count_of_ratings
-        self.average_rating = round((
-            s_a_r * s_c_r + rate) / (self.count_of_ratings + 1), 1)
+
+        self.average_rating = round((self.average_rating
+                                     * self.count_of_ratings + rate)
+                                    / (self.count_of_ratings + 1), 1)
         self.count_of_ratings += 1
