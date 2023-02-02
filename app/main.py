03826c6d7ea888a921809fa9c668b172fcae9651
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-02 15:55:15 +0200

    1 Commit

--- app/main.py
+++ app/main.py
@@ -1,8 +1,59 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(
+            self,
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, cars_car_instances: list) -> float:
+        costs = 0
+        for car in cars_car_instances:
+            if car.clean_mark < self.clean_power:
+                costs += self.calculate_washing_price(car)
+                self.wash_single_car(car)
+
+        return costs
+
+    def calculate_washing_price(self, car: Car) -> float:
+
+        print(
+            car.comfort_class * (
+                self.clean_power - car.clean_mark
+            ) * self.average_rating / self.distance_from_city_center, "Print")
+        print(round(car.comfort_class * (
+            self.clean_power - car.clean_mark
+        ) * self.average_rating / self.distance_from_city_center, 1))
+        return round(
+            car.comfort_class * (
+                self.clean_power - car.clean_mark
+            ) * self.average_rating / self.distance_from_city_center, 1
+        )
+
+    def wash_single_car(self, car: Car) -> None:
+        car.clean_mark = self.clean_power
+
+    def rate_service(self, rate: int) -> None:
+
+        self.average_rating = round(
+            (
+                self.average_rating * self.count_of_ratings + rate
+            ) / (
+                self.count_of_ratings + 1), 1)
+        self.count_of_ratings += 1
+
