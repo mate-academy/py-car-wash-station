0e8a63a5445f734c5d463d91da5f4fecd5305150
Maksym Nikityuk <maksym71417@gmail.com>
2023-02-02 16:30:55 +0200

    Car Wash Fixed

--- app/main.py
+++ app/main.py
@@ -1,22 +1,24 @@
 class Car:
-    def __init__(self, comfort_class: int,
-                 clean_mark: int, brand: str) -> None:
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
-    pass
 
 
 class CarWashStation:
 
-    def __init__(self, distance_from_city_center: float,
-                 clean_power: int, average_rating: float,
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
                  count_of_ratings: str) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
-    pass
 
     def serve_cars(self, cars_list: list) -> float:
         income = 0
@@ -32,8 +34,7 @@ class CarWashStation:
             * self.average_rating / self.distance_from_city_center, 1)
 
     def wash_single_car(self, car: Car) -> None:
-        if car.clean_mark < self.clean_power:
-            car.clean_mark = self.clean_power
+        car.clean_mark = self.clean_power
 
     def rate_service(self, rate: int) -> None:
         self.average_rating = round(
