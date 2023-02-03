7ce38ea888096180b22dbbad3f25d20c807d238e
dmytro <manjustice1998@gmail.com>
2023-02-03 18:08:12 +0200

    Added variables

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ class CarWashStation:
                  distance_from_city_center: float,
                  clean_power: int,
                  average_rating: float,
-                 count_of_ratings: int) -> None:
+                 count_of_ratings: int,) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -29,14 +29,14 @@ class CarWashStation:
         return income
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round(car.comfort_class * (self.clean_power - car.clean_mark)
-                     * self.average_rating / self.distance_from_city_center, 1)
+        washed_class = car.comfort_class * (self.clean_power - car.clean_mark)
+        rate_to_distance = self.average_rating / self.distance_from_city_center
+        return round(washed_class * rate_to_distance, 1)
 
     def wash_single_car(self, car: Car) -> None:
         car.clean_mark = self.clean_power
 
     def rate_service(self, rate: int) -> None:
         self.count_of_ratings += 1
-        self.average_rating = round((self.average_rating
-                                     * (self.count_of_ratings - 1) + rate)
-                                    / self.count_of_ratings, 1)
+        rates = self.average_rating * (self.count_of_ratings - 1) + rate
+        self.average_rating = round(rates / self.count_of_ratings, 1)
