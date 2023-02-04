3fb8a918f7cf88e08c35cb78b2ab6d2ef42df936
Vladyslav Demydenko <bublikyo3@gmail.com>
2023-02-04 18:22:58 +0100

    Dry code solution. Added method can_wash_car

--- app/main.py
+++ app/main.py
@@ -20,12 +20,15 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
+    def can_wash_car(self, car: Car) -> bool:
+        return car.clean_mark < self.clean_power
+
     def wash_single_car(self, car: Car) -> None:
-        if car.clean_mark < self.clean_power:
+        if self.can_wash_car(car):
             car.clean_mark = self.clean_power
 
     def calculate_washing_price(self, car: Car) -> float:
-        if car.clean_mark < self.clean_power:
+        if self.can_wash_car(car):
             diff = (self.clean_power - car.clean_mark)
             return round(
                 (
@@ -34,7 +37,7 @@ class CarWashStation:
                     ) / self.distance_from_city_center
                 ), 1
             )
-        return 0
+        return 0.0
 
     def serve_cars(self, cars: list) -> float:
         income = 0
