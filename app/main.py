49ba9909438217e42bae83e4c9d657056b06d6ac
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-02-06 17:56:59 +0200

    Fixed: types hints

--- app/main.py
+++ app/main.py
@@ -22,18 +22,18 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def wash_single_car(self, car: type[Car]) -> None:
+    def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
-    def calculate_washing_price(self, car: type[Car]) -> list:
+    def calculate_washing_price(self, car: Car) -> float:
         if self.clean_power > car.clean_mark:
             return round((car.comfort_class
                           * (self.clean_power - car.clean_mark)
                           * self.average_rating
                           / self.distance_from_city_center), 1)
 
-    def serve_cars(self, cars: list) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         total_price = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
