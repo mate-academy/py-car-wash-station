023724b633cea23d9ac36e9d326bf6bf109ea476
Zenon <zenion@ukr.net>
2023-02-06 20:26:35 +0200

    Fixed formulas

--- app/main.py
+++ app/main.py
@@ -17,11 +17,9 @@ class CarWashStation:
         self.count_of_ratings = count
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round(car.comfort_class
-                     * (self.clean_power
-                        - car.clean_mark)
-                     * self.average_rating
-                     / self.distance_from_city_center, 1)
+        difference = self.clean_power - car.clean_mark
+        upper_part = car.comfort_class * difference * self.average_rating
+        return round(upper_part / self.distance_from_city_center, 1)
 
     def serve_cars(self, cars: list[Car]) -> float:
         income = 0
@@ -36,8 +34,6 @@ class CarWashStation:
             car.clean_mark = self.clean_power
 
     def rate_service(self, rate: int) -> None:
-        self.average_rating = round((self.average_rating
-                                     * self.count_of_ratings
-                                     + rate)
-                                    / (self.count_of_ratings + 1), 1)
+        sum_of_rates = self.average_rating * self.count_of_ratings + rate
         self.count_of_ratings += 1
+        self.average_rating = round(sum_of_rates / self.count_of_ratings, 1)
