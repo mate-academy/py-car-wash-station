9a184bb51ac8ac3f041d1207d8ae74c45c51a2be
Sergii Machulin <machulinsv@gmail.com>
2023-02-06 15:51:22 +0200

    Remove redundant checks

--- app/main.py
+++ app/main.py
@@ -5,54 +5,22 @@ class Car:
             clean_mark: int,
             brand: str
     ) -> None:
-        if not (isinstance(comfort_class, int)
-                and comfort_class in range(1, 8)):
-            raise ValueError("comfort_class argument must be from 1 to 7")
         self.comfort_class = comfort_class
-
-        if not (isinstance(clean_mark, int)
-                and clean_mark in range(1, 11)):
-            raise ValueError(
-                "clean_mark argument must be from very dirty - 1 to clean - 10"
-            )
         self.clean_mark = clean_mark
-
-        if not (isinstance(brand, str)):
-            raise TypeError("brand argument is not string")
         self.brand = brand
 
 
 class CarWashStation:
     def __init__(
             self,
-            distance_from_city_center: int | float,
+            distance_from_city_center: float,
             clean_power: int,
-            average_rating: int | float,
+            average_rating: float,
             count_of_ratings: int
     ) -> None:
-        if not (isinstance(distance_from_city_center, int | float)
-                and distance_from_city_center
-                in [value / 10.0 for value in range(1, 101, 1)]):
-            raise ValueError(
-                "distance_from_city_center argument must be from 1.0 to 10.0"
-            )
         self.distance_from_city_center = distance_from_city_center
-
-        if not (isinstance(clean_power, int)
-                and clean_power in range(1, 11)):
-            raise ValueError(
-                "clean_power argument must be from powerful 1 to powerless 10"
-            )
         self.clean_power = clean_power
-
-        if not (isinstance(average_rating, int | float)
-                and average_rating
-                in [value / 10.0 for value in range(1, 51, 1)]):
-            raise ValueError("average_rating argument must be from 1.0 to 5.0")
         self.average_rating = average_rating
-
-        if not (isinstance(count_of_ratings, int)):
-            raise TypeError("count_of_ratings argument is not integer")
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, cars: list[Car]) -> float:
