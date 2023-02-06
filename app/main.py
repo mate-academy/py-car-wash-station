402f4c5ac9a6e3a868c06a53832e34b640f38d40
usrofgh <mailforgimail@gmail.com>
2023-02-06 19:56:38 +0200

    'continue' was removed from serve_cars method

--- app/main.py
+++ app/main.py
@@ -26,11 +26,9 @@ class CarWashStation:
     def serve_cars(self, cars: list[Car]) -> float:
         income = 0
         for car in cars:
-            if car.clean_mark >= self.clean_power:
-                continue
-
-            income += self.calculate_washing_price(car)
-            self.wash_single_car(car)
+            if car.clean_mark < self.clean_power:
+                income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
         return income
 
     def calculate_washing_price(self, car: Car) -> float:
