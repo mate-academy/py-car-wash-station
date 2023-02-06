193d9e159ca4eb8061803ce55fd883c8827d75a4
Paul Savchuk <nekrjkeee@gmail.com>
2023-02-06 18:27:05 +0200

    reworked annotations and formulas for readability and maintenability

--- app/main.py
+++ app/main.py
@@ -14,13 +14,11 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(
-            self,
-            distance_from_city_center: Union[float, int],
-            clean_power: int,
-            average_rating: float,
-            count_of_ratings: int,
-    ) -> None:
+    def __init__(self,
+                 distance_from_city_center: Union[float, int],
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int) -> None:
 
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
@@ -43,22 +41,17 @@ class CarWashStation:
         return total_income
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round(
-            (self.clean_power - car.clean_mark)
-            * car.comfort_class
-            * self.average_rating
-            / self.distance_from_city_center,
-            1,
-        )
+        money_modifier = (self.clean_power - car.clean_mark) * car.comfort_class
+        our_rating_modifier = money_modifier * self.average_rating
+        total_income = our_rating_modifier / self.distance_from_city_center
+        return round(total_income, 1)
 
     def wash_single_car(self, car: Car) -> None:
         if self.can_we_wash_car(car):
             car.clean_mark = self.clean_power
 
     def rate_service(self, mark: Union[float, int]) -> None:
-        self.average_rating = round(
-            (self.average_rating * self.count_of_ratings + mark)
-            / (self.count_of_ratings + 1),
-            1,
-        )
+        new_rating_sum = self.average_rating * self.count_of_ratings + mark
+        not_rounded_average = new_rating_sum / (self.count_of_ratings + 1)
+        self.average_rating = round(not_rounded_average, 1)
         self.count_of_ratings += 1
