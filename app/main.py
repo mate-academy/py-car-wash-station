8738ce24cb96fea75571bbc3969d53fec57a81b2
Anatolii Pozniak <a13101990a@gmail.com>
2023-02-07 19:53:07 +0200

    fixed readability

--- app/main.py
+++ app/main.py
@@ -33,8 +33,8 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         return round(
-            car.comfort_class *
-            (self.clean_power - car.clean_mark)
+            car.comfort_class
+            * (self.clean_power - car.clean_mark)
             * self.average_rating
             / self.distance_from_city_center, 1)
 
