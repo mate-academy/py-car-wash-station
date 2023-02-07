7b25f7f2d28b75ea1979c8a138fe0e45101d65f1
MikhailLyvak <michael.lyvak@gmail.com>
2023-02-07 16:32:09 +0200

    Change issures

--- app/main.py
+++ app/main.py
@@ -49,12 +49,8 @@ class CarWashStation:
 
     def rate_service(self, mark: int) -> None:
         self.average_rating = round(
-            (
-                self.average_rating * self.count_of_ratings + mark
-            )
-            / (
-                self.count_of_ratings + 1
-            ), 1
+            (self.average_rating * self.count_of_ratings + mark)
+            / (self.count_of_ratings + 1), 1
         )
 
         self.count_of_ratings += 1
