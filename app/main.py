36bea7cbacda997291802c8432988169a6af2215
Ostap <anastasiia@Anastasiias-Air.Dlink>
2023-02-08 15:39:43 +0200

    split my code using variables

--- app/main.py
+++ app/main.py
@@ -32,10 +32,10 @@ class CarWashStation:
         return income
 
     def calculate_washing_price(self, car: Car) -> float:
-        car_wash_price = round(
-            car.comfort_class
-            * (self.clean_power - car.clean_mark)
-            * (self.average_rating / self.distance_from_city_center), 1)
+        suitable = self.average_rating / self.distance_from_city_center
+        clean_rate = self.clean_power - car.clean_mark
+        car_wash_price = round((car.comfort_class * clean_rate * suitable), 1)
+
         return car_wash_price
 
     def wash_single_car(self, car: Car) -> None:
