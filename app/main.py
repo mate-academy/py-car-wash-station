671bda59acbf1a2408f42a6c9c46d650c2c494b3
Mykola Savchyn <kola.savchyn@gmail.com>
2023-02-09 13:05:36 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, car_list: list) -> float:
+    def serve_cars(self, car_list: list[Car]) -> float:
         price = 0.0
         for car in car_list:
             if car.clean_mark < self.clean_power:
