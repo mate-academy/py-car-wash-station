04495921adc6f9cfeda63b37e707179d3d94c83e
Serhii <serhii.domik@gmail.com>
2023-02-15 15:01:22 +0200

    Fixed3

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str
+                 ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
+
 
 class CarWashStation:
     def __init__(self,
@@ -24,12 +29,17 @@ class CarWashStation:
                 result += self.calculate_washing_price(car)
         return result
 
-    def calculate_washing_price(self, car: Car) -> int:
-        price = (car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating / self.distance_from_city_center)
-        return round(price)
+    def calculate_washing_price(self, car: Car) -> float:
+        price = (car.comfort_class * (self.clean_power - car.clean_mark)
+                 * self.average_rating / self.distance_from_city_center)
+        return round(price, 1)
 
     def wash_single_car(self, car: Car) -> None:
         car.clean_mark = self.clean_power
 
     def rate_service(self, rate: int) -> float:
-        return round((self.average_rating * self.count_of_ratings + rate) / (self.count_of_ratings + 1), 1)
+        self.average_rating = round(
+            (self.average_rating * self.count_of_ratings + rate)
+            / (self.count_of_ratings + 1), 1)
+        self.count_of_ratings += 1
+        return self.average_rating
