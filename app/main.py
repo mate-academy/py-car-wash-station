6900e28f5f9ec99ea62dc7787b4ec36fa9efe25f
Volodymyr Yerokhov <kavbian@gmail.com>
2023-02-22 17:07:00 +0200

    'shortened_the_line'

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(self, comfort_class: int,
+                 clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -14,7 +15,10 @@ class CarWashStation:
         self.count_of_ratings = count_of_ratings
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round(car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating / self.distance_from_city_center, 1)
+        dif_clean = self.clean_power - car.clean_mark
+        numerator = car.comfort_class * dif_clean * self.average_rating
+        price = numerator / self.distance_from_city_center
+        return round(price, 1)
 
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark >= self.clean_power:
@@ -32,5 +36,6 @@ class CarWashStation:
         return income
 
     def rate_service(self, evaluation: float) -> None:
-        self.average_rating = round((self.average_rating * self.count_of_ratings + evaluation) / (self.count_of_ratings + 1), 1)
+        sum_of_mk = self.average_rating * self.count_of_ratings + evaluation
+        self.average_rating = round(sum_of_mk / (self.count_of_ratings + 1), 1)
         self.count_of_ratings += 1
