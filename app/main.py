5f0e83372ce5f8fdacdf1fac773e63a621156f52
Petro Havrysh <petro.havrysh@gmail.com>
2023-03-05 21:00:31 +0200

    'New_messeg'

--- app/main.py
+++ app/main.py
@@ -6,9 +6,9 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int,
+    def __init__(self, dis_to_city: float, clean_power: int,
                  average_rating: float, count_of_ratings: int) -> None:
-        self.distance_from_city_center = distance_from_city_center
+        self.dis_to_city = dis_to_city
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
@@ -24,7 +24,7 @@ class CarWashStation:
     def calculate_washing_price(self, car: Car) -> float:
         return round(
             car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating /
-            self.distance_from_city_center,
+            self.dis_to_city,
             1
         )
 
