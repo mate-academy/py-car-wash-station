7aea56f58269055c50246706b0a51ffa0bed2dde
Petro Havrysh <petro.havrysh@gmail.com>
2023-03-05 21:12:26 +0200

    'New_messeg'

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(self, comfort_class: int, clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -23,16 +24,15 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         return round(
-            car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating /
-            self.dis_to_city,
-            1
-        )
+            car.comfort_class * (self.clean_power - car.clean_mark)
+            * self.average_rating / self.dis_to_city, 1)
 
     def wash_single_car(self, car: Car) -> None:
         car.clean_mark = self.clean_power
 
     def rate_service(self, rating: float) -> None:
-        self.average_rating = round((self.average_rating * self.count_of_ratings + rating) /
+        self.average_rating = round((self.average_rating * self.count_of_ratings
+                                     + rating) /
                                     (self.count_of_ratings + 1), 1)
         self.count_of_ratings += 1
 # Наче  код
