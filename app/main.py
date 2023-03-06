6f4022d4b81d8d56f2de3423579c9e46e50006fc
Petro Havrysh <petro.havrysh@gmail.com>
2023-03-06 10:52:59 +0200

    'Ische_ras_100'

--- app/main.py
+++ app/main.py
@@ -32,9 +32,9 @@ class CarWashStation:
 
     def rate_service(self, rating: float) -> None:
         self.average_rating = round(
-            (self.average_rating * self.count_of_ratings + rating) /
-            (self.count_of_ratings + 1),
+            (self.average_rating * self.count_of_ratings +
+             rating) / (self.count_of_ratings + 1),
             1
         )
         self.count_of_ratings += 1
-# Наче  код
+# код
