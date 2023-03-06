b35040638284ed7ef102932164b45d9ffe2af182
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-03-06 15:15:09 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ class CarWashStation:
         self.clean_power = clean_power
         self.average_rating = round(average_rating, 1)
         self.count_of_ratings = count_of_ratings
-        
+
     def serve_cars(self, cars: list) -> int:
         result = 0
         for car in cars:
@@ -29,7 +29,7 @@ class CarWashStation:
             (self.average_rating * self.count_of_ratings) + num)
             / (self.count_of_ratings + 1)))
         self.count_of_ratings += 1
-        
+
     def calculate_washing_price(self, car: Car) -> float:
         price = (car.comfort_class * (self.clean_power - car.clean_mark)
                  * (self.average_rating / self.distance_from_city_center))
