a2b61f54b0c37e0911fcc5e5a551a3531645509f
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-03-12 21:51:13 +0200

    Finish task

--- app/main.py
+++ app/main.py
@@ -1,8 +1,71 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(
+            self,
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
+    ) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, cars: list) -> float:
+        filtered_cars = filter(
+            lambda car: car.clean_mark < self.clean_power, cars
+        )
+        income: float = 0
+
+        for car in filtered_cars:
+            income += self.calculate_washing_price(car)
+            self.wash_single_car(car)
+
+        return round(income, 1)
+
+    def calculate_washing_price(self, car: Car) -> float:
+        return round(car.comfort_class
+                     * (self.clean_power - car.clean_mark)
+                     * self.average_rating
+                     / self.distance_from_city_center
+                     , 1)
+
+    def wash_single_car(self, car: Car) -> None:
+        if self.clean_power > car.clean_mark:
+            car.clean_mark = self.clean_power
+
+    def rate_service(self, rate: int) -> None:
+        self.average_rating = round((self.average_rating
+                                     * self.count_of_ratings + rate)
+                                    / (self.count_of_ratings + 1)
+                                    , 1)
+
+        self.count_of_ratings += 1
+
+
+wash_station = CarWashStation(
+    distance_from_city_center=6,
+    clean_power=8,
+    average_rating=3.9,
+    count_of_ratings=11
+)
+
+print(wash_station.average_rating)    # 3.9
+print(wash_station.count_of_ratings)  # 11
+
+wash_station.rate_service(5)
+
+print(wash_station.average_rating)    # 4.0
+print(wash_station.count_of_ratings)  # 12
