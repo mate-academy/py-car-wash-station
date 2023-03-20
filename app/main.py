621e8b0ddb2812fdd0a8db04db53e7069ed683f8
Munkoor <eee1lvlboss@gmail.com>
2023-03-20 22:27:34 +0200

    fix problems with flake8

--- app/main.py
+++ app/main.py
@@ -27,9 +27,10 @@ class CarWashStation:
         return price
 
     def calculate_washing_price(self, car: Car) -> float:
-        price = (car.comfort_class * ((self.clean_power - car.clean_mark)
-                                      * self.average_rating)) \
-                                       / self.distance_from_city_center
+        price = (
+            car.comfort_class * ((self.clean_power - car.clean_mark)
+                                 * self.average_rating)
+        ) / self.distance_from_city_center
 
         return round(price, 1)
 
