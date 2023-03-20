9b6fa8becc709cf6893d199c07f4ce2f348daf75
Munkoor <eee1lvlboss@gmail.com>
2023-03-20 12:14:54 +0200

    eliminated bugs

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ class CarWashStation:
         for car in cars:
             if car.clean_mark < self.clean_power:
                 price += self.calculate_washing_price(car)
-                car.clean_mark = self.clean_power
+                self.wash_single_car(car)
 
         return price
 
