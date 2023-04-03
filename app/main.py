768640f50c6fad2d40ff554113e8b958a044a9c5
Artem Uhon <artemugon@gmail.com>
2023-04-03 16:57:56 +0300

    Solved task

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> any:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -8,12 +8,12 @@ class Car:
 class CarWashStation:
 
     def __init__(
-        self,
-        distance_from_city_center: float,
-        clean_power: int,
-        average_rating: float,
-        count_of_ratings: int
-) -> None:
+            self,
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -31,11 +31,11 @@ class CarWashStation:
                 self.wash_single_car(car)
         return income
 
-    def calculate_washing_price(self, car: Car) -> int:
+    def calculate_washing_price(self, car: Car) -> float:
         return round(car.comfort_class * (self.clean_power - car.clean_mark)
                      * self.average_rating / self.distance_from_city_center, 1)
 
-    def rate_service(self, rate: int) -> int:
+    def rate_service(self, rate: int) -> float:
         rating_before = self.average_rating * self.count_of_ratings
         self.count_of_ratings += 1
         self.average_rating = round((rating_before + rate)
