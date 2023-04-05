6d1631d6bda1ff25da5b8b85dea0cedf843bda77
Stanislav Bulanov <p99kab00@gmail.com>
2023-04-05 18:18:18 +0300

    Initial implementation of the module

--- app/main.py
+++ app/main.py
@@ -1,8 +1,145 @@
+"""Contains Car and CarWashStation classes."""
+
+
 class Car:
-    # write your code here
-    pass
+    """Class implementation for a car."""
+
+    def __init__(
+        self,
+        comfort_class: int,
+        clean_mark: int,
+        brand: str
+    ) -> None:
+        """Create an instance of a car.
+
+        Args:
+            comfort_class - car comfort class from 1 to 7
+            clean_mark - car cleanness level from 1 to 10
+            brand - car brand name
+
+        Returns:
+            An instance of a Car class
+
+        Raises:
+            A ValueError if comfort class is not within 1 and 7
+            A ValueError if car cleanness level is not within 1 and 10
+        """
+        if comfort_class < 1 or comfort_class > 7:
+            raise ValueError("Comfort class must be within 1 and 7")
+
+        if clean_mark < 1 or clean_mark > 10:
+            raise ValueError("Cleanness level must be within 1 and 10")
+
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    """Class implementation for a car wash station."""
+
+    def __init__(
+        self,
+        distance_from_city_center: int,
+        clean_power: int,
+        average_rating: float,
+        count_of_ratings: int
+    ) -> None:
+        """Create an instance of a car wash station.
+
+        Args:
+            distance_from_city_center - distance from city center from 1 to 10
+            clean_power - the level of cleanness the station provides
+            averate_rating - average ratinf of the station from 1.0 to 5.0
+            count_of_ratings - total amount of the station ratings
+
+        Returns:
+            An instalce of a CarWashStation class
+
+        Raises:
+            A ValueError if distance from city center is not within 1 and 10
+            A ValueError if average rating is not within 1.0 and 5.0
+        """
+        if distance_from_city_center < 1 or distance_from_city_center > 10:
+            raise ValueError(
+                "Distance from city center must be within 1 and 10"
+            )
+
+        if average_rating < 1 or average_rating > 5:
+            raise ValueError("Average rating must be within 1 and 5")
+
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, cars: list[Car]) -> float:
+        """Service list of cars and return total income of service.
+
+        Args:
+            cars: - list of Car class instances
+
+        Returns:
+            Total amount of service cost
+        """
+        total_income = 0
+
+        for car in cars:
+            if self.is_serviceable(car):
+                total_income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
+
+        return total_income
+
+    def calculate_washing_price(self, car: Car) -> float:
+        """Calculate service cost for a single car.
+
+        Args:
+            car - an instance of Car class
+
+        Returns:
+            Service cost for a single car
+        """
+        return round(
+            car.comfort_class
+            * (self.clean_power - car.clean_mark)
+            * self.average_rating
+            / self.distance_from_city_center
+            , 1
+        )
+
+    def wash_single_car(self, car: Car) -> None:
+        """Update cleanness level for a single car.
+
+        Args:
+            car - an instance of Car class
+        """
+        car.clean_mark = self.clean_power
+
+    def rate_service(self, rating: int) -> None:
+        """Update average rating and total number of ratings.
+
+        Args:
+            rating - rating from 1 to 5
+        """
+        if rating < 1 or rating > 5:
+            raise ValueError("Rating should be within 1 and 5")
+
+        current_total_rating = self.average_rating * self.count_of_ratings
+        self.count_of_ratings += 1
+        self.average_rating = round(
+            (current_total_rating + rating)
+            / self.count_of_ratings
+            , 1
+        )
+
+    def is_serviceable(self, car: Car) -> bool:
+        """Check if a car could be serviceable.
+
+        Args:
+            car - an instance of Car class
+
+        Returns:
+            A result of a car check for the service
+        """
+        return car.clean_mark < self.clean_power
