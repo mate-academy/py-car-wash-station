e13aa16378b19c6bc89d81e25900ce1a32b2f77f
Illya Svyshcho <iill44iikk@gmail.com>
2023-04-06 18:18:02 +0300

    Solution of car wash station

--- app/main.py
+++ app/main.py
@@ -1,8 +1,44 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, list_of_cars: list[Car]) -> float:
+        sum_of_income = 0
+        for car in list_of_cars:
+            if car.clean_mark < self.clean_power:
+                sum_of_income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
+        return round(sum_of_income, 1)
+
+    def calculate_washing_price(self, info_car: Car) -> float:
+        return round(info_car.comfort_class
+                     * (self.clean_power - info_car.clean_mark)
+                     * self.average_rating / self.distance_from_city_center, 1)
+
+    def wash_single_car(self, info_car: Car) -> None:
+        if info_car.clean_mark < self.clean_power:
+            info_car.clean_mark = self.clean_power
+
+    def rate_service(self, car_info_raiting: float) -> None:
+        self.count_of_ratings += 1
+        self.average_rating = round((self.average_rating
+                                     * (self.count_of_ratings - 1)
+                                     + car_info_raiting)
+                                    / self.count_of_ratings, 1)
