947edb1b54aabee4789f6dad06d72b62fd134a50
Roman Fedorchuk <slasherroman@gmail.com>
2023-04-07 12:57:24 +0300

    'revised'

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
@@ -31,11 +31,12 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         clean_difference = self.clean_power - car.clean_mark
-        washing_price = \
-            car.comfort_class \
-            * clean_difference \
-            * self.average_rating \
+        washing_price = (
+            car.comfort_class
+            * clean_difference
+            * self.average_rating
             / self.distance_from_city_center
+        )
         return round(washing_price, 1)
 
     def wash_single_car(self, car: Car) -> None:
