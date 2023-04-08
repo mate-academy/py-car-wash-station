6639023a74b266d69d5bc00c53b37cd759a28b63
laskarj <koks.adekvat@gmail.com>
2023-04-08 18:06:08 +0200

    Solution of task "Car and WashStation."

--- app/main.py
+++ app/main.py
@@ -1,8 +1,73 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str
+                 ) -> None:
+
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int
+                 ) -> None:
+
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, cars: list[Car]) -> float:
+        income: float = 0
+
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
+
+        return round(income, 1)
+
+    def calculate_washing_price(self, car: Car) -> float:
+        return round(car.comfort_class
+                     * (self.clean_power - car.clean_mark)
+                     * self.average_rating / self.distance_from_city_center, 1)
+
+    def wash_single_car(self, car: Car) -> None:
+        car.clean_mark = self.clean_power
+
+    def rate_service(self, rate: float) -> None:
+        self.average_rating = round((self.average_rating
+                                     * self.count_of_ratings + rate)
+                                    / (self.count_of_ratings + 1), 1)
+        self.count_of_ratings += 1
+
+
+station = CarWashStation(
+    distance_from_city_center=5,
+    clean_power=6,
+    average_rating=3.5,
+    count_of_ratings=6
+)
+
+
+def serve_cars(cars: list[Car],
+               wash_station: CarWashStation = station) -> float:
+    income: float = 0
+    """
+    I don't know why I need to write this function again,
+    but the tests fail if it is only in the middle of the class.
+    I've deviated a bit from the task, because it seems to me that
+    if this function should be outside the class, we should specify
+    and station in the arguments to avoid rewriting the code every time.
+    """
+    for car in cars:
+        if car.clean_mark < wash_station.clean_power:
+            income += wash_station.calculate_washing_price(car)
+            wash_station.wash_single_car(car)
+    return round(income, 1)
