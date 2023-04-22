8e04b7ccabcd98fbba897fec8f3776c1d08576a1
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2023-04-22 13:14:58 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,37 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(self, comfort_class: int, clean_mark: int,
+                 brand: str) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self, distance_from_city_center: float, clean_power: int,
+                 average_rating: float, count_of_ratings: int) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, cars: list) -> float:
+        income = 0
+        for car in cars:
+            income += self.calculate_washing_price(car)
+            self.wash_single_car(car)
+        return income
+
+    def calculate_washing_price(self, car: Car) -> float:
+        clean_mark_change = max(0, self.clean_power - car.clean_mark)
+        return round(car.comfort_class * self.average_rating
+                     * clean_mark_change / self.distance_from_city_center, 1)
+
+    def wash_single_car(self, car: Car) -> None:
+        if self.clean_power > car.clean_mark:
+            car.clean_mark = self.clean_power
+
+    def rate_service(self, rate: int) -> None:
+        sum_of_rates = self.average_rating * self.count_of_ratings
+        self.count_of_ratings += 1
+        self.average_rating = round((sum_of_rates + rate)
+                                    / self.count_of_ratings, 1)
