03a8d79586822d069dccca27a74d5df13f14a476
Andrew Sulym <a.sulymm@gmail.com>
2023-04-25 12:06:30 +0100

    Bugs fixed

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 class Car:
     def __init__(self, comfort_class: int,
-                 clean_mark: int, 
+                 clean_mark: int,
                  brand: str
                  ) -> None:
         self.comfort_class = comfort_class
