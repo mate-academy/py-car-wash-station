523d537725447c2b437be2998083f894b0ecef74
Ihor Bachuk <igor.bachuk04@gmail.com>
2023-05-07 21:35:15 +0300

    made CarWashStation calculate_washing_price method

--- app/main.py
+++ app/main.py
@@ -24,3 +24,14 @@ class CarWashStation:
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
+
+    def calculate_washing_price(self, car: Car) -> float:
+        return (
+            round((car.comfort_class
+                   * (self.clean_power - car.clean_mark)
+                   * self.average_rating / self.distance_from_city_center), 1)
+            if self.clean_power > car.clean_mark
+            else 0
+        )
+
+    # def wash_single_car(self, car: Car) -> None:
