42afce061e210638c3243369ac4984964f0003e4
FadieievaA <fadieieva.arina@gmail.com>
2023-05-13 12:10:49 +0300

    Added changes

--- app/main.py
+++ app/main.py
@@ -22,16 +22,13 @@ class CarWashStation:
         return round(car.comfort_class * (self.clean_power - car.clean_mark) \
                          * self.average_rating / self.distance_from_city_center, 1)
 
-    def wash_single_car(self, car: Car) -> bool:
+    def wash_single_car(self, car: Car) -> None:
         car.clean_mark = self.clean_power
 
-    def serve_cars(self, cars: list) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         whole_sum = 0
         for car in cars:
-            #print("washing " + car.brand)
-            #print(car.clean_mark, self.clean_power)
             if car.clean_mark < self.clean_power:
-                #print("washing " + car.brand)
                 whole_sum += self.calculate_washing_price(car)
                 self.wash_single_car(car)
 
