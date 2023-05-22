c48400c75d9591444b06b352dd3ae1a53e202be8
Oleksii Kiselov <crift34@gmail.com>
2023-05-22 13:06:07 -0400

    Refactored the types of input arguments

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
@@ -33,7 +33,7 @@ class CarWashStation:
                 self.wash_single_car(car)
         return income
 
-    def calculate_washing_price(self, car: str) -> None:
+    def calculate_washing_price(self, car: Car) -> float:
         return round(
             (car.comfort_class
              * (self.clean_power - car.clean_mark)
@@ -41,7 +41,7 @@ class CarWashStation:
             1
         )
 
-    def wash_single_car(self, car: str) -> None:
+    def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
