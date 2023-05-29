8ceee978f0890c5dce7e26617925811115b0c8e1
Oleksii Kolii <igravkompik@gmail.com>
2023-05-29 16:14:36 +0300

    removed comments

--- app/main.py
+++ app/main.py
@@ -31,9 +31,6 @@ class CarWashStation:
     def wash_single_car(self, car: Car) -> int:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
-        # price = self.calculate_washing_price(car)
-        # car.clean_mark = self.clean_power
-        # return price
 
     def calculate_washing_price(self, car: Car) -> float:
         return round(car.comfort_class
