dfc1f49b66e3e9dffe434f1339ad2242a84c54af
Yaryk Konoplyov <kannabis252@gmail.com>
2023-05-29 19:17:37 +0300

    Solution + variable price removed

--- app/main.py
+++ app/main.py
@@ -20,12 +20,12 @@ class CarWashStation:
         self.count_of_ratings = count_of_ratings
 
     def calculate_washing_price(self, car: Car) -> float:
-        price = round(
+        return round(
             car.comfort_class * (self.clean_power - car.clean_mark)
             * (self.average_rating / self.distance_from_city_center),
             1
         )
-        return price
+
 
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
