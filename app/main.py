f62ce9ab6efaf62edc6e966879ce5dfc00c05ab6
Vlad Larin <lordgadyka71@gmail.com>
2023-05-29 09:25:00 +0300

    'updated-line-break'

--- app/main.py
+++ app/main.py
@@ -28,14 +28,14 @@ class CarWashStation:
             car.clean_mark = self.clean_power
 
     def rate_service(self, rating: int) -> None:
-        new_rating = (self.average_rating * self.count_of_ratings + rating) /\
-            (self.count_of_ratings + 1)
+        new_rating = ((self.average_rating * self.count_of_ratings + rating)
+                      / (self.count_of_ratings + 1))
         self.average_rating = round(new_rating, 1)
         self.count_of_ratings += 1
 
     def calculate_washing_price(self, car: Car) -> float:
-        price = car.comfort_class * (self.clean_power - car.clean_mark) *\
-            self.average_rating / self.distance_from_city_center
+        price = (car.comfort_class * (self.clean_power - car.clean_mark)
+                 * self.average_rating / self.distance_from_city_center)
         return round(price, 1) if car.clean_mark < self.clean_power else 0.0
 
     def serve_cars(self, cars: list[Car]) -> float:
