c7378b36ecb26246bb80bbe86b041c5829395abe
Mykhailo Korotia <mr.anderson528491@gmail.com>
2023-05-29 17:36:08 +0300

    'solution'

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(self, comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -19,16 +21,17 @@ class CarWashStation:
         income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
-                income += self.calculate_washing_priceпше (car)
+                income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
         return round(income, 1)
 
-    def calculate_washing_price(self, car) -> float:
+    def calculate_washing_price(self, car: Car) -> float:
         income = round(car.comfort_class * (self.clean_power - car.clean_mark)
-                       * self.average_rating / self.distance_from_city_center, 1)
+                       * self.average_rating
+                       / self.distance_from_city_center, 1)
         return income
 
-    def wash_single_car(self, car) -> None:
+    def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
@@ -37,20 +40,3 @@ class CarWashStation:
         self.average_rating = round((self.average_rating
                                      * (self.count_of_ratings - 1) + rating)
                                     / self.count_of_ratings, 1)
-
-
-
-bmw = Car(3, 3, 'BMW')
-audi = Car(4, 9, 'Audi')
-
-wash_station = CarWashStation(
-    distance_from_city_center=5,
-    clean_power=6,
-    average_rating=3.5,
-    count_of_ratings=6
-)
-
-income = wash_station.serve_cars([bmw, audi])
-
-print(income)
-print(bmw.clean_mark)
