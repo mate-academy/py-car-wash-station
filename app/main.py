3547119c6e38354fc5ca850d479d1c37475921a9
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-05-29 14:10:57 +0300

    replace "\"

--- app/main.py
+++ app/main.py
@@ -32,8 +32,9 @@ class CarWashStation:
         return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        price = car.comfort_class * (self.clean_power - car.clean_mark)\
-            * self.average_rating / self.distance_from_city_center
+        price_diff = car.comfort_class * (self.clean_power - car.clean_mark)
+        ratio = self.average_rating / self.distance_from_city_center
+        price = price_diff * ratio
         return round(price, 1)
 
     def wash_single_car(self, car: Car) -> None:
