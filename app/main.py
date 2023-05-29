80055d5c6154602cb3e6532bbe7b7367eec089c0
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-05-29 20:24:40 +0300

    deleted the unnecessary variable and change 'total_income' variable

--- app/main.py
+++ app/main.py
@@ -20,25 +20,23 @@ class CarWashStation:
         self.count_of_ratings = count_of_ratings
 
     def calculate_washing_price(self, car: Car) -> float:
-        single_wash_cost = (car.comfort_class
-                            * (self.clean_power
-                               - car.clean_mark)
-                            * (self.average_rating
-                               / self.distance_from_city_center))
-        return single_wash_cost
+        return (car.comfort_class
+                * (self.clean_power
+                   - car.clean_mark)
+                * (self.average_rating
+                   / self.distance_from_city_center))
 
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
     def serve_cars(self, wash_list: list) -> float:
-        total_income = 0.0
+        total_income = 0
         for car in wash_list:
             if car.clean_mark < self.clean_power:
                 total_income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
-        total_income = round(total_income, 1)
-        return total_income
+        return round(total_income, 1)
 
     def rate_service(self, new_rate: int) -> None:
         total_rating = self.average_rating * self.count_of_ratings + new_rate
