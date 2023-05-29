c26f8c2dc0b181ff3a99c1e7a2bc78813217d8a5
Oleksii Kolii <igravkompik@gmail.com>
2023-05-29 16:10:03 +0300

    fixed wash_single_car

--- app/main.py
+++ app/main.py
@@ -24,13 +24,16 @@ class CarWashStation:
         income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
-                income += self.wash_single_car(car)
+                income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
         return round(income, 1)
 
     def wash_single_car(self, car: Car) -> int:
-        price = self.calculate_washing_price(car)
-        car.clean_mark = self.clean_power
-        return price
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
+        # price = self.calculate_washing_price(car)
+        # car.clean_mark = self.clean_power
+        # return price
 
     def calculate_washing_price(self, car: Car) -> float:
         return round(car.comfort_class
