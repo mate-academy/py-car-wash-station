1c4750fa8c17d3e5c8a5ac217e7dbf80d194289b
Veinmax <aturjust88@gmail.com>
2023-05-30 13:43:37 +0300

    Solution3

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(self, comfort_class: int,
+                 clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
