315fcb64d0db0f9fafa40ceaaa8e39f7e69447b7
Alek Ilchenko <yaichenko87@gmail.com>
2023-06-16 15:01:19 +0100

    Update

--- app/main.py
+++ app/main.py
@@ -1,28 +1,39 @@
 class Car:
-    def __init__(self, comfort_class: int,
-                 clean_mark: int,
-                 brand: str) -> None:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self,
-                 distance_from_city_center: float,
-                 clean_power: int,
-                 average_rating: float,
-                 count_of_ratings: int) -> None:
+    def __init__(
+            self,
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, cars: list) -> float:
-        pre_serve_cars = [car for car in cars
-                          if self.is_possible_wash_car(car)]
-        income = sum([self.calculate_washing_price(car)
-                      for car in pre_serve_cars])
+        pre_serve_cars = [
+            car for car in cars
+            if self.is_possible_wash_car(car)
+        ]
+        income = sum(
+            [
+                self.calculate_washing_price(car)
+                for car in pre_serve_cars
+            ]
+        )
         for car in pre_serve_cars:
             self.wash_single_car(car)
         return round(income, ndigits=1)
