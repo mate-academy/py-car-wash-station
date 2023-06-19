31ff532182897a9936fc065f6c474535497531ed
Ivan Zhaivoronok <ivan.zhaivoronok@ukr.net>
2023-06-19 15:14:12 +0300

    Created CarWashStation class (init method)

--- app/main.py
+++ app/main.py
@@ -5,5 +5,11 @@ class Car:
         self.brand = brand
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(
+        self,
+        distance_from_city_center: float,
+        clean_power: int,
+        average_rating: float,
+        count_of_ratings: int
+    ) -> None:
+        pass
