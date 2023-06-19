23a12be8f17e0aa6075e0c66f9cd25e8c23cad20
Ivan Zhaivoronok <ivan.zhaivoronok@ukr.net>
2023-06-19 15:32:03 +0300

    created wash_single_car method

--- app/main.py
+++ app/main.py
@@ -17,9 +17,16 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
     
-    def calculate_washing_price(self, car: Car):
+    def calculate_washing_price(self, car: Car) -> float:
         return round((car.comfort_class 
                 * (self.clean_power - car.clean_mark)
                 * self.average_rating 
                 / self.distance_from_city_center),
                 1)
+    
+    def wash_single_car(self, car: Car) -> Car:
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
+        return car
+    
+    
