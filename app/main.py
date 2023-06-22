d361044c8d2b936d850752063461ab8946fd84a7
andriy-py <andriy.omelya@gmail.com>
2023-06-22 19:03:02 +0300

    task ready

--- app/main.py
+++ app/main.py
@@ -37,7 +37,7 @@ class CarWashStation:
     def wash_single_car(self, car: Car) -> None:
         car.clean_mark = max(car.clean_mark, self.clean_power)
 
-    def serve_cars(self, cars: list) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         income = 0.0
         for car in cars:
             if car.clean_mark < self.clean_power:
