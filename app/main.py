96e69e8a01b08ee79332cc9d67d8440cd275e4b3
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-06-28 21:52:56 +0300

    fifth_solution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 class Car:
 
-    def __init__(self,
-                 comfort_class: int,
-                 clean_mark: int,
-                 brand: str
-                 ) -> None:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -12,12 +13,13 @@ class Car:
 
 class CarWashStation:
 
-    def __init__(self,
-                 distance_from_city_center: int,
-                 clean_power: int,
-                 average_rating: float,
-                 count_of_ratings: int
-                 ) -> None:
+    def __init__(
+            self,
+            distance_from_city_center: int,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
