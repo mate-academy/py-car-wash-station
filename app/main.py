1d7a91d52cfb077f3c3218dceb98448fcdd38a84
makukh <makukh@gmail.com>
2023-07-05 13:30:30 +0300

    save01

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
+    def __init__(self):
     pass
