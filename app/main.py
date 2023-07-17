2f70df40768d2f449f0de00ebf890faf963fa527
Nikita Plotnytskyi <Nikita.plotnitckiy@gmail.com>
2023-07-17 20:15:54 +0300

    code style changes

--- app/main.py
+++ app/main.py
@@ -1,20 +1,25 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) \
-            -> None:
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int,
-                 average_rating: float, count_of_ratings: int) -> None:
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, list_car: list) -> float:
+    def serve_cars(self, list_car: list[Car]) -> float:
         price = 0
         for car in list_car:
             if car.clean_mark < self.clean_power:
@@ -22,15 +27,19 @@ class CarWashStation:
                 self.wash_single_car(car)
         return round(price, 1)
 
-    def wash_single_car(self, other: object) -> None:
-        other.clean_mark = self.clean_power
+    def wash_single_car(self, car: Car) -> None:
+        car.clean_mark = self.clean_power
 
     def rate_service(self, grade: int) -> None:
         self.count_of_ratings += 1
-        self.average_rating = round(((self.count_of_ratings - 1)
-                                    * self.average_rating + grade)
-                                    / self.count_of_ratings, 1)
+        average_rating = (((self.count_of_ratings - 1)
+                           * self.average_rating + grade)
+                          / self.count_of_ratings)
+        self.average_rating = round(average_rating, 1)
 
-    def calculate_washing_price(self, car: object) -> float:
-        return car.comfort_class * (self.clean_power - car.clean_mark) \
-            * self.average_rating / self.distance_from_city_center
+    def calculate_washing_price(self, car: Car) -> float:
+        return (car.comfort_class
+                * (self.clean_power
+                   - car.clean_mark)
+                * self.average_rating
+                / self.distance_from_city_center)
