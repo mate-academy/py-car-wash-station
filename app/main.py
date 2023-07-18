ec86994288550185ecad5493aadf8d4f143f5ab4
Nikita Plotnytskyi <Nikita.plotnitckiy@gmail.com>
2023-07-18 14:10:59 +0300

    1. fixed method __init__ 2. updated method calculate_washing_price

--- app/main.py
+++ app/main.py
@@ -1,19 +1,23 @@
 class Car:
-    def __init__(self,
-                 comfort_class: int,
-                 clean_mark: int,
-                 brand: str) -> None:
+    def __init__(
+        self,
+        comfort_class: int,
+        clean_mark: int,
+        brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self,
-                 distance_from_city_center: float,
-                 clean_power: int,
-                 average_rating: float,
-                 count_of_ratings: int) -> None:
+    def __init__(
+        self,
+        distance_from_city_center: float,
+        clean_power: int,
+        average_rating: float,
+        count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -25,7 +29,7 @@ class CarWashStation:
             if car.clean_mark < self.clean_power:
                 price += self.calculate_washing_price(car)
                 self.wash_single_car(car)
-        return round(price, 1)
+        return price
 
     def wash_single_car(self, car: Car) -> None:
         car.clean_mark = self.clean_power
@@ -38,8 +42,10 @@ class CarWashStation:
         self.average_rating = round(average_rating, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        return (car.comfort_class
-                * (self.clean_power
-                   - car.clean_mark)
-                * self.average_rating
-                / self.distance_from_city_center)
+        price = (
+            car.comfort_class
+            * (self.clean_power - car.clean_mark)
+            * self.average_rating
+            / self.distance_from_city_center
+        )
+        return round(price, 1)
