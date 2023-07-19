0dea7552ae5fd6d6d012dd2d322fbe7ce623b8d6
Lliia Kostenko <136697879+liliia-kostenko@users.noreply.github.com>
2023-07-19 22:11:23 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ class Car:
             comfort_class: int,
             clean_mark: int,
             brand: str
-        ) -> None:
+    ) -> None:
 
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
@@ -30,7 +30,7 @@ class CarWashStation:
 
         for car in cars:
             if car.clean_mark < self.clean_power:
-                income += self.calculate_washing_price(car)
+                income = income + self.calculate_washing_price(car)
                 self.wash_single_car(car)
         return round(income, 1)
 
@@ -49,6 +49,6 @@ class CarWashStation:
 
     def rate_service(self, rating: float) -> None:
         total_ratings = self.average_rating * self.count_of_ratings
-        self.count_of_ratings += 1
+        self.count_of_ratings = self.count_of_ratings + 1
         self.average_rating = round((total_ratings + rating)
                                     / self.count_of_ratings, 1)
