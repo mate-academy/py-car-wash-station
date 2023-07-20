3ad009cefefaa2d6d6b64f279c2ea97a4e6d1a17
Alex Pelevin <ya.pelevin1999@gmail.com>
2023-07-20 17:04:16 +0300

    fix length of math operation in line

--- app/main.py
+++ app/main.py
@@ -12,13 +12,13 @@ class CarWashStation:
         float,
         clean_power: int,
         average_rating: float,
-        count_of_rating: int
+        count_of_ratings: int
     ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.float = float
         self.clean_power = clean_power
         self.average_rating = average_rating
-        self.count_of_rating = count_of_rating
+        self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, cars: list[Car]) -> float:
         income: float = 0
@@ -31,11 +31,14 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         diff_clean = self.clean_power - car.clean_mark
-        return car.comfort_class * diff_clean * self.average_rating / self.distance_from_city_center
+        mult_class_clean = car.comfort_class * diff_clean
+        return mult_class_clean * self.average_rating / self.distance_from_city_center
 
     def wash_single_car(self, car: Car) -> None:
         car.clean_mark = self.clean_power
 
     def rate_service(self, rate: float) -> None:
-        self.average_rating = round((self.count_of_rating * self.average_rating + rate) / (self.average_rating + 1), 1)
-        self.count_of_rating += 1
+        new_ratings_count = self.count_of_ratings + 1
+        all_ratings = self.count_of_ratings * self.average_rating + rate
+        self.average_rating = round(all_ratings / new_ratings_count, 1)
+        self.count_of_ratings = new_ratings_count
