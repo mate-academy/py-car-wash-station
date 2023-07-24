0901f75266efb86a52e69a1429570e295c26251c
Ruslan <rusipbox@gmail.com>
2023-07-24 17:18:07 +0300

    Test (py-car-wash-station) corrected 24.07.23

--- app/main.py
+++ app/main.py
@@ -2,10 +2,13 @@ from typing import List
 
 
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: float, brand: str):
+    def __init__(self, comfort_class: int,
+                 clean_mark: float,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
+
 
 class CarWashStation:
     def __init__(self, distance_from_city_center: float,
@@ -30,7 +33,7 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         difference = self.clean_power - car.clean_mark
-        remainder= self.average_rating / self.distance_from_city_center
+        remainder = self.average_rating / self.distance_from_city_center
         result_price = car.comfort_class * difference * remainder
         return round(result_price, 1)
 
