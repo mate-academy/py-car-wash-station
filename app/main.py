51270e2db994e76cd511831c65c5f38adee6da33
Ruslan <rusipbox@gmail.com>
2023-07-29 14:57:35 +0300

    Test (py-car-wash-station) newline arguments

--- app/main.py
+++ app/main.py
@@ -11,16 +11,22 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float,
-                 clean_power: float,
-                 average_rating: float,
-                 count_of_ratings: int) -> None:
+    def __init__(
+            self,
+            distance_from_city_center: float,
+            clean_power: float,
+            average_rating: float,
+            count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, car_list: List[Car]) -> float:
+    def serve_cars(
+            self,
+            car_list: List[Car]
+    ) -> float:
         total_income = 0.0
 
         for car in car_list:
@@ -31,7 +37,9 @@ class CarWashStation:
 
         return round(total_income, 1)
 
-    def calculate_washing_price(self, car: Car) -> float:
+    def calculate_washing_price(
+            self, car: Car
+    ) -> float:
         difference = self.clean_power - car.clean_mark
         remainder = self.average_rating / self.distance_from_city_center
         result_price = car.comfort_class * difference * remainder
