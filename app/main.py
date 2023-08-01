cadb6333aa9fc49d7f2dc2828b0e184f892d6f5d
Kateryna Zadorozhna <136272476+katryana@users.noreply.github.com>
2023-08-01 19:01:49 +0300

    Update main.py final version

--- app/main.py
+++ app/main.py
@@ -1,25 +1,21 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int,
-                     brand: str) -> None:
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
         if comfort_class < 1 or comfort_class > 7:
             print(TypeError)
         if clean_mark < 1 or clean_mark > 10:
-            print(TypeError)
-            
+            print(TypeError)          
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float,
-                              clean_power: int, average_rating: float,
-                              count_of_ratings: int) -> None:
+    def __init__(self, distance_from_city_center: float, clean_power: int, 
+                 average_rating: float, count_of_ratings: int) -> None:
         if distance_from_city_center < 1 or distance_from_city_center > 10:
             print(TypeError)
         if average_rating < 1 or average_rating > 5:
             print(TypeError)
-            
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = round(average_rating, 1)
