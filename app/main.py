e7305e3819b633b8354f9974996c59e9a4c302f8
Ostap Ivakh <o.ivakh95@gmail.com>
2023-08-06 22:10:25 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,48 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str
+                 ) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int
+                 ) -> None:
+        self.income = 0
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, cars_list: list) -> float:
+        for car in cars_list:
+            if car.clean_mark < self.clean_power:
+                self.income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
+
+        return round(self.income, 1)
+
+    def calculate_washing_price(self, car: Car) -> float:
+        washing_price = car.comfort_class * \
+            (self.clean_power - car.clean_mark) * \
+            self.average_rating / self.distance_from_city_center
+
+        return round(washing_price, 1)
+
+    def wash_single_car(self, car: Car) -> None:
+        if self.clean_power > car.clean_mark:
+            car.clean_mark = self.clean_power
+
+    def rate_service(self, mark: int) -> None:
+        tmp = round(((self.average_rating * self.count_of_ratings + mark)
+                    / (self.count_of_ratings + 1)), 1)
+        self.average_rating = tmp
+        self.count_of_ratings += 1
