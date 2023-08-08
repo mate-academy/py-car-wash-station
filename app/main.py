5aaa109221ecc391efa1b11ccaf79d0e3f51f924
Tymur Yermolenko <wonnnakoo@gmail.com>
2023-08-08 20:57:12 +0300

    fix indentation

--- app/main.py
+++ app/main.py
@@ -1,21 +1,14 @@
 class Car:
-    def __init__(self,
-                 comfort_class: int,
-                 clean_mark: int,
-                 brand: str
-                 ) -> None:
+    def __init__(self, comfort_class: int,
+                 clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self,
-                 distance_from_city_center: float,
-                 clean_power: int,
-                 average_rating: float,
-                 count_of_ratings: int
-                 ) -> None:
+    def __init__(self, distance_from_city_center: float, clean_power: int,
+                 average_rating: float, count_of_ratings: int) -> None:
         self.distance = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -44,9 +37,7 @@ class CarWashStation:
     def rate_service(self, rate: int) -> None:
         self.count_of_ratings += 1
 
-        new_rate = ((self.average_rating
-                    * (self.count_of_ratings - 1)
-                    + rate)
-                    / self.count_of_ratings
-                    )
+        new_rate = (self.average_rating * (self.count_of_ratings - 1) + rate)
+        new_rate /= self.count_of_ratings
+
         self.average_rating = round(new_rate, 1)
