db0117ca3faeaadb25b15cfb30bab338e878a7f9
Oleh Chernous <chernous.oleh@gmail.com>
2023-08-08 18:14:47 +0200

    fixed annotations, fixed 'serve_cars' function (DRY), added check condition to 'wash_single_car' function

--- app/main.py
+++ app/main.py
@@ -23,18 +23,12 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         income = 0
 
         for car in cars:
             if car.clean_mark < self.clean_power:
-                income += round(
-                    car.comfort_class
-                    * (self.clean_power - car.clean_mark)
-                    * self.average_rating
-                    / self.distance_from_the_city_center,
-                    1,
-                )
+                income += self.calculate_washing_price(car)
                 CarWashStation.wash_single_car(self, car)
 
         return income
@@ -51,7 +45,8 @@ class CarWashStation:
         return income
 
     def wash_single_car(self, car: Car) -> None:
-        car.clean_mark = self.clean_power
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
 
     def rate_service(self, rate: int | float) -> None:
         new_rating = (rate + self.average_rating * self.count_of_ratings) / (
