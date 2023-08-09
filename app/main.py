cded259e27032521fbbd497f33f6b8f9329939cc
Naholiuk Yevhen <e.nagolyuk@gmail.com>
2023-08-09 12:51:42 +0300

    Deleted print and fixed visibility of long string

--- app/main.py
+++ app/main.py
@@ -25,14 +25,13 @@ class CarWashStation:
             if car.clean_mark < self.clean_power:
                 income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
-                print(self.calculate_washing_price(car))
         return round(income, 1)
 
     def calculate_washing_price(self, car_calculate: Car) -> float:
-        return round(
-            car_calculate.comfort_class * (self.clean_power
-                                           - car_calculate.clean_mark)
-            * (self.average_rating / self.distance_from_city_center), 1)
+        return round(car_calculate.comfort_class
+                     * (self.clean_power - car_calculate.clean_mark)
+                     * self.average_rating
+                     / self.distance_from_city_center, 1)
 
     def wash_single_car(self, car_wash: Car) -> None:
         if self.clean_power > car_wash.clean_mark:
