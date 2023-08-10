69d1e086d77d7a69f467655a6a8cb33f21066fbf
Illia <kononenko2109@gmail.com>
2023-08-10 14:59:56 +0300

    Implemented py-car-wash-station Corrected indentation and type annotation

--- app/main.py
+++ app/main.py
@@ -1,20 +1,27 @@
 class Car:
-    def __init__(self, comfort_class: int,
-                 clean_mark: int, brand: str) -> None:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int,
-                 average_rating: float, count_of_ratings: int) -> None:
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         total_income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
@@ -24,9 +31,12 @@ class CarWashStation:
         return round(total_income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round((car.comfort_class * (
-            self.clean_power - car.clean_mark) * (
-            self.average_rating / self.distance_from_city_center)), 1)
+        return round(
+            (car.comfort_class
+             * (self.clean_power - car.clean_mark)
+             * (self.average_rating / self.distance_from_city_center)),
+            1
+        )
 
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
