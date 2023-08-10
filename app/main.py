f2b32889d5a5333de763c8749228928e9f9bf0d2
Yaroslav Mazurkevych <yaroslavmazurkevych@gmail.com>
2023-08-10 16:21:09 +0300

    py-car-wash-station

--- app/main.py
+++ app/main.py
@@ -2,12 +2,12 @@ from typing import Callable, Any
 
 
 class Car:
+
     def __init__(
             self,
             comfort_class: int,
             clean_mark: int,
-            brand: str
-    ) -> None:
+            brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -19,25 +19,21 @@ class CarWashStation:
             distance_from_city_center: float,
             clean_power: int,
             average_rating: float,
-            count_of_ratings: int
-    ) -> None:
+            count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, cars: list) -> float:
-
         income = 0
         for clean_car in cars:
             if self.check_clean_mark_lower_then_power(clean_car):
                 income += self.calculate_washing_price(clean_car)
                 self.wash_single_car(clean_car)
-
         return income
 
     def calculate_washing_price(self, cars: Any) -> float:
-
         income = cars.comfort_class * (self.clean_power - cars.clean_mark)\
             * self.average_rating / self.distance_from_city_center
 
@@ -47,8 +43,7 @@ class CarWashStation:
         return car.clean_mark < self.clean_power
 
     def wash_single_car(self, car: Callable) -> None:
-
-        if self.check_clean_mark_lower_then_power(Car):
+        if self.check_clean_mark_lower_then_power(car):
             car.clean_mark = self.clean_power
 
     def rate_service(self, rate: float) -> None:
