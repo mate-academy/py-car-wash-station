0618f2508bb48c6314bdf057103da72501353251
Davydova Olha <o.chookity@gmail.com>
2023-08-10 12:09:21 +0300

    Refactor init methods

--- app/main.py
+++ app/main.py
@@ -2,9 +2,12 @@ from __future__ import annotations
 
 
 class Car:
-    def __init__(self, comfort_class: int,
-                 clean_mark: int,
-                 brand: str) -> None:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
 
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
@@ -12,10 +15,13 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float,
-                 clean_power: int,
-                 average_rating: float,
-                 count_of_ratings: int) -> None:
+    def __init__(
+            self,
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
+    ) -> None:
 
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
