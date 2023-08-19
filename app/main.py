40228bb24f5a26326ce9f66556ff7330d2247292
VasylTurok <127683195+VasylTurok@users.noreply.github.com>
2023-08-19 13:13:56 +0300

    Update app/main.py
    Co-authored-by: Viktoria Pavlej <98127979+pavlejviki@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -16,7 +16,8 @@ class CarWashStation:
             distance_from_city_center: float,
             clean_power: int,
             average_rating: float,
-            count_of_ratings: int) -> None:
+            count_of_ratings: int
+    ) -> None:
 
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
