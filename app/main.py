1b67f5e6115bcb1bc7264744231559fd23e06e2f
Gulhaya Kulsahatowa <gulhayakulsahatowa@gmail.com>
2023-08-19 20:51:59 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,18 +1,19 @@
+from typing import List
 class Car:
-    def __init__(self, comfort_class, clean_mark, brand):
+    def __init__(self, comfort_class: float, clean_mark: float, brand: str):
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center, clean_power, average_rating, count_of_ratings):
+    def __init__(self, distance_from_city_center: int, clean_power: float, average_rating: float, count_of_ratings: int):
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, *args):
+    def serve_cars(self, *args: Car) -> float:
         count_car_list = []
         for car in args[0]:
             if car.clean_mark < self.clean_power:
@@ -23,15 +24,15 @@ class CarWashStation:
         print(count_car_list)
         return count_car
 
-    def calculate_washing_price(self, car):
+    def calculate_washing_price(self, car: Car) -> float:
         price = (car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating) / self.distance_from_city_center
         return round(price, 1)
 
-    def wash_single_car(self, car):
+    def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
-    def rate_service(self, rating):
+    def rate_service(self, rating: float) -> float:
         self.average_rating = round((self.average_rating * self.count_of_ratings + rating) / (self.count_of_ratings + 1), 1)
         self.count_of_ratings += 1
         return self.average_rating
