404abf3b3fb3bee749a45b76ee0dbee0e42aa431
AndriiYe <andrii.yevmenov@gmail.com>
2023-08-22 22:31:51 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,18 +1,21 @@
 class Car:
-    def __init__(self, comfort_class: int,
-                 clean_mark: int,
-                 brand: str) -> None:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self,
-                 distance_from_city_center: float,
-                 clean_power: int,
-                 average_rating: float,
-                 count_of_ratings: int) -> None:
+    def __init__(
+            self,
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -30,13 +33,13 @@ class CarWashStation:
         return round(car.comfort_class
                      * (self.clean_power - car.clean_mark)
                      * (self.average_rating
-                         / self.distance_from_city_center), 1)
+                        / self.distance_from_city_center), 1)
 
     def wash_single_car(self, car: object) -> None:
         car.clean_mark = self.clean_power
 
     def rate_service(self, rate: float) -> None:
         self.average_rating = round((self.average_rating
-                                    * self.count_of_ratings + rate)
+                                     * self.count_of_ratings + rate)
                                     / (self.count_of_ratings + 1), 1)
         self.count_of_ratings += 1
