4b148f7f024bf9d57d81f5e5fe67498815473ab3
Depaton <you@example.com>
2023-08-23 12:51:11 +0300

    Solution with Formatting

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 class Car:
-    def __init__(self,
-                 comfort_class: int,
-                 clean_mark: int,
-                 brand: str) -> None:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
 
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
@@ -10,11 +12,13 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self,
-                 distance_from_city_center: float,
-                 clean_power: int,
-                 average_rating: float,
-                 count_of_ratings: int) -> None:
+    def __init__(
+            self,
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
+    ) -> None:
 
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
@@ -33,13 +37,13 @@ class CarWashStation:
         car.clean_mark = self.clean_power
 
     def calculate_washing_price(self, car: Car) -> float:
-        return car.comfort_class * (self.clean_power - car.clean_mark) \
+        return car.comfort_class \
+            * (self.clean_power - car.clean_mark) \
             * (self.average_rating / self.distance_from_city_center)
 
     def rate_service(self, rating: int) -> None:
-        self.average_rating = round((self.old_rating() + rating)
-                                    / (self.count_of_ratings + 1), 1)
+        old_rating = self.average_rating * self.count_of_ratings
+        self.average_rating = round(
+            (old_rating + rating) / (self.count_of_ratings + 1), 1
+        )
         self.count_of_ratings += 1
-
-    def old_rating(self) -> float:
-        return self.average_rating * self.count_of_ratings
