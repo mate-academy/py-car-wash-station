ba75bc9ad6c4551dc87ef3956c8aeb3612bd2ebe
AndriiYe <andrii.yevmenov@gmail.com>
2023-08-23 16:43:42 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
+from __future__ import annotations
+
+
 class Car:
     def __init__(
             self,
             comfort_class: int,
             clean_mark: int,
-            brand: str) -> None:
+            brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -15,7 +19,8 @@ class CarWashStation:
             distance_from_city_center: float,
             clean_power: int,
             average_rating: float,
-            count_of_ratings: int) -> None:
+            count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -29,17 +34,16 @@ class CarWashStation:
                 self.wash_single_car(car)
         return round(income, 1)
 
-    def calculate_washing_price(self, car: object) -> float:
-        return round(car.comfort_class
-                     * (self.clean_power - car.clean_mark)
+    def calculate_washing_price(self, car: Car) -> float:
+        return round(car.comfort_class * (self.clean_power - car.clean_mark)
                      * (self.average_rating
-                        / self.distance_from_city_center), 1)
+                     / self.distance_from_city_center), 1)
 
-    def wash_single_car(self, car: object) -> None:
+    def wash_single_car(self, car: Car) -> None:
         car.clean_mark = self.clean_power
 
     def rate_service(self, rate: float) -> None:
         self.average_rating = round((self.average_rating
-                                     * self.count_of_ratings + rate)
+                                    * self.count_of_ratings + rate)
                                     / (self.count_of_ratings + 1), 1)
         self.count_of_ratings += 1
