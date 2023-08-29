e029585f82867bab239178a43bdb78043f3cd6cb
Elena Komarova <elenkomarova4@gmail.com>
2023-08-29 17:33:27 +0300

    'Solution_4'

--- app/main.py
+++ app/main.py
@@ -7,12 +7,18 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int,
-                 average_rating: float, count_of_ratings: int) -> None:
-        self.distance_from_city_center = distance_from_city_center
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int
+                 ) -> None:
+        self.distance_from_city_center = \
+            distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
-        self.count_of_ratings = count_of_ratings
+        self.count_of_ratings = \
+            count_of_ratings
 
     def serve_cars(self, cars: list) -> float:
         total_income = 0
