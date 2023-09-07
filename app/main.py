632bf2aa4e31765c5af64cee820702369b3a5cbb
Stepan Shevchuk <stepka.shevchuk@gmail.com>
2023-09-07 22:38:12 +0100

    wash-station

--- app/main.py
+++ app/main.py
@@ -17,18 +17,17 @@ class CarWashStation:
     def serve_cars(self, cars: list[Car]) -> float:
         income = 0
         for car in cars:
-            income += self.calculate_washing_price(car)
-            self.wash_single_car(car)
+            if car.clean_mark < self.clean_power:
+                income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
         return income
 
     def calculate_washing_price(self, car: Car) -> float:
-        if car.clean_mark < self.clean_power:
-            income = ((self.clean_power
-                       - car.clean_mark)
-                      * car.comfort_class
-                      * self.average_rating / self.distance)
-            return round(income, 1)
-        return 0
+        income = ((self.clean_power
+                   - car.clean_mark)
+                  * car.comfort_class
+                  * self.average_rating / self.distance)
+        return round(income, 1)
 
     def rate_service(self, mark: int) -> None:
         self.average_rating = round((self.average_rating
@@ -37,5 +36,4 @@ class CarWashStation:
         self.count_of_ratings += 1
 
     def wash_single_car(self, car: Car) -> None:
-        if car.clean_mark < self.clean_power:
-            car.clean_mark = self.clean_power
+        car.clean_mark = self.clean_power
