1f3893869368d14f2a6679bd0d238c0e73bae0a1
Stepan Shevchuk <stepka.shevchuk@gmail.com>
2023-09-08 13:56:14 +0100

    wash-station

--- app/main.py
+++ app/main.py
@@ -36,4 +36,5 @@ class CarWashStation:
         self.count_of_ratings += 1
 
     def wash_single_car(self, car: Car) -> None:
-        car.clean_mark = self.clean_power
+        if self.clean_power > car.clean_mark:
+            car.clean_mark = self.clean_power
