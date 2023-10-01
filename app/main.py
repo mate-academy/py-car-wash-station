dffeb3dc8ceeea71771b7bcd059945aefddbe0fe
Dmytro Petrykiv <111526221+petrykivd@users.noreply.github.com>
2023-10-01 15:54:38 +0300

    Update main.py [flake8 trouble]

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ class Car:
 class CarWashStation:
     def __init__(self,
                  distance_from_city_center: float,
-                 clean_power: int, 
+                 clean_power: int,
                  average_rating: float,
                  count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
