ff87d72a55a48d9b05abbc6ef13bc06e02f66e77
Kostia <kostia3507@gmail.com>
2023-10-02 20:38:39 +0300

    Fixed formatting

--- app/main.py
+++ app/main.py
@@ -1,28 +1,31 @@
 class Car:
-    def __init__(self,
-                 comfort_class: int,
-                 clean_mark: int,
-                 brand: str
-                 ) -> None:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self,
-                 distance_from_city_center: float,
-                 clean_power: int,
-                 average_rating: float,
-                 count_of_ratings: int
-                 ) -> None:
+    def __init__(
+            self,
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
-        self.income = 0.0
 
     def serve_cars(self, list_of_cars: list) -> float:
+        self.income = 0.0
+
         for car in list_of_cars:
             self.wash_single_car(car)
         return self.income
