bf27c1c2ecfa2cfef0acf023ff3d85d2fa0899e4
Сергей <kovalenkoserhii33@gmail.com>
2023-10-02 20:20:23 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 class Car:
     def __init__(self, comfort_class: int,
-                 clean_mark: int, brand: str) -> None:
+                 clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -8,7 +9,8 @@ class Car:
 
 class CarWashStation:
     def __init__(self, distance_from_city_center: float,
-                 clean_power: int, average_rating: float,
+                 clean_power: int,
+                 average_rating: float,
                  count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
@@ -19,26 +21,24 @@ class CarWashStation:
         income = 0.0
         for car in car_list:
             if car.clean_mark < self.clean_power:
-                wash_cost = self.calculate_washing_price(car)
-                income += wash_cost
+                income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
         return round(income, 1)
 
-    def calculate_washing_price(self, car: str) -> None:
+    def calculate_washing_price(self, car: Car) -> float:
         price = (
             car.comfort_class * (self.clean_power - car.clean_mark)
             * self.average_rating / self.distance_from_city_center
         )
         return round(price, 1)
 
-    def wash_single_car(self, car: list) -> None:
+    def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
     def rate_service(self, rating: float) -> float:
         self.average_rating = round(
             (self.average_rating * self.count_of_ratings + rating)
-            / (self.count_of_ratings + 1),
-            1  # Round to one decimal place
+            / (self.count_of_ratings + 1), 1
         )
         self.count_of_ratings += 1
