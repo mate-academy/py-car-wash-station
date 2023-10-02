5f8936b74bcbc7284930ed183be841750f7926cd
sakhaline <alexandra.ivva@gmail.com>
2023-10-02 08:20:56 +0300

    Income attribute is made as CarWashStation attribute; if statement is fixed

--- app/main.py
+++ app/main.py
@@ -16,12 +16,11 @@ class CarWashStation:
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
-
-    def serve_cars(self, cars: list) -> float:
         self.income = 0.0
 
+    def serve_cars(self, cars: list) -> float:
         for car in cars:
-            if car.clean_mark <= self.clean_power:
+            if car.clean_mark < self.clean_power:
                 self.income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
 
@@ -29,8 +28,10 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         return round(
-            car.comfort_class * (self.clean_power - car.clean_mark)
-            * (self.average_rating / self.distance_from_city_center), 1)
+            car.comfort_class
+            * (self.clean_power - car.clean_mark)
+            * (self.average_rating / self.distance_from_city_center),
+            1)
 
     def wash_single_car(self, car: Car) -> None:
         car.clean_mark = self.clean_power
@@ -38,5 +39,6 @@ class CarWashStation:
     def rate_service(self, rate: int) -> None:
         self.average_rating = round((self.count_of_ratings
                                      * self.average_rating + rate)
-                                    / (self.count_of_ratings + 1), 1)
+                                    / (self.count_of_ratings + 1),
+                                    1)
         self.count_of_ratings += 1
