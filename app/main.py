be35adbb221612bfc339230cc706dd8fe63b21f8
Dmytro Pyrih <dima.purig@gmail.com>
2023-10-02 19:38:54 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int,
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
                  brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
@@ -7,20 +9,24 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int,
-                 average_rating: float, count_of_ratings: int) -> None:
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
     def calculate_washing_price(self, car: Car) -> float:
-        return (car.comfort_class * abs(car.clean_mark - self.clean_power)
-                * self.average_rating / self.distance_from_city_center)
+        wsh_price = (car.comfort_class * abs(car.clean_mark - self.clean_power)
+                     * self.average_rating / self.distance_from_city_center)
+        return round(wsh_price, 1)
 
-    def wash_single_car(self, car: Car) -> Car:
-        car.clean_mark = self.clean_power
-        return car
+    def wash_single_car(self, car: Car) -> None:
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
 
     def serve_cars(self, cars: list) -> float:
         money_income = 0
