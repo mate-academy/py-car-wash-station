457b6614849ada37c732a34c3e5c1736b88e5c16
Severyn Krok <142496674+SevKrok@users.noreply.github.com>
2023-10-03 15:33:30 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -32,8 +32,8 @@ class CarWashStation:
         return round(
             car.comfort_class * self.average_rating
             * (self.clean_power - car.clean_mark)
-            / self.distance_from_city_center,
-            1)
+            / self.distance_from_city_center, 1
+        )
 
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
@@ -42,6 +42,6 @@ class CarWashStation:
     def rate_service(self, mark: int) -> None:
         self.average_rating = round(
             (self.average_rating * self.count_of_ratings + mark)
-            / (self.count_of_ratings + 1),
-            1)
+            / (self.count_of_ratings + 1), 1
+        )
         self.count_of_ratings += 1
