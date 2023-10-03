dc7dbf87999fdf9fc9ac98239dc1ef15c8cee09d
Severyn Krok <sevkrok@gmail.com>
2023-10-03 15:36:14 +0300

    fix formatting

--- app/main.py
+++ app/main.py
@@ -13,7 +13,8 @@ class CarWashStation:
                  distance_from_city_center: float,
                  clean_power: int,
                  average_rating: float,
-                 count_of_ratings: int) -> None:
+                 count_of_ratings: int
+                 ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -32,8 +33,8 @@ class CarWashStation:
         return round(
             car.comfort_class * self.average_rating
             * (self.clean_power - car.clean_mark)
-            / self.distance_from_city_center,
-            1)
+            / self.distance_from_city_center, 1
+        )
 
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
@@ -42,6 +43,6 @@ class CarWashStation:
     def rate_service(self, mark: int) -> None:
         self.average_rating = round(
             (self.average_rating * self.count_of_ratings + mark)
-            / (self.count_of_ratings + 1),
-            1)
+            / (self.count_of_ratings + 1), 1
+        )
         self.count_of_ratings += 1
