2f1131f9722055c0fc6ed4ab4e0555a986a78dc2
Serhii <martinenko.sergiy@gmail.com>
2023-10-04 20:18:15 +0300

    return annotations fixed

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
-from typing import Any
-
-
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> Any:
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -20,7 +20,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list) -> Any:
+    def serve_cars(self, cars: list) -> float:
         income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
@@ -34,11 +34,11 @@ class CarWashStation:
                      * self.average_rating
                      / self.distance_from_city_center, 1)
 
-    def wash_single_car(self, car: Car) -> Any:
+    def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
-    def rate_service(self, rating: int) -> Any:
+    def rate_service(self, rating: int) -> None:
         new_average = ((self.average_rating
                         * self.count_of_ratings
                         + rating)
