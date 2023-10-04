e75a2ddb77298be236c9ec7d74197fad8bc85bfe
unknown <niki27042005@gmail.com>
2023-10-04 15:01:19 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,19 +1,27 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> str:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: int, clean_power: int,
-                 average_rating: int, count_of_ratings: int) -> str:
+    def __init__(
+            self,
+            distance_from_city_center: int,
+            clean_power: int,
+            average_rating: int,
+            count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: str) -> str:
+    def serve_cars(self, cars: list) -> float:
         total_income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
@@ -22,7 +30,7 @@ class CarWashStation:
                 self.wash_single_car(car)
         return round(total_income, 1)
 
-    def calculate_washing_price(self, car: str) -> str:
+    def calculate_washing_price(self, car: list) -> float:
         return round(
             car.comfort_class
             * (self.clean_power - car.clean_mark)
@@ -30,11 +38,11 @@ class CarWashStation:
             1
         )
 
-    def wash_single_car(self, car: str) -> str:
+    def wash_single_car(self, car: list) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
-    def rate_service(self, rating: int) -> str:
+    def rate_service(self, rating: int) -> None:
         self.average_rating = round(
             (self.average_rating * self.count_of_ratings + rating)
             / (self.count_of_ratings + 1),
