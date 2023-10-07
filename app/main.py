57b837bf06b8cd6e5a8919007d0e95da4f9a6973
Ilya Grynyshyn <grynyshyn080@gmail.com>
2023-10-07 14:54:33 +0300

    Fix parameters position

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 class Car:
     def __init__(
-            self, comfort_class: int, clean_mark: int, brand: str
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
     ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
@@ -9,8 +12,11 @@ class Car:
 
 class CarWashStation:
     def __init__(
-            self, distance_from_city_center: float,
-            clean_power: int, average_rating: float, count_of_ratings : int
+            self,
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
     ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
@@ -27,7 +33,7 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         return round(car.comfort_class * (
-            self.clean_power - car.clean_mark
+                self.clean_power - car.clean_mark
         ) * self.average_rating / self.distance_from_city_center, 1)
 
     def wash_single_car(self, car: Car) -> None:
@@ -37,6 +43,6 @@ class CarWashStation:
     def rate_service(self, rate: float) -> None:
         self.average_rating = round(
             (self.average_rating * self.count_of_ratings + rate) / (
-                self.count_of_ratings + 1
+                    self.count_of_ratings + 1
             ), 1)
         self.count_of_ratings += 1
