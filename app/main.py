f6ec70c1df7a32d3417aa6cd479b8af34eb9eb50
Ilya Grynyshyn <grynyshyn080@gmail.com>
2023-10-07 15:02:49 +0300

    Replace annotation & fix formatting

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, list_cars: list) -> int:
+    def serve_cars(self, list_cars: list[Car]) -> float:
         income = 0
         for car in list_cars:
             if car.clean_mark < self.clean_power:
@@ -32,9 +32,11 @@ class CarWashStation:
         return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round(car.comfort_class * (
-                self.clean_power - car.clean_mark
-        ) * self.average_rating / self.distance_from_city_center, 1)
+        return round(
+            car.comfort_class
+            * (self.clean_power - car.clean_mark)
+            * self.average_rating / self.distance_from_city_center, 1
+        )
 
     def wash_single_car(self, car: Car) -> None:
         if self.clean_power >= car.clean_mark:
@@ -42,7 +44,6 @@ class CarWashStation:
 
     def rate_service(self, rate: float) -> None:
         self.average_rating = round(
-            (self.average_rating * self.count_of_ratings + rate) / (
-                    self.count_of_ratings + 1
-            ), 1)
+            (self.average_rating * self.count_of_ratings + rate)
+            / (self.count_of_ratings + 1), 1)
         self.count_of_ratings += 1
