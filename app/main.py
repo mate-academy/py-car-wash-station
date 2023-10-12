4bfcf8b6fd0855c1eede26d5836706372f18b977
Yuliia Dudych <juliadudych@gmail.com>
2023-10-12 11:27:32 +0300

    Define CarWashStation class

--- app/main.py
+++ app/main.py
@@ -11,5 +11,14 @@ class Car:
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(
+            self,
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
+    ) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
