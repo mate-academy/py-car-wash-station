1615b38db7f0d970727a25ed6108055dca781efa
Grigoriy Gesiod <g.gesiod@gmail.com>
2023-10-13 17:12:13 +0300

    The closing bracket is also written on a new line

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ class Car:
             self,
             comfort_class: int,
             clean_mark: int,
-            brand: str) -> None:
+            brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -24,7 +25,8 @@ class CarWashStation:
 
     def serve_cars(
             self,
-            cars: list[Car]) -> float:
+            cars: list[Car]
+    ) -> float:
         income = 0
         for car in cars:
             if self.clean_power > car.clean_mark:
@@ -34,7 +36,8 @@ class CarWashStation:
 
     def calculate_washing_price(
             self,
-            car: Car) -> float:
+            car: Car
+    ) -> float:
         price = (
             car.comfort_class
             * (self.clean_power - car.clean_mark)
@@ -45,13 +48,15 @@ class CarWashStation:
 
     def wash_single_car(
             self,
-            car: Car) -> None:
+            car: Car
+    ) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
     def rate_service(
             self,
-            rate: float) -> None:
+            rate: float
+    ) -> None:
         self.count_of_ratings += 1
         new_rating = (
             self.average_rating * (self.count_of_ratings - 1) + rate
