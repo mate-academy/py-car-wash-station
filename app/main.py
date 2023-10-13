4d9cebb00d2434d7403183f88c5c6e0f34a19bcf
Denysov Viacheslav <wdenisov113@gmail.com>
2023-10-13 15:33:34 +0300

    Solution2

--- app/main.py
+++ app/main.py
@@ -1,23 +1,32 @@
 class Car:
-    def __init__(self, comfort_class: int,
-                 clean_mark: int, brand: str) -> None:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: int, clean_power: int,
-                 average_rating: int, count_of_ratings: int) -> None:
+    def __init__(
+            self,
+            distance_from_city_center: int,
+            clean_power: int,
+            average_rating: int,
+            count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
     def calculate_washing_price(self, car: Car) -> float:
-        clean = self.clean_power - car.clean_mark
-        rat_dist = self.average_rating / self.distance_from_city_center
-        return round(car.comfort_class * clean * rat_dist, 1)
+        clean_car = self.clean_power - car.clean_mark
+        distance_to_rating_ratio = self.average_rating / self.distance_from_city_center
+        return round(car.comfort_class * clean_car * distance_to_rating_ratio, 1)
 
     def wash_single_car(self, car: Car) -> None:
         if self.clean_power >= car.clean_mark:
@@ -29,8 +38,8 @@ class CarWashStation:
         else:
             self.count_of_ratings += 1
             total_rating = self.average_rating * (self.count_of_ratings - 1)
-            rat = (total_rating + rating) / self.count_of_ratings
-            self.average_rating = round(rat, 1)
+            new_rating = (total_rating + rating) / self.count_of_ratings
+            self.average_rating = round(new_rating, 1)
 
     def serve_cars(self, cars: list) -> float:
         income = 0
