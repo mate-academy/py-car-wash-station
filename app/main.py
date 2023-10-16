366dec11d29444585ce7f6f0fc1eaf39d7bb0b30
Damodar <alaso.se@gmail.com>
2023-10-16 16:31:28 +0300

    fixed annotations

--- app/main.py
+++ app/main.py
@@ -20,22 +20,22 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, car_list: list) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         income = 0
-        for car in car_list:
+        for car in cars:
             if car.clean_mark < self.clean_power:
                 income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
         return income
 
-    def calculate_washing_price(self, car: list) -> float:
+    def calculate_washing_price(self, car: Car) -> float:
         return round(
             car.comfort_class * (
                 self.clean_power - car.clean_mark
             ) * self.average_rating / self.distance_from_city_center,
             1)
 
-    def wash_single_car(self, car: list) -> None:
+    def wash_single_car(self, car: Car) -> None:
         car.clean_mark = self.clean_power
 
     def rate_service(self, rating: int) -> None:
