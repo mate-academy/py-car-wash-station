703515cede4f3c90997b0190c8dc05ea92bb40c3
Liliya <liliyakhu@ukr.net>
2023-10-23 16:04:12 +0300

    make code optimization

--- app/main.py
+++ app/main.py
@@ -1,26 +1,30 @@
 class Car:
-    def __init__(self, comfort_class: int,
-                 clean_mark: int, brand: str) -> None:
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int,
-                 average_rating: float, count_of_ratings: int) -> None:
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list) -> float:
-        appropriate_cars = [car for car in cars
-                            if car.clean_mark < self.clean_power]
+    def serve_cars(self, cars: list[Car]) -> float:
         total_income = 0
-        for car in appropriate_cars:
-            total_income += self.calculate_washing_price(car)
-            self.wash_single_car(car)
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                total_income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
         return round(total_income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
