b6ec4993810fc18b3eba997f9bf1d70ccc38ecc0
Vlad <mr.darmstadtium@gmail.com>
2023-11-01 17:08:28 +0100

    add type annotations

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ class Car:
     def __init__(self,
                  comfort_class: int,
                  clean_mark: int,
-                 brand: str):
+                 brand: str) -> None:
 
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
@@ -14,14 +14,14 @@ class CarWashStation:
                  distance_from_city_center: float,
                  clean_power: int,
                  average_rating: float,
-                 count_of_ratings: int):
+                 count_of_ratings: int) -> None:
 
         self.distance_from_city_center = round(distance_from_city_center, 1)
         self.clean_power = clean_power
         self.average_rating = round(average_rating, 1)
         self.count_of_ratings = count_of_ratings
 
-    def calculate_washing_price(self, car) -> float:
+    def calculate_washing_price(self, car: Car) -> float:
 
         clean_dif = self.clean_power - car.clean_mark
         car_clas = car.comfort_class
@@ -32,11 +32,11 @@ class CarWashStation:
 
         return round(income_for_car, 1)
 
-    def wash_single_car(self, car):
+    def wash_single_car(self, car: Car) -> None:
 
         car.clean_mark = self.clean_power
 
-    def rate_service(self, mark: int):
+    def rate_service(self, mark: int) -> None:
 
         rating_sum = ((self.average_rating * self.count_of_ratings) + mark)
         self.count_of_ratings += 1
@@ -51,3 +51,7 @@ class CarWashStation:
             self.wash_single_car(car)
 
         return income
+
+
+c = Car(1, 1, "1")
+print(type(c))
