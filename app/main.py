773d236f413df9abf79a6f20539c64b57bff5a0c
Oleksii Mileiko <alexexile1987@gmail.com>
2023-11-06 04:40:40 +0200

    Add calculate_washing_price

--- app/main.py
+++ app/main.py
@@ -22,3 +22,9 @@ class CarWashStation:
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
+
+    def calculate_washing_price(self, car: Car) -> float:
+        return \
+            car.comfort_class * \
+            (self.clean_power - car.clean_mark) * \
+            self.average_rating / self.distance_from_city_center
