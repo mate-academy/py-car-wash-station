86efd40c5eea552815fc72ed3b44299cd55c0c7c
Mikola Rozhansky <bymikola@gmail.com>
2023-11-07 21:26:43 +0200

    init

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 class Car:
     # write your code here
-    pass
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
