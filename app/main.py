ff8826177f5d1e020b7eed03060994b3f2d3d013
Dmytro Gertsovenko <gertsovenko@gmail.com>
2023-11-13 15:28:25 +0200

    Code corrections

--- app/main.py
+++ app/main.py
@@ -1,20 +1,32 @@
 class Car:
-    def __init__(self, comfort_class: int,
-                 clean_mark: int, brand: str) -> None:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int,
-                 average_rating: float, count_of_ratings: int) -> None:
+    def __init__(
+            self,
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list[Car]) -> float:
+    def serve_cars(
+            self,
+            cars: list[Car]
+    ) -> float:
         income = 0
         for car in cars:
             if self.clean_power > car.clean_mark:
@@ -22,16 +34,26 @@ class CarWashStation:
                 self.wash_single_car(car)
         return round(income, 1)
 
-    def calculate_washing_price(self, car: Car) -> float:
+    def calculate_washing_price(
+            self,
+            car: Car
+    ) -> float:
         return (car.comfort_class
                 * (self.clean_power - car.clean_mark)
                 * self.average_rating / self.distance_from_city_center)
 
-    def wash_single_car(self, car: Car) -> None:
+    def wash_single_car(
+            self,
+            car: Car
+    ) -> None:
         car.clean_mark = self.clean_power
 
-    def rate_service(self, mark: int) -> None:
+    def rate_service(
+            self,
+            mark: int
+    ) -> None:
         self.average_rating = (
             round((self.average_rating * self.count_of_ratings + mark)
-                  / (self.count_of_ratings + 1), 1))
+                  / (self.count_of_ratings + 1), 1)
+        )
         self.count_of_ratings += 1
