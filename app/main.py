5f50d3a5d6f9183be5ddf60379623e920cfa746d
Viktor Shcherbak <dev.psychologist@gmail.com>
2023-11-13 20:19:12 +0200

    Task after fixing!

--- app/main.py
+++ app/main.py
@@ -1,15 +1,23 @@
 class Car:
-    def __init__(self, comfort_class: int,
-                 clean_mark: int, brand: str) -> None:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: int,
-                 clean_power: int, average_rating: float,
-                 count_of_ratings: int) -> None:
+    def __init__(
+            self,
+            distance_from_city_center: int,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -27,8 +35,9 @@ class CarWashStation:
     def rate_service(self, rate: int) -> None:
         total_rating = self.average_rating * self.count_of_ratings
         self.count_of_ratings += 1
-        self.average_rating = round((total_rating + rate)
-                                    / self.count_of_ratings, 1)
+        self.average_rating = round(
+            (total_rating + rate) / self.count_of_ratings, 1
+        )
 
     def serve_cars(self, cars: list[Car]) -> float:
         income = 0.0
@@ -36,7 +45,6 @@ class CarWashStation:
         for car in cars:
             if self.wash_single_car(car):
                 income += self.calculate_washing_price(car)
-                # self.rate_service(car.comfort_class)
                 car.clean_mark = self.clean_power
 
         return income
