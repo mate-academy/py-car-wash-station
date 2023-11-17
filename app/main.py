0f34a16c04213299a7d499988586d8b78ccabcdd
Zoresław Dmytryszyn <zoreslav.dmytryshyn@gmail.com>
2023-11-17 13:00:31 +0100

    change type annotations, move each parameter on a new line

--- app/main.py
+++ app/main.py
@@ -1,22 +1,23 @@
-from typing import Any
-
-
 class Car:
-    def __init__(self,
-                 comfort_class: int,
-                 clean_mark: int,
-                 brand: str) -> None:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self,
-                 distance_from_city_center: float,
-                 clean_power: int,
-                 average_rating: float,
-                 count_of_ratings: int) -> None:
+    def __init__(
+            self,
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
+    ) -> None:
 
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
@@ -31,11 +32,11 @@ class CarWashStation:
                 self.wash_single_car(car)
         return income
 
-    def calculate_washing_price(self, car: Any) -> float:
+    def calculate_washing_price(self, car: Car) -> float:
         return round(car.comfort_class * (self.clean_power - car.clean_mark)
                      * self.average_rating / self.distance_from_city_center, 1)
 
-    def wash_single_car(self, car: Any) -> None:
+    def wash_single_car(self, car: Car) -> None:
         car.clean_mark = self.clean_power
 
     def rate_service(self, rate: int) -> None:
