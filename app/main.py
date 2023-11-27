5c8235c23e0be562177a812c015d1ed697fe800f
Oleksandr <gubar3113@gmail.com>
2023-11-27 23:39:34 +0200

    wash_solution_2

--- app/main.py
+++ app/main.py
@@ -2,7 +2,11 @@ from typing import List
 
 
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str
+                 ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -28,8 +32,9 @@ class CarWashStation:
         return total_income
 
     def calculate_washing_price(self, car: Car) -> float:
-        washing_price = (car.comfort_class *
-                         (self.clean_power - car.clean_mark) * self.average_rating / self.distance_from_city_center)
+        diff_mark = self.clean_power - car.clean_mark
+        washing_price = car.comfort_class * diff_mark * self.average_rating
+        washing_price /= self.distance_from_city_center
         return round(washing_price, 1)
 
     def wash_single_car(self, car: Car) -> None:
@@ -37,6 +42,8 @@ class CarWashStation:
             car.clean_mark = self.clean_power
 
     def rate_service(self, rating: float) -> None:
-        self.average_rating = ((self.average_rating * self.count_of_ratings + rating) / (self.count_of_ratings + 1))
+        self.average_rating = self.average_rating * self.count_of_ratings
+        self.average_rating += rating
+        self.average_rating /= (self.count_of_ratings + 1)
         self.average_rating = round(self.average_rating, 1)
         self.count_of_ratings += 1
