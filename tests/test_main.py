edaf8838a336ffa3c05eb180ba50db20e4a21dd4
Oleksandr <gubar3113@gmail.com>
2023-12-04 21:32:24 +0200

    wash solution 5

--- tests/test_main.py
+++ tests/test_main.py
@@ -39,7 +39,6 @@ def test_car_wash_station(cars, wash_station, total_cost):
     income = wash_station.serve_cars(cars)
     assert income == total_cost, f"Income should equal to {total_cost}"
 
-
 def test_wash_single_car_is_called():
     with patch.object(CarWashStation, 'wash_single_car') as mock_method:
         CarWashStation(3, 9, 4, 11).serve_cars([Car(2, 1, "Ford")])
