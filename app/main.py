f4beca706ca32cf125bee8d0c00a0ddbb2625b48
Max <bidun.max@gmail.com>
2023-12-05 23:12:05 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,45 @@
+from typing import Any
+
+
 class Car:
-    # write your code here
-    pass
+    def __init__(self, comfort_class: int,
+                 clean_mark: int, brand: str) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self, distance_from_city_center: float, clean_power: int,
+                 average_rating: float, count_of_ratings: int) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def wash_single_car(self, car: Any) -> bool:
+        if car.clean_mark < self.clean_power:
+            return True
+        else:
+            return False
+
+    def calculate_washing_price(self, car: Any) -> float:
+        return round(car.comfort_class * (self.clean_power - car.clean_mark)
+                     * self.average_rating
+                     / self.distance_from_city_center, 1)
+
+    def rate_service(self, number: int) -> None:
+        if 1 <= number <= 5:
+            total_rating = self.average_rating * self.count_of_ratings
+            total_rating += number
+            self.count_of_ratings += 1
+            self.average_rating = total_rating / self.count_of_ratings
+            self.average_rating = round(self.average_rating, 1)
+
+    def serve_cars(self, car_list: list) -> int:
+        price_car = 0
+        for car in list(car_list):
+            if self.wash_single_car(car):
+                price_car += self.calculate_washing_price(car)
+                car.clean_mark = self.clean_power
+        return price_car
