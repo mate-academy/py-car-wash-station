590f2fca7b4ee2f26745ef4b8022f23af799aec4
Oleksandr <gubar3113@gmail.com>
2023-12-05 20:59:37 +0200

    wash solution 6

--- app/main.py
+++ app/main.py
@@ -14,12 +14,13 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self,
-                 distance_from_city_center: float,
-                 clean_power: int,
-                 average_rating: float,
-                 count_of_ratings: int
-                 ) -> None:
+    def __init__(
+            self,
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -43,9 +44,9 @@ class CarWashStation:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
-    def rate_service(self, rt: float) -> None:
+    def rate_service(self, rate: float) -> None:
         self.average_rating *= self.count_of_ratings
-        self.average_rating += rt
+        self.average_rating += rate
         self.average_rating /= (self.count_of_ratings + 1)
         self.average_rating = round(self.average_rating, 1)
         self.count_of_ratings += 1
