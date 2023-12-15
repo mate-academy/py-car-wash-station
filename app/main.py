aaed8c2c086e76eff029f4db565c0e8c7d757a53
xrkxdd <nazar.mc28@gmail.com>
2023-12-15 12:31:39 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,48 @@
+from __future__ import annotations
+
+
 class Car:
-    # write your code here
-    pass
+
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str) -> Car:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+
+    def __init__(
+            self,
+            distance_from_city_center: int,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int) -> CarWashStation:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, cars: list) -> float:
+        incomes = [self.wash_single_car(car) for car in cars]
+        incomes = sum([income for income in incomes if income])
+        return round(incomes, 1)
+
+    def calculate_washing_price(self, car: Car) -> float:
+        price = car.comfort_class * (self.clean_power - car.clean_mark) * \
+            self.average_rating / self.distance_from_city_center
+        return round(price, 1)
+
+    def wash_single_car(self, car: Car) -> float:
+        if self.clean_power > car.clean_mark:
+            result = self.calculate_washing_price(car)
+            car.clean_mark = self.clean_power
+            return result
+
+    def rate_service(self, score: int) -> None:
+        score = self.average_rating * self.count_of_ratings + score
+        self.count_of_ratings += 1
+        self.average_rating = round(score / self.count_of_ratings, 1)
