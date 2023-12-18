80c3d87b6eac54b56b65007c28c8d14ef83e50f3
Evgeniy Vasin <evgeniy97v@gmail.com>
2023-12-18 19:52:18 +0200

    edit mistake

--- app/main.py
+++ app/main.py
@@ -4,15 +4,17 @@ from typing import List
 class Car:
     def __init__(
         self, comfort_class: int, clean_mark: int, brand: str
-        ) -> None:
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float,
-                clean_power: int, average_rating: float, count_of_ratings: int) -> None:
+    def __init__(
+            self, distance_from_city_center: float,
+            clean_power: int, average_rating: float, count_of_ratings: int
+        ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -28,8 +30,8 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         return round(
-            car.comfort_class * (self.clean_power - car.clean_mark) * (self.average_rating
-            / self.distance_from_city_center),
+            car.comfort_class * (self.clean_power - car.clean_mark)
+             * (self.average_rating / self.distance_from_city_center),
             1
         )
 
