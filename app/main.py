e57d089b4ca402598341a02e06a3243b96322263
Evgeniy Vasin <evgeniy97v@gmail.com>
2023-12-18 19:25:59 +0200

    edit flake 8 mistake

--- app/main.py
+++ app/main.py
@@ -1,17 +1,19 @@
 from typing import List
 
+
 class Car:
     def __init__(
-            self, comfort_class: int, clean_mark: int, brand: str
-            ) -> None:
+        self, comfort_class: int, clean_mark: int, brand: str
+        ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
+
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, 
-                 clean_power: int, 
-                 average_rating: float, 
+    def __init__(self, distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
                  count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
@@ -27,7 +29,8 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         return round(
-            car.comfort_class * (self.clean_power - car.clean_mark) * (self.average_rating / self.distance_from_city_center),
+            car.comfort_class * (self.clean_power - car.clean_mark)
+             * (self.average_rating / self.distance_from_city_center),
             1
         )
 
