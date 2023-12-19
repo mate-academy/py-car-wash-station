02e16e9e7dd6ee26e22ef977b18cf4ab8f20a59d
Evgeniy Vasin <evgeniy97v@gmail.com>
2023-12-19 15:36:54 +0200

    edit indentations

--- app/main.py
+++ app/main.py
@@ -12,9 +12,9 @@ class Car:
 
 class CarWashStation:
     def __init__(
-            self, distance_from_city_center: float,
-            clean_power: int, average_rating: float, count_of_ratings: int
-        ) -> None:
+        self, distance_from_city_center: float,
+        clean_power: int, average_rating: float, count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -31,7 +31,7 @@ class CarWashStation:
     def calculate_washing_price(self, car: Car) -> float:
         return round(
             car.comfort_class * (self.clean_power - car.clean_mark)
-             * (self.average_rating / self.distance_from_city_center),
+            * (self.average_rating / self.distance_from_city_center),
             1
         )
 
