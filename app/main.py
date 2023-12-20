3814277c2671ec269f9bea1b7b635444fdaceaf3
kabust <tulyulyuk91@gmail.com>
2023-12-20 13:44:38 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,42 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def calculate_washing_price(self, car: Car):
+        return (car.comfort_class
+                * (self.clean_power - car.clean_mark)
+                * self.average_rating / self.distance_from_city_center)
+
+    def wash_single_car(self, car: Car):
+        if self.clean_power > car.clean_mark:
+            car.clean_mark = self.clean_power
+
+    def rate_service(self, rate: int):
+        self.count_of_ratings += 1
+        self.average_rating += (rate - self.average_rating) / self.count_of_ratings
+        # Sometimes values are not as expected due to the way floats are stored in memory
+        # We can fix it using string formatting, but test requires only floats
+        self.average_rating = round(self.average_rating, 1)
+
+    def serve_cars(self, cars: list[Car]):
+        income = 0
+        for car in cars:
+            if car.clean_mark > self.clean_power:
+                continue
+            income += self.calculate_washing_price(car)
+            self.wash_single_car(car)
+        return income
