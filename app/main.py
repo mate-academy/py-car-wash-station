10bbb546d27a04c07f4fe97e7f7c1f65c7c17029
sergey <38cerg12021979@jmal.com>
2023-12-24 15:50:15 +0200

    24_12_23_16_00

--- app/main.py
+++ app/main.py
@@ -15,21 +15,21 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, list_car: list) -> int:
+    def serve_cars(self, cars: list[Car]) -> int:
         result = 0
-        for car in list_car:
+        for car in cars:
             if self.clean_power > car.clean_mark:
                 result += self.calculate_washing_price(car)
                 self.wash_single_car(car)
         return result
 
-    def calculate_washing_price(self, car: str) -> int:
+    def calculate_washing_price(self, car: Car) -> int:
         return round(car.comfort_class
                      * (self.clean_power - car.clean_mark)
                      * (self.average_rating
                         / self.distance_from_city_center), 1)
 
-    def wash_single_car(self, car: str) -> None:
+    def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
