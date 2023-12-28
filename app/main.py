44d0f011ae5524ae37d20319e21d77c8cc9a121f
Serhii Panchenko <serji13@gmail.com>
2023-12-29 00:03:14 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -27,15 +27,19 @@ class CarWashStation:
         return income
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round(car.comfort_class * (
-                    self.clean_power - car.clean_mark) * self.average_rating
-                     / self.distance_from_city_center, 1)
+        return round(
+            car.comfort_class * (self.clean_power - car.clean_mark)
+            * self.average_rating / self.distance_from_city_center, 1
+        )
 
     def wash_single_car(self, car: Car) -> bool:
         return self.clean_power > car.clean_mark
 
     def rate_service(self, rate: int) -> None:
         self.average_rating = round(
-            (self.average_rating * self.count_of_ratings + rate) / (
-                        self.count_of_ratings + 1), 1)
+            (
+                self.average_rating * self.count_of_ratings + rate) / (
+                self.count_of_ratings + 1
+            ), 1
+        )
         self.count_of_ratings += 1
