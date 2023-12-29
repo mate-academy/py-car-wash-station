6b24f507646c7b511aef0a94f37fd35e19619ebf
Vladyslav Pliashechnyk <152858380+Vladyslav-Pliashechnyk@users.noreply.github.com>
2023-12-29 17:26:57 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,21 +1,23 @@
 class Car:
-    def __init__(self,
-                 comfort_class: int,
-                 clean_mark: int,
-                 brand: str
-                 ) -> None:
+    def __init__(
+        self, 
+        comfort_class: int, 
+        clean_mark: int, 
+        brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self,
-                 distance_from_city_center: float,
-                 clean_power: int,
-                 average_rating: float,
-                 count_of_ratings: int
-                 ) -> None:
+    def __init__(
+        self, 
+        distance_from_city_center: float, 
+        clean_power: int, 
+        average_rating: float, 
+        count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -36,7 +38,8 @@ class CarWashStation:
                      / self.distance_from_city_center, 1)
 
     def wash_single_car(self, car: Car) -> None:
-        car.clean_mark = self.clean_power
+        if self.clean_power > car.clean_mark:
+            car.clean_mark = self.clean_power
 
     def rate_service(self, rate: int) -> None:
         total_score = self.average_rating * self.count_of_ratings + rate
