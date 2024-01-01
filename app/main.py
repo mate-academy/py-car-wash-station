0c157295c5703bcabe1f1ff0bb53553493662649
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-01-01 17:47:19 +0200

    fix annotation and indentation

--- app/main.py
+++ app/main.py
@@ -26,14 +26,15 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         incomes = [self.wash_single_car(car) for car in cars]
         incomes = sum([income for income in incomes if income])
         return round(incomes, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        price = car.comfort_class * (self.clean_power - car.clean_mark) * \
-            self.average_rating / self.distance_from_city_center
+        price = (car.comfort_class * (self.clean_power - car.clean_mark)
+                 * self.average_rating / self.distance_from_city_center)
+
         return round(price, 1)
 
     def wash_single_car(self, car: Car) -> float:
