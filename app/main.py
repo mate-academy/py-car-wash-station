bd3cddb25f946087b63056817f20cca0ac00b5b9
DenZatolokin <denzatolokin@gmail.com>
2024-01-02 18:06:52 +0200

    washing station is working (corrected)

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         income = 0
         for car in cars:
             income += self.calculate_washing_price(car)
