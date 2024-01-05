80b82110df1ba2188cf44394dffc564dc8c3108f
PythonZem <pythonzem@gmail.com>
2024-01-05 19:57:14 +0200

    add solution / Not redy to relis
    - develop serve_cars, calculate_washing_price,
    wash_single_car,
    ate_service

--- app/main.py
+++ app/main.py
@@ -21,20 +21,24 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars_list: list) -> int:
-        pass
+    def serve_cars(self, cars_list: list) -> float:
+        result = 0
+        for car in cars_list:
+            if car.clean_mark < self.clean_power:
+                result += self.calculate_washing_price(car)
+                self.wash_single_car(car)
+        return result
 
-    def calculate_washing_price(self,
-                                comfort_class: int,
-                                clean_mark: int
-                                ) -> int:
-        pass
+    def calculate_washing_price(self, car: Car) -> float:
+        price = car.comfort_class * (self.clean_power - car.clean_mark)\
+                * self.average_rating / self.distance_from_city_center
+        return round(price, 1)
 
-    def wash_single_car(self,
-                        comfort_class: int,
-                        clean_mark: int
-                        ) -> int:
-        pass
+    def wash_single_car(self, car: Car) -> None:
+        car.clean_mark = self.clean_power
 
-    def rate_service(self, rate: int) -> int:
-        pass
+    def rate_service(self, rate: int) -> None:
+        total_rate = self.average_rating * self.count_of_ratings
+        self.count_of_ratings += 1
+        new_rate = round((total_rate + rate) / self.count_of_ratings, 1)
+        self.average_rating = new_rate
