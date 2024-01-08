e3e4ed9fec24a6d93966a2d4183b7b9f853b83df
volry <v.v.ryzhuk@gmail.com>
2024-01-08 20:18:07 +0200

    init CarWashStation

--- app/main.py
+++ app/main.py
@@ -5,5 +5,8 @@ class Car:
         self. brand = brand
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float, count_of_ratings: int):
+       self.distance_from_city_center = distance_from_city_center
+       self.clean_power = clean_power
+       self.average_rating = average_rating
+       self.count_of_ratings =count_of_ratings
