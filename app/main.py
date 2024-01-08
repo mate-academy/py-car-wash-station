a3e478de42963aaf75dfbeffeded1bf27e0c284d
volry <v.v.ryzhuk@gmail.com>
2024-01-08 20:11:25 +0200

    init Car class

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 class Car:
-    # write your code here
-    pass
-
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self. brand = brand
 
 class CarWashStation:
     # write your code here
