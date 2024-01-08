4e895376d9cf9230d9286890bc685d70ae555128
volry <v.v.ryzhuk@gmail.com>
2024-01-08 21:06:06 +0200

    formatting issues

--- app/main.py
+++ app/main.py
@@ -1,15 +1,22 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(self, comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self. brand = brand
 
+
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float, count_of_ratings: int):
-       self.distance_from_city_center = distance_from_city_center
-       self.clean_power = clean_power
-       self.average_rating = average_rating
-       self.count_of_ratings =count_of_ratings
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, cars):
         total_income = 0.0
@@ -22,7 +29,7 @@ class CarWashStation:
         return round(total_income, 1)
 
 
-    def calculate_washing_price(self, car):
+    def calculate_washing_price(self, car) -> None:
         if self.clean_power > car.clean_mark:
             cost = car.comfort_class * (self.clean_power - car.clean_mark) * (
                                self.average_rating / self.distance_from_city_center)
