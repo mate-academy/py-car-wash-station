1c2a5bd6279e2acc271454361ed1d3a151290dd0
PythonZem <pythonzem@gmail.com>
2024-01-08 16:29:18 +0200

    keep one style Flake8

--- app/main.py
+++ app/main.py
@@ -1,29 +1,32 @@
 class Car:
-    def __init__(self,
-                 comfort_class: int,
-                 clean_mark: int,
-                 brand: str
-                 ) -> None:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self,
-                 distance_from_city_center: int,
-                 clean_power: int,
-                 average_rating: int,
-                 count_of_ratings: int
-                 ) -> None:
+    def __init__(
+            self,
+            distance_from_city_center: int,
+            clean_power: int,
+            average_rating: int,
+            count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self,
-                   cars_list: list
-                   ) -> float:
+    def serve_cars(
+            self,
+            cars_list: list
+    ) -> float:
         result = 0
         for car in cars_list:
             if car.clean_mark < self.clean_power:
@@ -31,22 +34,25 @@ class CarWashStation:
                 self.wash_single_car(car)
         return result
 
-    def calculate_washing_price(self,
-                                car: Car
-                                ) -> float:
+    def calculate_washing_price(
+            self,
+            car: Car
+    ) -> float:
         different = self.clean_power - car.clean_mark
         rate_to_dist = self.average_rating / self.distance_from_city_center
         price = round(car.comfort_class * different * rate_to_dist, 1)
         return price
 
-    def wash_single_car(self,
-                        car: Car
-                        ) -> None:
+    def wash_single_car(
+            self,
+            car: Car
+    ) -> None:
         car.clean_mark = self.clean_power
 
-    def rate_service(self,
-                     rate: int
-                     ) -> None:
+    def rate_service(
+            self,
+            rate: int
+    ) -> None:
         total_rate = self.average_rating * self.count_of_ratings
         self.count_of_ratings += 1
         new_rate = round((total_rate + rate) / self.count_of_ratings, 1)
