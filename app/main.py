1624c771586feb184b23165d879295014e8a4355
volry <v.v.ryzhuk@gmail.com>
2024-01-08 20:54:54 +0200

    fix error in init CarWash+ add washing_price

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ class Car:
 class CarWashStation:
     def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float, count_of_ratings: int):
        self.distance_from_city_center = distance_from_city_center
-       self.clean_power = clean_power-Car
+       self.clean_power = clean_power
        self.average_rating = average_rating
        self.count_of_ratings =count_of_ratings
 
@@ -16,7 +16,12 @@ class CarWashStation:
            pass
 
        def calculate_washing_price(self, car):
-           pass
+           if self.clean_power > car.clean_mark:
+               cost = car.comfort_class * (self.clean_power - car.clean_mark) * (
+                               self.average_rating / self.distance_from_city_center)
+               return round(cost, 1)
+            else:
+               return 0.0
 
        def wash_single_car(self, car):
            pass
