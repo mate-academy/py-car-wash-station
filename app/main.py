32d3a8531ed822804eeebfb096bb191949640d51
Serhii Buryk <sergmarten21@gmail.com>
2024-01-10 19:12:27 +0200

    changed annotation

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list) -> None:
+    def serve_cars(self, cars: list) -> float:
         income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
@@ -25,13 +25,13 @@ class CarWashStation:
                 self.wash_single_car(car)
         return income
 
-    def calculate_washing_price(self, car: object) -> None:
+    def calculate_washing_price(self, car: Car) -> float:
         return round(
             (car.comfort_class * (self.clean_power - car.clean_mark)
              * self.average_rating / self.distance_from_city_center), 1
         )
 
-    def wash_single_car(self, car: object) -> None:
+    def wash_single_car(self, car: Car) -> int:
         car.clean_mark = self.clean_power
         return car.clean_mark
 
@@ -41,4 +41,3 @@ class CarWashStation:
                    + rate) / (self.count_of_ratings + 1), 1)
         )
         self.count_of_ratings += 1
-        return self.average_rating, self.count_of_ratings
