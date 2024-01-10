6a57e74aa684e6faa1a99ea77bfc9cd2750b569b
Tymofii Skopenko <youmustdeth@gmail.com>
2024-01-10 19:45:52 +0200

    update wash_single_car

--- app/main.py
+++ app/main.py
@@ -35,7 +35,8 @@ class CarWashStation:
         )
 
     def wash_single_car(self, car: Car) -> None:
-        car.clean_mark = self.clean_power
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
 
     def rate_service(self, rating: float) -> None:
         self.average_rating = round(
