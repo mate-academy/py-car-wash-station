465dd02017f96fa336b7e43ac021a11d2f498da0
Serhii Dobrovinskii <serhiy.dobrovinskiy@gmail.com>
2024-01-10 19:14:50 +0200

    sol

--- app/main.py
+++ app/main.py
@@ -7,19 +7,17 @@ class Car:
         self.clean_mark = clean_mark
         self.brand = brand
 
-
 class CarWashStation:
     def __init__(self,
                  distance_from_city_center: float,
                  clean_power: int,
                  average_rating: float,
                  count_of_ratings: int,
-                 income: float = 0.0) -> None:
+                 ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
-        self.income = income
 
     def serve_cars(self, cars: list[Car]) -> float:
         income = 0.0
