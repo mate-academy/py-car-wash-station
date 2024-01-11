c28c0a6d168ae6958cc137a11172f4bbc73b898b
Serhii Buryk <sergmarten21@gmail.com>
2024-01-11 11:04:34 +0200

    add checker to wash_single_car and include PEP8 styling

--- app/main.py
+++ app/main.py
@@ -28,16 +28,20 @@ class CarWashStation:
     def calculate_washing_price(self, car: Car) -> float:
         return round(
             (car.comfort_class * (self.clean_power - car.clean_mark)
-             * self.average_rating / self.distance_from_city_center), 1
+             * self.average_rating / self.distance_from_city_center)
+            , 1
         )
 
     def wash_single_car(self, car: Car) -> int:
-        car.clean_mark = self.clean_power
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
         return car.clean_mark
 
     def rate_service(self, rate: int) -> None:
         self.average_rating = (
-            round(((self.average_rating * self.count_of_ratings)
-                   + rate) / (self.count_of_ratings + 1), 1)
+            round(
+                ((self.average_rating * self.count_of_ratings) + rate)
+                / (self.count_of_ratings + 1)
+                , 1)
         )
         self.count_of_ratings += 1
