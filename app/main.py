dd690f3d390eb890accad12b4060861be9211a3c
Vitalii Haponiuk <doninpotato@gmail.com>
2024-01-12 17:10:24 +0200

    corrected

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ class Car:
             self,
             comfort_class: int,
             clean_mark: int,
-            brand: str) -> None:
+            brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -15,7 +16,8 @@ class CarWashStation:
             distance_from_city_center: float,
             clean_power : int,
             average_rating : float,
-            count_of_ratings : int) -> None:
+            count_of_ratings : int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
