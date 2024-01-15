132869e6a25b4258f8e50b1005edf727d4c7f9e0
Serhii Buryk <sergmarten21@gmail.com>
2024-01-15 19:12:56 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 class Car:
-    def __init__(self, comfort_class: int,
+    def __init__(self,
+                 comfort_class: int,
                  clean_mark: int,
                  brand: str) -> None:
         self.comfort_class = comfort_class
@@ -8,7 +9,8 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float,
+    def __init__(self,
+                 distance_from_city_center: float,
                  clean_power: int,
                  average_rating: float,
                  count_of_ratings: int) -> None:
@@ -17,7 +19,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
@@ -27,9 +29,13 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         return round(
-            (car.comfort_class * (self.clean_power - car.clean_mark)
-             * self.average_rating / self.distance_from_city_center)
-            , 1
+            (
+                    car.comfort_class
+                    * (self.clean_power - car.clean_mark)
+                    * self.average_rating
+                    / self.distance_from_city_center
+            ),
+            1
         )
 
     def wash_single_car(self, car: Car) -> int:
@@ -38,10 +44,12 @@ class CarWashStation:
         return car.clean_mark
 
     def rate_service(self, rate: int) -> None:
-        self.average_rating = (
-            round(
-                ((self.average_rating * self.count_of_ratings) + rate)
-                / (self.count_of_ratings + 1)
-                , 1)
+        self.average_rating = round(
+            (
+                    ((self.average_rating * self.count_of_ratings)
+                     + rate)
+                    / (self.count_of_ratings + 1)
+            ),
+            1
         )
         self.count_of_ratings += 1
