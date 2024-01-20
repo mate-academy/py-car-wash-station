42a3fb5f0928aafce5149c5458ffb64d87d40130
voronvasia <voronvasia@gmail.com>
2024-01-20 22:04:20 +0200

    fixed bug

--- app/main.py
+++ app/main.py
@@ -41,7 +41,8 @@ class CarWashStation:
             )
 
     def wash_single_car(self, car: Car) -> None:
-        car.clean_mark = self.clean_power
+        if self.clean_power > car.clean_mark:
+            car.clean_mark = self.clean_power
 
     def rate_service(self, rate: int) -> None:
         self.average_rating = round(
