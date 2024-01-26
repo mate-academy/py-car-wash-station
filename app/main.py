e3595bb41a3a3dbb5d80f8739d8ded9c737a3f27
Vladyslav Supruniuk <Vladyslav.Supruniuk@contracted.pmi.com>
2024-01-26 14:27:47 +0200

    Fix

--- app/main.py
+++ app/main.py
@@ -45,7 +45,8 @@ class CarWashStation:
         )
 
     def wash_single_car(self, car: Car) -> None:
-        car.clean_mark = self.clean_power
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
 
     def rate_service(self, rate: int) -> None:
         self.average_rating = round(
