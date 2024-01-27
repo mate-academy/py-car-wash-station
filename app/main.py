b577d6ac51a1bdad64b3badefa26343b622cd012
stefanko <denys.stefanko@gmail.com>
2024-01-27 12:01:57 +0200

    linter fix

--- app/main.py
+++ app/main.py
@@ -23,7 +23,8 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         return round(car.comfort_class * (self.clean_power - car.clean_mark)
-                     * (self.average_rating / self.distance_from_city_center), 1)
+                     * (self.average_rating / self.distance_from_city_center),
+                     1)
 
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
