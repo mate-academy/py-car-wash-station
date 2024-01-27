9de51abbe87d73cf3a6fae40782d755aab05091a
stefanko <denys.stefanko@gmail.com>
2024-01-27 12:06:52 +0200

    linter fix

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 class Car:
-    def __init__(self, comfort_class, clean_mark, brand):
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center, clean_power,
-                 average_rating, count_of_ratings):
+    def __init__(self, distance_from_city_center: int, clean_power: int,
+                 average_rating: float, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
