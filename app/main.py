1953e143758bf8975a62d1f78106120a7440b280
Oleksii Muhyr <1.2.aznch@gmai.com>
2024-01-29 16:14:21 +0200

    py-car-wash station changed

--- app/main.py
+++ app/main.py
@@ -16,24 +16,24 @@ class CarWashStation:
 
     def serve_cars(self, cars: list) -> float:
         income = 0.0
-
         for car in cars:
-            if car.clean_mark < self.clean_power:
-                income += self.clean_power * 1.5
+            if self.clean_power > car.clean_mark:
+                income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
         return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        price = (car.comfort_class * (self.clean_power - car.clean_mark)
-                 * (self.average_rating / self.distance_from_city_center))
-        return round(price, 1)
+        return round(
+            (car.comfort_class * (self.clean_power - car.clean_mark)
+             * self.average_rating / self.distance_from_city_center), 1
+        )
 
     def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
-    def rate_service(self, rating: float) -> None:
-        total_rating = self.average_rating * self.count_of_ratings
+    def rate_service(self, rate: float) -> None:
         self.count_of_ratings += 1
-        self.average_rating = round((total_rating + rating)
+        self.average_rating = round(self.average_rating
+                                    + (rate - self.average_rating)
                                     / self.count_of_ratings, 1)
