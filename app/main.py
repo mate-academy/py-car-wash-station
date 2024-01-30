7571bbc0be115feabd73beb0d507ff58e5641601
Viktar Chaunavy <paca.vaca.pachvara@gmail.com>
2024-01-30 22:01:11 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,21 +1,25 @@
 class Car:
-    def __init__(self, comfort_class: int,
-                 clean_mark: int, brand: str) -> None:
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float,
-                 clean_power: bool, average_rating: float,
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: bool,
+                 average_rating: float,
                  count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, list_of_cars: list) -> float:
+    def serve_cars(self, list_of_cars: list[Car]) -> float:
         cost_for_job = 0
         for car in list_of_cars:
             if self.clean_power > car.clean_mark:
@@ -29,7 +33,6 @@ class CarWashStation:
                      * self.average_rating / self.distance_from_city_center, 1)
 
     def wash_single_car(self, next_car: Car) -> None:
-        # if self.clean_power > next_car.clean_mark:
         next_car.clean_mark = self.clean_power
 
     def rate_service(self, new_rate: int) -> None:
