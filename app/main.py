54a333a0135c697307b933746ce3de1c1099e78d
Ihor Dmytriv <dmytriwihor@gmail.com>
2024-02-01 12:11:39 +0100

    Corrected the annotation

--- app/main.py
+++ app/main.py
@@ -35,7 +35,7 @@ class CarWashStation:
             1
         )
 
-    def serve_cars(self, cars: list) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
