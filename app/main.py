116d4beb1ddc7a3953e2a080ca839407ebe3fc2a
dmytro-yefimenko <dmytro@yefimenko.uk>
2024-02-02 20:11:48 +0200

    Fixed return type annotations

--- app/main.py
+++ app/main.py
@@ -1,6 +1,3 @@
-from typing import Any
-
-
 class Car:
     def __init__(self, comfort_class: int,
                  clean_mark: int, brand: str) -> None:
@@ -18,16 +15,16 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def calculate_washing_price(self, car: Car) -> Any:
+    def calculate_washing_price(self, car: Car) -> float:
         price = (car.comfort_class * (self.clean_power - car.clean_mark)
                  * (self.average_rating / self.distance_from_city_center))
         return round(price, 1)
 
-    def wash_single_car(self, car: Car) -> Any:
+    def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
-    def serve_cars(self, cars: list) -> Any:
+    def serve_cars(self, cars: list[Car]) -> float:
         income = 0
 
         for car in cars:
@@ -38,7 +35,7 @@ class CarWashStation:
 
         return round(income, 1)
 
-    def rate_service(self, new_rating: int) -> Any:
+    def rate_service(self, new_rating: int) -> None:
         sum_of_ratings = (self.average_rating
                           * self.count_of_ratings) + new_rating
         self.count_of_ratings += 1
