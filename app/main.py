224189e4b4886a1c1fd8019b4673a4a6a967648b
Molchanov Ihor <=>
2024-02-03 16:09:02 +0200

    fixed code style, change money list->int, delete unused filter

--- app/main.py
+++ app/main.py
@@ -1,32 +1,36 @@
 class Car:
-    def __init__(self,
-                 comfort_class: int,
-                 clean_mark: int,
-                 brand: str) -> None:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self,
-                 distance_from_city_center: int,
-                 clean_power: int,
-                 average_rating: float,
-                 count_of_ratings: int) -> None:
+    def __init__(
+            self,
+            distance_from_city_center: int,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, car_list: list) -> float:
-        money = []
+        money = 0
         for car in car_list:
             if car.clean_mark < self.clean_power:
                 cash = self.calculate_washing_price(car)
-                money.append(cash)
+                money += cash
                 self.wash_single_car(car)
-        return round(sum(money), 1)
+        return round(money, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
         result = round(
@@ -36,10 +40,8 @@ class CarWashStation:
         )
         return result
 
-    def wash_single_car(self,
-                        car: Car) -> None:
-        if car.clean_mark < self.clean_power:
-            car.clean_mark = self.clean_power
+    def wash_single_car(self, car: Car) -> None:
+        car.clean_mark = self.clean_power
 
     def rate_service(self, rating: float) -> None:
         self.count_of_ratings += 1
