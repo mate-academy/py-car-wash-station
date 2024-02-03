cdec9a4084340cbf00259e0a7831c97659cecd2e
Molchanov Ihor <=>
2024-02-03 14:03:07 +0200

    fixed flake8 error (line size)

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: int, clean_power: int, average_rating: float,
+    def __init__(self,
+                 distance_from_city_center: int,
+                 clean_power: int,
+                 average_rating: float,
                  count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
@@ -30,11 +36,14 @@ class CarWashStation:
         )
         return result
 
-    def wash_single_car(self, car: Car) -> None:
+    def wash_single_car(self,
+                        car: Car) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
     def rate_service(self, rating: float) -> None:
         self.count_of_ratings += 1
-        new_rating = ((self.average_rating * (self.count_of_ratings - 1)) + rating) / self.count_of_ratings
+        new_rating = (
+                             (self.average_rating * (self.count_of_ratings - 1))
+                      + rating) / self.count_of_ratings
         self.average_rating = round(new_rating, 1)
