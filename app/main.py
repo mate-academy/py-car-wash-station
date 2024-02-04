c4f36aaf37b7a6a83780dedc8e36df8e676d95e7
RomanHlodann <roman.hlodann@gmail.com>
2024-02-04 22:12:11 +0200

    fixed logic and code readability

--- app/main.py
+++ app/main.py
@@ -1,33 +1,43 @@
 class Car:
-    def __init__(self, comfort_class: int,
-                 clean_mark: int, brand: str) -> None:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: int,
-                 clean_power: int, average_rating: float,
-                 count_of_ratings: int) -> None:
+    def __init__(
+            self,
+            distance_from_city_center: int,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, cars: list) -> float:
-        return round(sum([self.wash_single_car(car) for car in cars]), 1)
+        income = 0
+        for car in cars:
+            if self.clean_power > car.clean_mark:
+                income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
+        return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
         return round(car.comfort_class * (self.clean_power - car.clean_mark)
                      * self.average_rating / self.distance_from_city_center, 1)
 
-    def wash_single_car(self, car: Car) -> float:
-        if self.clean_power >= car.clean_mark:
-            washing_price = self.calculate_washing_price(car)
+    def wash_single_car(self, car: Car) -> None:
+        if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
-            return washing_price
-        return 0
 
     def rate_service(self, rate: float) -> None:
         self.average_rating = round(
