8da1539a0886fcc9b119d80c09db674585719d54
Anna Zadorozhna <annalepiska@gmail.com>
2024-02-05 21:21:59 +0200

    removed redundant check

--- app/main.py
+++ app/main.py
@@ -44,8 +44,7 @@ class CarWashStation:
         )
 
     def wash_single_car(self, car: Car) -> None:
-        car.clean_mark = self.clean_power \
-            if (self.clean_power > car.clean_mark) else car.clean_mark
+        car.clean_mark = self.clean_power
 
     def rate_service(self, mark: float) -> None:
         self.average_rating = round(
