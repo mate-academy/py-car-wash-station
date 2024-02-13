77ec17ecf3dd0ba11360df8f82d5cc0e11ac4880
Maksim <sma4no.7@gmail.com>
2024-02-13 17:33:17 +0100

    Im, confused. Please help

--- app/main.py
+++ app/main.py
@@ -18,34 +18,33 @@ class CarWashStation:
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, cars: list):
-        income = 0
+        income_counter = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
-                income += car.comfort_class * (self.clean_power - car.clean_mark) * (
-                            self.average_rating / self.distance_from_city)
+                income_counter += car.comfort_class * (self.clean_power - car.clean_mark) * (
+                        self.average_rating / self.distance_from_city)
                 car.clean_mark = self.clean_power
-
-        return round(income, 1)
+        return round(income_counter, 1)
 
     def calculate_washing_price(self, car):
-        return round(car.comfort_class * (self.clean_power - car.clean_mark) * (self.average_rating / self.distance_from_city), 1)
+        return round(
+            car.comfort_class * (self.clean_power - car.clean_mark) * (self.average_rating / self.distance_from_city),
+            1)
+
     def wash_single_car(self, car):
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
-    def rate_service(self,rate):
+    def rate_service(self, rate):
         self.count_of_ratings += 1
         total_ratings_sum = self.average_rating * (self.count_of_ratings - 1) + rate
-        self.average_rating = round((total_ratings_sum / self.count_of_ratings),1)
-
-
+        self.average_rating = round((total_ratings_sum / self.count_of_ratings), 1)
 
 
 bmw = Car(3, 3, 'BMW')
 audi = Car(4, 9, 'Audi')
 mercedes = Car(7, 1, 'Mercedes')
 ford = Car(2, 1, 'Ford')
-
 
 ws = CarWashStation(6, 8, 3.9, 11)
 
