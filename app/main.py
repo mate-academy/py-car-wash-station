01e69a43676df7060ccdfe38dcb3deb970971468
Maksim <sma4no.7@gmail.com>
2024-02-14 12:39:43 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -32,6 +32,7 @@ class CarWashStation:
                      * (self.clean_power - car.clean_mark)
                      * (self.average_rating / self.distance_from_city))
                 car.clean_mark = self.clean_power
+                self.wash_single_car(car)
         return round(income_counter, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
