df6ab39bcc13810d80a86d7b46febfcc0bf1041f
Mikita Havryliuk <chinter.games1@gmail.com>
2024-02-20 13:37:43 +0200

    feature: create Car class, init CarWashStation class, write calculate_washing_price method from CarWashStation

--- app/main.py
+++ app/main.py
@@ -1,8 +1,32 @@
+from typing import List
+
+
 class Car:
-    # write your code here
-    pass
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(
+            self,
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
+    ) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, cars_list: List[Car]):
+        pass
+
+    def calculate_washing_price(self, car: Car) -> float:
+        return round(
+            ((car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating)
+                / self.distance_from_city_center),
+            1
+        )
