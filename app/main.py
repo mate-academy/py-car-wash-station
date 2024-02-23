1831b9604415b02e09a28e5c856bf553f82a0485
skeletronys <mykytcey@gmail.com>
2024-02-23 12:07:48 +0100

    fix all

--- app/main.py
+++ app/main.py
@@ -1,27 +1,31 @@
 class Car:
-    def __init__(self, comfort_class: int,
-                 clean_mark: int,
-                 brand: str) -> None:
+    def __init__(
+                self,
+                comfort_class: int,
+                clean_mark: int,
+                brand: str
+                ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
-    pass
 
 
 class CarWashStation:
-    def __init__(self,
+    def __init__(
+                 self,
                  distance_from_city_center: float,
                  clean_power: int,
                  average_rating: float,
-                 count_of_ratings: float) -> None:
+                 count_of_ratings: float
+                ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, car_list: list) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         income = 0.0
-        for car in car_list:
+        for car in cars:
             if car.clean_mark < self.clean_power:
                 income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
