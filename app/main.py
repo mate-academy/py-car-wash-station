2bf7db5458f6f4fba56e135af25f71b4ace0b632
MRGL <Sonemongoo@gmail.com>
2024-02-24 17:03:07 +0200

    'Flake8'

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float, count_of_ratings: int):
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -20,17 +27,19 @@ class CarWashStation:
                 self.wash_single_car(car)
         return income
 
-    def calculate_washing_price(self, car) -> float:
-        price = (car.comfort_class *
-                 abs(self.clean_power - car.clean_mark) *
-                 self.average_rating /
-                 self.distance_from_city_center)
+    def calculate_washing_price(self, car: Car) -> float:
+        price = (car.comfort_class
+                 * abs(self.clean_power - car.clean_mark)
+                 * self.average_rating
+                 / self.distance_from_city_center)
         return round(price, 1)
 
     def wash_single_car(self, car: Car) -> None:
         car.clean_mark = self.clean_power
 
     def rate_service(self, mark: int) -> None:
-        new_rating = (self.average_rating * self.count_of_ratings + mark) / (self.count_of_ratings + 1)
+        new_rating = ((self.average_rating
+                      * self.count_of_ratings + mark)
+                      / (self.count_of_ratings + 1))
         self.average_rating = round(new_rating, 1)
         self.count_of_ratings += 1
