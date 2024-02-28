54db176dd73a064326a7916ac8000684da7001c8
Olha <olgazinevych@gmail.com>
2024-02-28 22:02:35 +0200

    Specified that serve_cars accepts list[Car] and removed else

--- app/main.py
+++ app/main.py
@@ -14,14 +14,12 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
                 income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
-            else:
-                continue
         return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
