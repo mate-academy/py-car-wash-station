062c80e8a24d5e03c3988f6980b13ffc37ec74dd
olvyshh <olvyshh@gmail.com>
2024-02-28 21:56:23 +0200

    Solution(edited 2)

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Car:
     def __init__(self,
                  comfort_class: int,
@@ -21,7 +24,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list[Car]) -> float:
+    def serve_cars(self, cars: List[Car]) -> float:
         income = 0.0
         for car in cars:
             if self.clean_power > car.clean_mark:
