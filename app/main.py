99488391eecaf358e5408cde3a4bf739e2836fcd
Sribniak Taras <sribniak.terry@protonmail.com>
2024-02-28 18:43:17 +0200

    funcs: serve_cars, calculate_washing_price, wash_single_car, rate_service are added

--- app/main.py
+++ app/main.py
@@ -15,3 +15,29 @@ class CarWashStation:
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, cars: list) -> float:
+        income = 0.0
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
+        return round(income, 1)
+
+    def calculate_washing_price(self, car: Car) -> float:
+        return round(
+            car.comfort_class * (
+                    self.clean_power - car.clean_mark) * self.average_rating / self.distance_from_city_center,
+            1
+        )
+
+    def wash_single_car(self, car: Car) -> None:
+        if self.clean_power > car.clean_mark:
+            car.clean_mark = self.clean_power
+
+    def rate_service(self, rating: float) -> None:
+        new_average_rating = (
+                                     (self.average_rating * self.count_of_ratings) + rating
+                             ) / (self.count_of_ratings + 1)
+        self.average_rating = round(new_average_rating, 1)
+        self.count_of_ratings += 1
