d9800450584cf598333db3b2c7dfd6660d5a1a2d
olvyshh <olvyshh@gmail.com>
2024-02-28 21:17:02 +0200

    Solution(edited)

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: [Car]) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         income = 0.0
         for car in cars:
             if self.clean_power > car.clean_mark:
