44a928d5d961d4591bc0b44567c6a12ee6b2392a
kirillpolozenko <polozenko.kirill.job@gmail.com>
2024-03-04 11:34:21 +0200

    fix: fix lint

--- app/main.py
+++ app/main.py
@@ -1,26 +1,26 @@
 class Car:
     def __init__(self,
-                comfort_class: int,
-                clean_mark: int,
-                brand: str
-                ) -> None:
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str
+                 ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
- 
+
 
 class CarWashStation:
     def __init__(self,
-                distance_from_city_center: float,
-                clean_power: int,
-                average_rating: float,
-                count_of_ratings: int
-                ) -> None:
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int
+                 ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
-     
+
     def serve_cars(self, cars: list[Car]) -> float:
         income = 0
         for car in cars:
@@ -28,7 +28,7 @@ class CarWashStation:
                 income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
         return round(income, 1)
-     
+
     def calculate_washing_price(self, car: Car) -> float:
         clean_difference = self.clean_power - car.clean_mark
         correlation = self.average_rating / self.distance_from_city_center
