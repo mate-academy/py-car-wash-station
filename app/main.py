094b886c0afd56d49e4efcde035f4a645a568994
Andrii Zubchenko <152086790+dissom@users.noreply.github.com>
2024-03-06 21:15:57 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,17 +1,21 @@
 class Car:
-    def __init__(self, comfort_class: int | float,
-                 clean_mark: int | float, brand: str) -> None:
+    def __init__(
+        self, comfort_class: int | float,
+                 clean_mark: int | float, brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: int | float,
-                 clean_power: int | float,
-                 average_rating: int | float,
-                 count_of_ratings: int | float
-                 ) -> None:
+    def __init__(
+        self, 
+        distance_from_city_center: int | float,
+        clean_power: int | float,
+        average_rating: int | float,
+        count_of_ratings: int | float
+    ) -> None:
 
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
