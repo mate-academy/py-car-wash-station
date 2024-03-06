e0bb91131f86b859251b628837e6283c3bdbe5bf
Andrii Zubchenko <dissom1987@gmail.com>
2024-03-06 21:10:30 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ class CarWashStation:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
-    def serve_cars(self, cars: list = Car) -> int | float:
+    def serve_cars(self, cars: list[Car]) -> int | float:
         income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
