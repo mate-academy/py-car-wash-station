427a722c4be741690c749d3243c1cbc91eb61055
Kyrylo <kbuchinskiy1@gmail.com>
2024-03-11 21:40:54 +0200

    fix fix fix Solution

--- app/main.py
+++ app/main.py
@@ -41,7 +41,6 @@ class CarWashStation:
     def wash_single_car(self, car: Car) -> float:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
-            
 
     def rate_service(self, mark: float) -> None:
         self.average_rating = round((self.average_rating
