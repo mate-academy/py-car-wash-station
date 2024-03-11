d9dd02e139125e08937d20365a63cec27fe5b4d4
Serhiy Krakivskiy <krakivskiy@gmail.com>
2024-03-12 00:04:37 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -24,20 +24,15 @@ class CarWashStation:
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, cars: list[Car]) -> any:
-        income = sum(
-            [
-                self.calculate_washing_price(car)
-                for car in cars
-                if car.clean_mark < self.clean_power
-            ]
-        )
-        [self.wash_single_car(car)
-         for car in cars
-         if car.clean_mark < self.clean_power]
+        income = 0
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
         return income
 
     def calculate_washing_price(self, car: Car) -> float:
-        washing_price = round(
+        return round(
             (
                 car.comfort_class
                 * (self.clean_power - car.clean_mark)
@@ -45,17 +40,15 @@ class CarWashStation:
             ),
             1,
         )
-        return washing_price
 
     def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
-    def rate_service(self, *args) -> None:
-        for rate in args:
-            new_count_of_rating = self.count_of_ratings + 1
-            new_average_rating = (
-                (self.average_rating * self.count_of_ratings) + rate
-            ) / new_count_of_rating
-            self.average_rating = round(new_average_rating, 1)
-            self.count_of_ratings = new_count_of_rating
+    def rate_service(self, rate: float) -> None:
+        new_count_of_rating = self.count_of_ratings + 1
+        new_average_rating = (
+            (self.average_rating * self.count_of_ratings) + rate
+        ) / new_count_of_rating
+        self.average_rating = round(new_average_rating, 1)
+        self.count_of_ratings = new_count_of_rating
