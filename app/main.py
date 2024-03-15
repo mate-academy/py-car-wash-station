27086d2eb5a848c6c11d122aabefca8baadf52b2
Shershen Alexandra <hornet240204@gmail.com>
2024-03-15 09:15:40 +0200

    Fix E126 error

--- app/main.py
+++ app/main.py
@@ -32,10 +32,10 @@ class CarWashStation:
         return income
 
     def calculate_washing_price(self, car: Car) -> float:
-        washing_price = (
-                car.comfort_class * (self.clean_power - car.clean_mark)
-                * self.average_rating / self.distance_from_city_center
-        )
+        washing_price = (car.comfort_class
+                         * (self.clean_power - car.clean_mark)
+                         * self.average_rating
+                         / self.distance_from_city_center)
         return round(washing_price, 1)
 
     def wash_single_car(self, car: Car) -> None:
