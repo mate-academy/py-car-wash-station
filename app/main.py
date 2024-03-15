e8f26c4aca7d6d93cac9d6f1338ea346f9a67131
kekW <panicua12@gmail.com>
2024-03-15 12:06:40 +0200

    fixed flake8 and other related errors

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int,
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
                  brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
@@ -7,27 +9,32 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: int, clean_power: int,
-                 average_rating: float, count_of_ratings: int) -> None:
+    def __init__(self,
+                 distance_from_city_center: int,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = round(average_rating, 1)
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list, income: float = 0) -> float:
+    def serve_cars(self, cars: list[Car], income: float = 0) -> float:
         for car in cars:
             if car.clean_mark < self.clean_power:
                 income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
         return income
 
-    def calculate_washing_price(self, car: callable) -> float:
-        calc_price = (car.comfort_class * (self.clean_power - car.clean_mark)
-                      * self.average_rating / self.distance_from_city_center)
+    def calculate_washing_price(self, car: Car) -> float:
+        calculated_price = (car.comfort_class
+                            * (self.clean_power - car.clean_mark)
+                            * self.average_rating
+                            / self.distance_from_city_center)
 
-        return round(calc_price, 1)
+        return round(calculated_price, 1)
 
-    def wash_single_car(self, car: callable) -> None:
+    def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
