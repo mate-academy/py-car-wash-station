55855b0deffce61ff8052b56f985d2cae93b553c
Andrii Chubenko <stalkerock@gmail.com>
2024-03-18 13:07:39 +0200

    Fixing of problem with readibility and annotations

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str)\
-            -> None:
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: int, clean_power: int,
-                 average_rating: int, count_of_ratings: int) -> None:
+    def __init__(
+        self,
+        distance_from_city_center: int,
+        clean_power: int,
+        average_rating: float,
+        count_of_ratings: int,
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -27,11 +31,13 @@ class CarWashStation:
             car.clean_mark = self.clean_power
 
     def calculate_washing_price(self, car: Car) -> float:
-        return (car.comfort_class * (self.clean_power - car.clean_mark)
-                * (self.average_rating / self.distance_from_city_center))
+        return (
+            car.comfort_class
+            * (self.clean_power - car.clean_mark)
+            * (self.average_rating / self.distance_from_city_center)
+        )
 
     def rate_service(self, rating: int) -> None:
         total_rating = self.average_rating * self.count_of_ratings
         self.count_of_ratings += 1
-        self.average_rating = round((total_rating + rating)
-                                    / self.count_of_ratings, 1)
+        self.average_rating = round((total_rating + rating) / self.count_of_ratings, 1)
