aa5cdecaecde6a6dfc08862572f428f6f681601d
Vladyslav Prymas <prymas.vladyslav@gmail.com>
2024-03-19 19:30:53 +0200

    Removed quotes

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list["Car"]) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         income = 0
         for car in cars:
             if car.clean_mark >= self.clean_power:
@@ -28,14 +28,14 @@ class CarWashStation:
             self.wash_single_car(car)
         return income
 
-    def calculate_washing_price(self, car: "Car") -> float:
+    def calculate_washing_price(self, car: Car) -> float:
         price = round(car.comfort_class
                       * (self.clean_power - car.clean_mark)
                       * self.average_rating
                       / self.distance_from_city_center, 1)
         return price
 
-    def wash_single_car(self, car: "Car") -> None:
+    def wash_single_car(self, car: Car) -> None:
         car.clean_mark = self.clean_power
 
     def rate_service(self, rate: int) -> None:
