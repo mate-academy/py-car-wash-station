b36909b35b6697cc6287a447f5806d1e902251bb
Pavlo <pavlo@MacBook-Pro-de-Pavlo.local>
2024-03-20 00:43:17 +0100

    Solution: fix flake8 errors

--- app/main.py
+++ app/main.py
@@ -1,18 +1,14 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(self, comfort_class: int, clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(
-        self,
-        distance_from_city_center: int,
-        clean_power: int,
-        average_rating: float,
-        count_of_ratings: int
-    ) -> None:
+    def __init__(self, distance_from_city_center: int, clean_power: int,
+                 average_rating: float, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -27,7 +23,8 @@ class CarWashStation:
         return income
 
     def calculate_washing_price(self, car: Car) -> float:
-        cost = car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating / self.distance_from_city_center
+        cost = (car.comfort_class * (self.clean_power - car.clean_mark)
+                * self.average_rating / self.distance_from_city_center)
         return round(cost, 1)
 
     def wash_single_car(self, car: Car) -> float:
@@ -42,4 +39,3 @@ class CarWashStation:
         new_rate_sum = self.average_rating * self.count_of_ratings + rating
         self.count_of_ratings += 1
         self.average_rating = round(new_rate_sum / self.count_of_ratings, 1)
-
