7c829f8fc9a13170cb652821c10824694aa48e7d
Serhii Khodko <sergeyhodko123@gmail.com>
2024-03-19 10:12:25 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
+from typing import List
+
+
 class Car:
-    def __init__(self, comfort_class: int,
+    def __init__(self,
+                 comfort_class: int,
                  clean_mark: int,
                  brand: str) -> None:
         self.comfort_class = comfort_class
@@ -8,8 +12,11 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int,
-                 average_rating: float, count_of_ratings: int) -> None:
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -32,7 +39,7 @@ class CarWashStation:
         total_rating += rate
         self.average_rating = round(total_rating / self.count_of_ratings, 1)
 
-    def serve_cars(self, cars: list) -> float:
+    def serve_cars(self, cars: List[Car]) -> float:
         total_cost = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
