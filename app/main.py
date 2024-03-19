f1546c085b532245bc44f0022b77e269dd433c20
semermaks <semermaks@mail.com>
2024-03-19 09:37:29 +0200

    fixed rating int to float 2.0

--- app/main.py
+++ app/main.py
@@ -15,8 +15,8 @@ class CarWashStation:
     def __init__(self,
                  distance_from_city_center: int,
                  clean_power: int,
-                 average_rating: int,
-                 count_of_ratings: float) -> None:
+                 average_rating: float,
+                 count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
