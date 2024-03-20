40948fd58c9a07919892afa6506f0e2b4d352922
bwaah1 <chykivan@gmail.com>
2024-03-20 12:07:12 +0200

    Add value validators

--- app/main.py
+++ app/main.py
@@ -5,6 +5,10 @@ class Car:
             clean_mark: int,
             brand: str
     ) -> None:
+        if not 1 <= comfort_class <= 7:
+            raise ValueError("Comfort class should be between 1 and 7")
+        if not 1 <= clean_mark <= 10:
+            raise ValueError("Clean mark should be between 1 and 10")
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -13,11 +17,19 @@ class Car:
 class CarWashStation:
     def __init__(
             self,
-            distance_from_city_center: int,
+            distance_from_city_center: float,
             clean_power: int,
             average_rating: float,
             count_of_ratings: int
     ) -> None:
+        if not 1.0 <= distance_from_city_center <= 10.0:
+            raise ValueError("Distance from city center should be between 1.0 and 10.0")
+        if not 1 <= clean_power <= 10:
+            raise ValueError("Clean power should be between 1 and 10")
+        if not 1.0 <= average_rating <= 5.0:
+            raise ValueError("Average rating should be between 1.0 and 5.0")
+        if count_of_ratings < 0:
+            raise ValueError("Count of ratings cannot be negative")
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -38,10 +50,13 @@ class CarWashStation:
                          / self.distance_from_city_center)
         return round(washing_price, 1)
 
-    def wash_single_car(self, car: Car) -> bool:
-        car.clean_mark = self.clean_power
+    def wash_single_car(self, car: Car) -> None:
+        if self.clean_power > car.clean_mark:
+            car.clean_mark = self.clean_power
 
     def rate_service(self, rating: int) -> None:
+        if not 1.0 <= rating <= 5.0:
+            raise ValueError("Rating should be between 1.0 and 5.0")
         total_ratings = self.average_rating * self.count_of_ratings
         self.count_of_ratings += 1
         self.average_rating = round((total_ratings + rating)
