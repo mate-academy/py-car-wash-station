7e917499d031020dba44354393f95f1483d727f0
Ruxxell <furman06228@gmail.com>
2024-03-20 16:47:09 +0200

    Fixed 3.0.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 class Car:
     def __init__(self,
                  comfort_class: int,
@@ -49,12 +50,12 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         return round(car.comfort_class
-            * (self.clean_power
-            - car.clean_mark)
-            * self.average_rating
-            / self.distance_from_city_center,
-            1
-        )
+                     * (self.clean_power
+                        - car.clean_mark)
+                     * self.average_rating
+                     / self.distance_from_city_center,
+                     1
+                     )
 
     def wash_multiple_cars(self, cars: list) -> None:
         for car in cars:
