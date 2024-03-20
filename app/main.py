bd5b183f3f096263edc610278265169dcaad75ff
Serhii Khodko <118362909+messedxup@users.noreply.github.com>
2024-03-20 15:12:39 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from typing import List
 
 
+
 class Car:
     def __init__(self,
                  comfort_class: int,
@@ -23,26 +24,31 @@ class CarWashStation:
         self.count_of_ratings = count_of_ratings
 
     def wash_single_car(self, car: Car) -> None:
-        if self.clean_power > car.clean_mark:
-            car.clean_mark = self.clean_power
+        if 1 <= car.clean_mark <= 10:
+            if self.clean_power > car.clean_mark:
+                car.clean_mark = self.clean_power
 
     def calculate_washing_price(self, car: Car) -> float:
         clean_diff = self.clean_power - car.clean_mark
-        return round(car.comfort_class
-                     * clean_diff
-                     * self.average_rating
-                     / self.distance_from_city_center, 1)
+        if (1 <= car.comfort_class <= 7
+                and 1.0 <= self.distance_from_city_center <= 10.0):
+            return round(car.comfort_class
+                         * clean_diff
+                         * self.average_rating
+                         / self.distance_from_city_center, 1)
 
     def rate_service(self, rate: int) -> None:
-        total_rating = self.average_rating * self.count_of_ratings
-        self.count_of_ratings += 1
-        total_rating += rate
-        self.average_rating = round(total_rating / self.count_of_ratings, 1)
+        if 1.0 <= self.average_rating <= 5.0:
+            total_rating = self.average_rating * self.count_of_ratings
+            self.count_of_ratings += 1
+            total_rating += rate
+            self.average_rating = round(total_rating / self.count_of_ratings, 1)
 
     def serve_cars(self, cars: List[Car]) -> float:
         total_cost = 0
         for car in cars:
-            if car.clean_mark < self.clean_power:
-                total_cost += self.calculate_washing_price(car)
-                self.wash_single_car(car)
+            if 1 <= car.clean_mark <= 10:
+                if car.clean_mark < self.clean_power:
+                    total_cost += self.calculate_washing_price(car)
+                    self.wash_single_car(car)
         return total_cost
