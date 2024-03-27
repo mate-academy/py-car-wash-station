2609f60ea2a55a5ed70783116146e2479361169e
dvkim28 <d.villarionovich@gmail.com>
2024-03-27 01:12:08 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -2,14 +2,17 @@ from typing import List
 
 
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) \
+            -> None:
         self.comfort_class: int = comfort_class
         self.clean_mark: int = clean_mark
         self.brand: str = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float, count_of_ratings: int) -> None:
+    def __init__(self, distance_from_city_center: float, clean_power: int,
+                 average_rating: float, count_of_ratings: int) \
+            -> None:
         self.distance_from_city_center: float = distance_from_city_center
         self.clean_power: int = clean_power
         self.average_rating: float = average_rating
@@ -25,7 +28,8 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         return round(
-            car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating / self.distance_from_city_center,
+            car.comfort_class * (self.clean_power - car.clean_mark)
+            * self.average_rating / self.distance_from_city_center,
             1
         )
 
@@ -34,5 +38,9 @@ class CarWashStation:
             car.clean_mark = self.clean_power
 
     def rate_service(self, rating: int) -> None:
-        self.average_rating = round((self.average_rating * self.count_of_ratings + rating) / (self.count_of_ratings + 1), 1)
+        self.average_rating = round(
+            (self.average_rating * self.count_of_ratings + rating)
+            / (self.count_of_ratings + 1),
+            1
+        )
         self.count_of_ratings += 1
